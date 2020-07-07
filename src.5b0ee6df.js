2ab394f32069c63bc8753637a197ac9e4919a247
Alexander Repeta <alexander.repeta@gmail.com>
2020-07-07 22:37:33 +0300

    Updates

--- src.5b0ee6df.js
+++ src.5b0ee6df.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/parcel-project-template/src.884e08e2.js.map
+//# sourceMappingURL=/src.5b0ee6df.js.map
