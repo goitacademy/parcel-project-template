17b20b5145487d286431ba05a876977e1d13b555
Roman Voznyy <roman.voznyy.52@gmail.com>
2020-07-11 19:14:31 +0300

    fixed modal.js and modal styles

--- src/modal.js
+++ src/modal.js
@@ -1,12 +1,16 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
+    closeModalBtn: document.querySelectorAll('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.forEach((el) => {
+    el.addEventListener('click', toggleModal);
+  });
+  refs.closeModalBtn.forEach((el) => {
+    el.addEventListener('click', toggleModal);
+  });
 
   function toggleModal() {
     document.body.classList.toggle('modal-open');
