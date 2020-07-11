2c377b0f2c6f739143107bcb4509c95be88719c7
Oleksii Fedorkan <alex.fedorkan@gmail.com>
2020-07-11 17:52:21 +0300

    Add all layouts and styles for tablet and desktop.

---
+++ src/form-send.js
@@ -0,0 +1,11 @@
+(() => {
+  document.querySelector('.js-speaker-form').addEventListener('submit', e => {
+    e.preventDefault();
+
+    new FormData(e.currentTarget).forEach((value, name) =>
+      console.log(`${name}: ${value}`),
+    );
+
+    e.currentTarget.reset();
+  });
+})();
