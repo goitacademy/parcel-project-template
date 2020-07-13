26c88078907cfc6a6a7b78390b1861ba3770050c
taraiihor <63466237+taraiihor@users.noreply.github.com>
2020-07-13 17:50:54 +0300

    додав картинки по точкам

---
+++ src/slider.js
@@ -0,0 +1,51 @@
+/* Индекс слайда по умолчанию */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+/* Функция увеличивает индекс на 1, показывает следующй слайд*/
+function plusSlide() {
+  showSlides((slideIndex += 1));
+}
+
+/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+function minusSlide() {
+  showSlides((slideIndex -= 1));
+}
+
+/* Устанавливает текущий слайд */
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+/* Основная функция слайдера */
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName('slider-item');
+  var dots = document.getElementsByClassName('slider-dots_item');
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += ' active';
+}
+
+document
+  .querySelector('.button-slayder .prev')
+  .addEventListener('click', minusSlide);
+
+document
+  .querySelector('.button-slayder .next')
+  .addEventListener('click', plusSlide);
+
+document.querySelector('.item-1').addEventListener('click', currentSlide(1));
+document.querySelector('.item-2').addEventListener('click', currentSlide(2));
+document.querySelector('.item-3').addEventListener('click', currentSlide(3));
