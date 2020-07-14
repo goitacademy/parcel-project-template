17acb40877a96a3fb0872d33e0e030207d2bfc5b
Irina-Koksharova <biodgerelo@ukr.net>
2020-07-15 00:40:42 +0300

    slider is ready

--- src/js/siema.js
+++ src/js/siema.js
@@ -1,4 +1,35 @@
 import Siema from 'siema';
 const mySiema = new Siema();
-document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
-document.querySelector('.next').addEventListener('click', () => mySiema.next());
+
+const refs = {
+  indicators: Array.from(document.querySelectorAll('.image-identify')),
+};
+
+const findActiveIndex = () =>
+  refs.indicators.findIndex(elem => elem.classList.contains('is-check'));
+
+document.querySelector('.prev').addEventListener('click', () => {
+  mySiema.prev();
+
+  const prevElemIndex = findActiveIndex();
+
+  if (prevElemIndex === 0) {
+    return;
+  }
+
+  refs.indicators[prevElemIndex].classList.remove('is-check');
+  refs.indicators[prevElemIndex - 1].classList.add('is-check');
+});
+
+document.querySelector('.next').addEventListener('click', () => {
+  mySiema.next();
+
+  const prevElemIndex = findActiveIndex();
+
+  if (prevElemIndex === refs.indicators.length - 1) {
+    return;
+  }
+
+  refs.indicators[prevElemIndex].classList.remove('is-check');
+  refs.indicators[prevElemIndex + 1].classList.add('is-check');
+});
