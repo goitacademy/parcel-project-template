ab872cc3b107da7cc9ae466e73f2db24bd8f7987
Andrii Vakhotskyi <andre2603foreigner@gmail.com>
2020-07-14 13:11:48 +0300

    Modal window script

---
+++ src/js/modal-script.js
@@ -0,0 +1,19 @@
+const refs = {
+  openModalBtn: document.querySelector('[data-open-modal]'),
+  closeModalBtn: document.querySelector('[data-close-modal]'),
+  backdrop: document.querySelector('[data-backdrop]'),
+};
+
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
+
+refs.backdrop.addEventListener('click', logBackdropClick);
+
+function toggleModal() {
+  document.body.classList.toggle('modal-open');
+  refs.backdrop.classList.toggle('is-hidden');
+}
+
+function logBackdropClick() {
+  console.log('Это клик в бекдроп');
+}
