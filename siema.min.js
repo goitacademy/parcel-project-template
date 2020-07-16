f76c99db40fb3a5df3d0b046b3de951c80baf456
Powerman-code <64640573+Powerman-code@users.noreply.github.com>
2020-07-16 17:03:08 +0300

    1

--- siema.min.js
+++
@@ -1,3 +0,0 @@
-const mySiema = new Siema();
-document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
-document.querySelector('.next').addEventListener('click', () => mySiema.next());
