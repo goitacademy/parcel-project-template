e10c55945c45ac6f1179e3ae9c839262e13f3280
Andriy Kyrylenko <z9877969@ukr.net>
2020-07-16 22:54:36 +0300

    remove comment from slider-dremka

--- src/slider-dremka.js
+++ src/slider-dremka.js
@@ -51,12 +51,6 @@
       prevRadio.removeAttribute('checked');
 
       this.displayActiveItem();
-      // refs.items[this.activeIndex].setAttribute('data-active', '');
-      // [...refs.items]
-      //   .filter(el => !el.getAttribute('data-active'))
-      //   .map(el => el.setAttribute('style', 'display: block'));
-
-      // refs.items[this.activeIndex].setAttribute('style', 'display: block');
       refs.radioBtns[this.activeIndex].setAttribute('checked', '');
     },
 
@@ -77,43 +71,14 @@
       this.setActiveItem();
       console.log('this.activeIndex', this.activeIndex);
     },
-
-    // listingRadio({ target }) {
-    //   const { name, value } = target;
-    //   const activeRadio = value;
-    //   console.log('name', name);
-    //   // if (name === 'slider') {
-    //   this.activeIndex = [...refs.radioList.children].findIndex(
-    //     el => el === target.closest('[data-radio]'),
-    //   );
-    //   this.setActiveItem();
-    //   // }
-    //   console.log('this.activeIndex', this.activeIndex);
-    // },
   };
-
-  // slider.displayActiveItem.call(slider);
 
   refs.nextBtn.addEventListener('click', slider.listingNext.bind(slider));
   refs.prevBtn.addEventListener('click', slider.listingPrev.bind(slider));
-  // refs.radioList.addEventListener('click', slider.listingRadio.bind(slider));
-
-  // const onResize = () => {
-  //   if (window.matchMedia('screen and (max-width: 479px)').matches) {
-  //     slider.start();
-  //   } else {
-  //     slider.stop();
-  //   }
-  // };
-
-  // window.addEventListener('load', () => slider.displayActiveItem());
 
   // listner for run slider
   window.addEventListener('load', () => {
-    //   if (window.matchMedia('screen and (max-width: 479px)').matches) {
     slider.start();
-    //   }
   });
   // listner for run slider -END
 })();
-// window.addEventListener('resize', onResize);
