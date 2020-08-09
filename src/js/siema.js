75d69599d988d7ae382b4d926bfad1f1a6264190
Alena <alena.senkiv@gmail.com>
2020-08-09 23:06:16 +0300

    header with siema slider

---
+++ src/js/siema.js
@@ -0,0 +1,5 @@
+import Siema from 'siema';
+
+const mySiema = new Siema({});
+document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
+document.querySelector('.next').addEventListener('click', () => mySiema.next());
