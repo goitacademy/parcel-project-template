09997d11ab1c096627718f1d194974ba2705def3
Dmitry Medychenko <rAv3ndima91@gmail.com>
2020-08-10 20:49:06 +0300

    Updates

--- menu.0c91648c.js
+++ menu.0c91648c.js
@@ -167,7 +167,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56800" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61979" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
