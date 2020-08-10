bc8d0f25f4e4a4c4021956f9604c9e0ffd8dee30
Dmitry Medychenko <rAv3ndima91@gmail.com>
2020-08-10 21:31:21 +0300

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -221,7 +221,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61979" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62293" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
