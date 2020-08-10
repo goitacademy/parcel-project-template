8fcc90341ac6824e9d85f39fda4ba9fd3bebbe4e
Maksym Lytvyniuk <lytvyniukmax@gmail.com>
2020-08-10 15:47:40 +0300

    new header with carousel

--- src/menu.js
+++ src/menu.js
@@ -1,15 +1,22 @@
 (() => {
-  const menuBtnRef = document.querySelector('[data-menu-button]');
-  const mobileMenuRef = document.querySelector('[data-menu]');
+  const refs = {
+    openModalBtn: document.querySelector('[data-menu-open]'),
+    closeModalBtn: document.querySelector('[data-menu-close]'),
+    modal: document.querySelector('[data-menu]'),
+    menuNav: document.querySelector('.menu-navigation'),
+  };
 
-  menuBtnRef.addEventListener('click', () => {
-    const expanded =
-      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-
-    menuBtnRef.classList.toggle('is-open');
-    menuBtnRef.setAttribute('aria-expanded', !expanded);
-
-    mobileMenuRef.classList.toggle('is-open');
-    mobileMenuRef.classList.toggle('is-hidden');
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.menuNav.addEventListener('click', e => {
+    if (e.target.value !== e.currentTarget.value) {
+      return;
+    }
+    refs.modal.classList.remove('is-open');
   });
+
+  function toggleModal() {
+    document.body.classList.toggle('menu-open');
+    refs.modal.classList.toggle('is-open');
+  }
 })();
