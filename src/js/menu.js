b8c716b7ccab23a4b19d209e61cffe7b9dd457b2
Bagdasar Ovsepyan <bagdasar@ovsepyan.com.ua>
2020-08-27 21:53:49 +0300

    Create menu.js

---
+++ src/js/menu.js
@@ -0,0 +1,16 @@
+(() => {
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+  const logoRef = document.querySelector('[data-logo]');
+
+  menuBtnRef.addEventListener('click', () => {
+    const expanded =
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileMenuRef.classList.toggle('is-open');
+    logoRef.classList.toggle('is-open');
+  });
+})();
