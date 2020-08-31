2ed9eba2f05d3682c7fcf7d752fa4824b3dc5ad0
redreren <jelka0503@gmail.com>
2020-08-31 15:14:14 +0300

    Create modal.js

---
+++ src/js/modal.js
@@ -0,0 +1,14 @@
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector('[data-modal-open]'),
+        closeModalBtn: document.querySelector('[data-modal-close]'),
+        modal: document.querySelector('[data-modal]'),
+    };
+
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+
+    function toggleModal() {
+        refs.modal.classList.toggle('is-hidden');
+    }
+})();
