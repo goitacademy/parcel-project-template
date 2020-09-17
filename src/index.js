d11a1d2bdd43d7e5c29b85fb671630934d2bfbfd
nata <kotenkonata8@gmail.com>
2020-09-17 22:28:54 +0300

    Feedback Slider

--- src/index.js
+++ src/index.js
@@ -1,2 +1,36 @@
+import $ from 'jquery';
+import 'slick-carousel';
 import './sass/main.scss';
 import './modal.js';
+
+$(document).ready(() => {
+  const phoneWidth = 320;
+  const tabletWidth = 768;
+  const desktopWidth = 1314;
+
+  $('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: false,
+    asNavFor: '.slider-nav',
+  });
+
+  $('.slider-nav').slick({
+    dots: false,
+    slidesToShow: 4,
+    asNavFor: '.slider',
+    arrows: false,
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: tabletWidth,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
