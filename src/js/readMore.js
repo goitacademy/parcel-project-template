d4ee3fa44670e389dbcc97d6fe7f8a1e20955702
NataliBorshch <shrusher0367@gmail.com>
2020-09-19 11:12:57 +0300

    скрипт

--- src/js/readMore.js
+++ src/js/readMore.js
@@ -1,16 +1,15 @@
-function readMore(){
+function readMore() {
     var dots = document.getElementById("dots");
     var more = document.getElementById("more");
     var btn = document.getElementById("btn");
 
-    if(dots.style.display === "none"){
-        dots.style.display="inline";
-        btn.innerHTML="Читать далее";
-        more.style.display="none";
-    }
-    else{
-        dots.style.display="none";
-        btn.innerHTML="Скрыть";
-        more.style.display="inline";
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btn.innerHTML = "Читать далее";
+        more.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btn.innerHTML = "Скрыть";
+        more.style.display = "inline";
     }
 }
