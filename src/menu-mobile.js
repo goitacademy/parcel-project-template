e9234c2a4653f0d549257318ec3eccb3fe78ecea
YekaterinaPavlenko <pavluka1605@gmail.com>
2020-09-20 11:54:29 +0300

    добавляет скрипт и часть стилей впрограмму

--- src/menu-mobile.js
+++ src/menu-mobile.js
@@ -10,32 +10,12 @@
     mobileMenuRef.classList.toggle('is-open');
   });
   menuClose.addEventListener('click', () => {
-    const expanded =
-      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
     mobileMenuRef.classList.toggle('is-open');
   });
+  let elements = document.querySelectorAll('.menu-link');
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].onclick = function () {
+      mobileMenuRef.classList.toggle('is-open');
+    };
+  }
 })();
-
-// (() => {
-//   const menuBtnRef = document.querySelector('[data-menu-button]');
-//   const mobileMenuRef = document.querySelector('[data-menu]');
-//   const menuClose = document.querySelector('[data-menu-close]');
-//   const menuClick = document.querySelectorAll('.menu-link');
-//   menuBtnRef.addEventListener('click', () => {
-//     const expanded =
-//       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-//     menuBtnRef.classList.toggle('is-open');
-//     menuBtnRef.setAttribute('aria-expanded', !expanded);
-//     mobileMenuRef.classList.toggle('is-open');
-//   });
-//   menuClose.addEventListener('click', () => {
-//     const expanded =
-//       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-//     mobileMenuRef.classList.toggle('is-open');
-//   });
-//   menuClick.addEventListener('click', () => {
-//     const expanded =
-//       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-//     mobileMenuRef.classList.toggle('is-open');
-//   });
-// })();
