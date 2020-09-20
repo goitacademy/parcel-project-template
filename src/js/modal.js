e171ae7165a73a67a3fff604bf543dece2f5ed36
Aleksey Gelich <alexeygelich@gmail.com>
2020-09-20 13:03:22 +0300

    добавляет кнопку наверх

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,11 +3,21 @@
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
+    closeModalBtn1: document.querySelector('[data-modal-close1]'),
+    closeModalBtn2: document.querySelector('[data-modal-close2]'),
+    closeModalBtn3: document.querySelector('[data-modal-close3]'),
+    closeModalBtn4: document.querySelector('[data-modal-close4]'),
+    closeModalBtn5: document.querySelector('[data-modal-close5]'),
     modal: document.querySelector('[data-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn1.addEventListener('click', toggleModal);
+  refs.closeModalBtn2.addEventListener('click', toggleModal);
+  refs.closeModalBtn3.addEventListener('click', toggleModal);
+  refs.closeModalBtn4.addEventListener('click', toggleModal);
+  refs.closeModalBtn5.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-open');
