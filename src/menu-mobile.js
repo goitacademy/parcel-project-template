df1cdc8ba0ed0529edeaed0741e28a1d920b1964
YekaterinaPavlenko <pavluka1605@gmail.com>
2020-09-20 10:59:17 +0300

    добавляет часть стилей программы десктопа

--- src/menu-mobile.js
+++ src/menu-mobile.js
@@ -15,3 +15,27 @@
     mobileMenuRef.classList.toggle('is-open');
   });
 })();
+
+// (() => {
+//   const menuBtnRef = document.querySelector('[data-menu-button]');
+//   const mobileMenuRef = document.querySelector('[data-menu]');
+//   const menuClose = document.querySelector('[data-menu-close]');
+//   const menuClick = document.querySelectorAll('.menu-link');
+//   menuBtnRef.addEventListener('click', () => {
+//     const expanded =
+//       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+//     menuBtnRef.classList.toggle('is-open');
+//     menuBtnRef.setAttribute('aria-expanded', !expanded);
+//     mobileMenuRef.classList.toggle('is-open');
+//   });
+//   menuClose.addEventListener('click', () => {
+//     const expanded =
+//       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+//     mobileMenuRef.classList.toggle('is-open');
+//   });
+//   menuClick.addEventListener('click', () => {
+//     const expanded =
+//       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+//     mobileMenuRef.classList.toggle('is-open');
+//   });
+// })();
