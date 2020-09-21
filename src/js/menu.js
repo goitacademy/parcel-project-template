e82a64310eb2469803c4b2aacc023ecc963196c5
Iryna Zalata <iryna.zalata@gmail.com>
2020-09-21 12:03:10 +0300

    Виправляє помилки

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,7 +1,19 @@
 (() => {
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
+  const navLink = [...document.querySelectorAll('.site-nav-list-link')];
 
+  navLink.forEach(e => {
+    e.addEventListener('click', () => {
+      const expanded =
+        menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+      menuBtnRef.classList.toggle('is-open');
+      menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+      mobileMenuRef.classList.toggle('is-open');
+    });
+  });
   menuBtnRef.addEventListener('click', () => {
     const expanded =
       menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
