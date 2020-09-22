b36c9bba8ee3d4f7609972ea639c2cec50a5a1d1
IlyaLitvin <darnon88@gmail.com>
2020-09-22 10:33:35 +0300

    добавил бектутоп

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@ function backToTop() {
   let button = $('.back-to-top');
 
   $(window).on('scroll', () => {
-    if ($(this).scrollTop() >= 50) {
+    if ($(this).scrollTop() >= 150) {
       button.fadeIn();
     } else {
       button.fadeOut();
@@ -10,7 +10,7 @@ function backToTop() {
   });
 
   button.on('click', e => {
-    e.preventDefault();
+    // e.preventDefault();
     $('html'), animate({ scrollTop: 0 }, 1000);
   });
 }
