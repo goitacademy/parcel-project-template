1654633478c2ce1ba12ac867d6ce63c7bdf31b8c
Ivanov Evhenii <ivanov191192@gmail.com>
2020-10-14 18:34:46 +0300

    добавил слайдер

---
+++ src/js/swiper.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper('.swiper-container', {
+	autoHeight: true, //enable auto height
+	spaceBetween: 20,
+	slidesPerView: 1,
+	loop: true,
+	autoplay: {
+	  delay: 5000,
+	  disableOnInteraction: false,
+	},
+	pagination: {
+	  el: '.swiper-pagination',
+	  clickable: true,
+	},
+	navigation: {
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev',
+	},
+  });
