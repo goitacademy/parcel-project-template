956c92017e5c2a93d84f7144fc4cfa76ae955821
uroboro-ua <andrii.pavloshchuk@icloud.com>
2020-10-15 23:05:09 +0300

    Change #4
    Add hidden mode to tablet and mobile media for buttons on header

--- src/js/burger.js
+++ src/js/burger.js
@@ -2,16 +2,25 @@
   const openBtnRef = document.querySelector('[data-button-open]');
   const closeBtnRef = document.querySelector('[data-button-close]');
   const mobileMenuRef = document.querySelector('[data-menu]');
+  const headerButton = document.querySelector('.header__outside');
+
   openBtnRef.addEventListener('click', () => {
-    const expanded = openBtnRef.getAttribute('aria-expanded') === 'true' || false;
-    openBtnRef.classList.toggle('is-open');
-    openBtnRef.setAttribute('aria-expanded', !expanded);
+    //const expanded =
+    //openBtnRef.getAttribute('aria-expanded') === 'true' || false;
+    //openBtnRef.classList.toggle('is-open');
+    openBtnRef.style.visibility = 'hidden';
+    headerButton.style.visibility = 'hidden';
+    //openBtnRef.setAttribute('aria-expanded', !expanded);
     mobileMenuRef.classList.toggle('is-open');
   });
+
   closeBtnRef.addEventListener('click', () => {
-    const expanded = closeBtnRef.getAttribute('aria-expanded') === 'true' || false;
-    closeBtnRef.classList.toggle('is-open');
-    closeBtnRef.setAttribute('aria-expanded', !expanded);
+    //const expanded =
+    //closeBtnRef.getAttribute('aria-expanded') === 'true' || false;
+    //closeBtnRef.classList.toggle('is-open');
+    openBtnRef.style.visibility = 'initial';
+    headerButton.style.visibility = 'initial';
+    //closeBtnRef.setAttribute('aria-expanded', !expanded);
     mobileMenuRef.classList.toggle('is-open');
   });
 })();
