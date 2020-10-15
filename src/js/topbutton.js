4a9735ce2320e9c3dcd3aaad23640c1a2d03c2f8
Alex Starichenko <68742649+Glasgalas@users.noreply.github.com>
2020-10-15 16:21:30 +0300

    добавляет кнопку возврата наверх

---
+++ src/js/topbutton.js
@@ -0,0 +1,28 @@
+(function () {
+  'use strict';
+
+  function trackScroll() {
+    var scrolled = window.pageYOffset;
+    var coords = document.documentElement.clientHeight;
+
+    if (scrolled > coords) {
+      goTopBtn.classList.add('back_to_top-show');
+    }
+    if (scrolled < coords) {
+      goTopBtn.classList.remove('back_to_top-show');
+    }
+  }
+
+  function backToTop() {
+    var scrollStep = window.pageYOffset / 40;
+    if (window.pageYOffset > 0) {
+      window.scrollBy(0, -scrollStep);
+      setTimeout(backToTop, 0);
+    }
+  }
+
+  var goTopBtn = document.querySelector('.back_to_top');
+
+  window.addEventListener('scroll', trackScroll);
+  goTopBtn.addEventListener('click', backToTop);
+})();
