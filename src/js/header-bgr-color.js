3c3c096de317f2f8d6109b272e9c3ab61c0c62b7
Iryna Protsenko <irene.protsenko@gmail.com>
2020-10-16 03:26:14 +0300

    header for tablet -Draft need correct X and 2d btn

--- src/js/header-bgr-color.js
+++ src/js/header-bgr-color.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
   var scroll_pos = 0;
-  $('header1').scroll(function () {
+  $('header').scroll(function () {
     scroll_pos = $(this).scrollTop();
     if (scroll_pos > 390) {
-      $('header1').css('background-color', 'pink'),
-        $('header1').css('position', 'fixed');
+      $('header').css('background-color', 'pink'),
+        $('header').css('position', 'fixed');
     } else {
-      $('header1').css('background-color', 'aqua');
+      $('header').css('background-color', 'aqua');
     }
     console.log(scroll_pos);
   });
