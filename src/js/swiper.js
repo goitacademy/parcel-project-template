f8bd5d2483a89e267a8db3e86d93ca81995eda60
Ivanov Evhenii <ivanov191192@gmail.com>
2020-10-16 03:15:18 +0300

    пагинация слайдера

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,19 +1,20 @@
 var swiper = new Swiper('.swiper-container', {
-	autoHeight: true, //enable auto height
-	spaceBetween: 20,
-	slidesPerView: 1,
-	loop: true,
-	autoplay: {
-	  delay: 5000,
-	  disableOnInteraction: false,
-	},
-	pagination: {
-	  el: '.swiper-pagination',
-	  clickable: true,
-	},
-	navigation: {
-	  nextEl: '.swiper-button-next',
-	  prevEl: '.swiper-button-prev',
-	},
-  });
-  
+  autoHeight: true, //enable auto height
+  spaceBetween: 20,
+  slidesPerView: 1,
+  loop: true,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    bulletClass: 'swiper-pagination-bulletmy',
+    bulletActiveClass: 'swiper-pagination-bullet-activemy',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
