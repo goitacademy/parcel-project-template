3dcfb04b5872a13a0443f32aa9a49b7508ada896
Ivanov Evhenii <ivanov191192@gmail.com>
2020-10-18 17:24:47 +0300

    fix menu

--- src/js/body-scroll.js
+++ src/js/body-scroll.js
@@ -12,3 +12,20 @@
     refs.overflow.classList.toggle('modal-open');
   }
 })();
+
+// Скрипт что бы при открытии меню не двигалось боди
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-menu-open]'),
+    closeModalBtn: document.querySelector('[data-menu-close]'),
+    overflow: document.querySelector('[data-overflow]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.overflow.classList.toggle('modal-open');
+  }
+})();
