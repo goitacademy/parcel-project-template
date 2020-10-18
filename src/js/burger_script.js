83b0d841c4c6c725fbc88c108ec813c1a6289f64
Tetiana Vashchenko <svitlogic@gmail.com>
2020-10-18 03:17:15 +0300

    add script

--- src/js/burger_script.js
+++ src/js/burger_script.js
@@ -1,5 +1,5 @@
 (() => {
-  const menuBtnRef = document.querySelector('[data-menu-button]');
+  // const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
 
   menuBtnRef.addEventListener('click', () => {
