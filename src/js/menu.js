bbda5d7e89cfe1cc6fa7ccefc7d930db6dc458e4
Ivanov Evhenii <ivanov191192@gmail.com>
2020-10-19 14:27:16 +0300

    fix modal

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,14 +1,37 @@
 (() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
 
-  menuBtnRef.addEventListener("click", () => {
+  menuBtnRef.addEventListener('click', () => {
     const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
-    menuBtnRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
 
-    mobileMenuRef.classList.toggle("is-open");
+    mobileMenuRef.classList.toggle('is-open');
   });
 })();
+
+let menuBtnRef = document.querySelector('[data-menu-button]');
+
+document.addEventListener('click', e => {
+  let menu = document.querySelector('[data-menu]');
+
+  if (
+    menu.classList.contains('is-open') &&
+    !e.target.closest('[data-menu]') &&
+    !e.target.closest('.header-page__menu-button')
+  ) {
+    let event = new Event('click');
+    menuBtnRef.dispatchEvent(event);
+  }
+});
+
+document.querySelectorAll('.site-nav__item').forEach(elem => {
+  let event = new Event('click');
+
+  elem.addEventListener('click', e => {
+    menuBtnRef.dispatchEvent(event);
+  });
+});
--- src/js/modal-map.js
+++ src/js/modal-map.js
@@ -4,11 +4,31 @@
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
+  // refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
-
+  refs.openModalBtn.addEventListener('click', () => {
+    refs.modal.classList.add('is-open');
+    refs.modal.classList.remove('is-hidden');
+  });
+  refs.closeModalBtn.addEventListener('click', () => {
+    refs.modal.classList.remove('is-open');
+    refs.modal.classList.add('is-hidden');
+  });
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+document.addEventListener('click', e => {
+  let modal = document.querySelector('[data-modal]');
+  let menuBtnRef = document.querySelector('[data-modal-close]');
+
+  if (
+    modal.classList.contains('is-open') &&
+    !e.target.closest('.modal') &&
+    !e.target.closest('[data-modal-open]')
+  ) {
+    let event = new Event('click');
+    menuBtnRef.dispatchEvent(event);
+  }
+});
