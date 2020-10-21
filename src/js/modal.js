c2f0f5c85da6844607e14ee7204b27448c089021
AnastasiaPolkovnikova <nastya.polkovnikova.99@mail.ru>
2020-10-22 01:50:06 +0700

    .

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,20 +1,22 @@
 (() => {
-    const refs = {
-        openModalBtn: document.querySelector("[data-open-modal]"),
-        closeModalBtn: document.querySelector("[data-close-modal]"),
-        backdrop: document.querySelector("[data-backdrop]"),
-    };
+  const refs = {
+    openModalBtn: document.querySelector('[data-open-modal]'),
+    openModalBtn2: document.querySelector('[data-open-modal-2]'),
+    closeModalBtn: document.querySelector('[data-close-modal]'),
+    backdrop: document.querySelector('[data-backdrop]'),
+  };
 
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn2.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-    refs.backdrop.addEventListener("click", logBackdropClick);
+  refs.backdrop.addEventListener('click', logBackdropClick);
 
-    function toggleModal() {
-        refs.backdrop.classList.toggle("is-hidden");
-    }
+  function toggleModal() {
+    refs.backdrop.classList.toggle('is-hidden');
+  }
 
-    function logBackdropClick() {
-        console.log("Это клик в бекдроп");
-    }
+  function logBackdropClick() {
+    console.log('Это клик в бекдроп');
+  }
 })();
