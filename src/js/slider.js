8150629f605c51467bafea0ee754d7b6768844ac
Vitalii Kovalchuk <vitaliikovalchuk55@gmail.com>
2020-11-03 20:36:48 +0200

    slider

---
+++ src/js/slider.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        adaptiveHeight: true,
+        slidesToShow: 1,
+        swipe: true,
+    });  
+
+    $('.wrapper').slick({
+        adaptiveHeight: true,
+        slidesToShow: 7,
+    });  
+});
+
