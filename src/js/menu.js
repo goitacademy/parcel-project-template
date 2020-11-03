52dd772a88987f94ed4e6fcbdb25e968d3482a03
ka2nq1 <ka2nq1@gmail.com>
2020-11-03 10:33:29 +0200

    Update menu.js

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,14 +1,19 @@
 (() => {
     const menuBtnRef = document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
-
+    const menuNavLink = [...document.querySelectorAll(".menu-nav-link")];
+    menuNavLink.forEach(e => {
+        e.addEventListener('click', () => {
+            menuBtnRef.classList.toggle("is-open");
+            mobileMenuRef.classList.toggle("is-open");
+        })
+    });
     menuBtnRef.addEventListener("click", () => {
         const expanded =
         menuBtnRef.getAttribute("aria-expanded") === "true" || false;
         
         menuBtnRef.classList.toggle("is-open");
         menuBtnRef.setAttribute("aria-expanded", !expanded);
-
         mobileMenuRef.classList.toggle("is-open");
     });
 })();
