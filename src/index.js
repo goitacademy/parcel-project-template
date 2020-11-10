e6c412e4ac3d685717eb81f4da0e8d76b57f7da5
Dmytro Vasylenko <tredstoun651@uk.net>
2020-11-10 20:56:36 +0200

    добавляет слайдер

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import "./sass/main.scss";
+import Siema from 'siema';
+import subscribe from './js/subscribe'
+import svguse from './js/svgxuse'
+import menu from './js/menu'
+
+const mySiema = new Siema({
+  selector: '.siema',
+  duration: 500,
+  easing: 'cubic-bezier(0.4, 0, 0.2, 1)',
+  perPage: 1,
+  startIndex: 0,
+  draggable: true,
+  multipleDrag: true,
+  threshold: 20,
+  loop: true,
+  rtl: false,
+  onInit: () => {},
+  onChange: () => {},
+});
+
+
+setInterval(() => mySiema.next(), 5000);
+document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
+document.querySelector('.next').addEventListener('click', () => mySiema.next());
+
+svguse()
+
