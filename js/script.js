dd992c255be4a7de622b30cd23576c2401e53bdb
koval-andrey <kovalandrey1234@gmail.com>
2020-11-13 02:36:11 +0200

    slider
    13.11.2020

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:true,
+		dots:true,
+		slidesToShow:1,
+		autoplay:true,
+		speed:800,
+		fade:true,
+		autoplaySpeed:800,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:2
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow:1
+				}
+			}
+		]
+	});
+});
+
