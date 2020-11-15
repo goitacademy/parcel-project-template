89e459803f2cac30f00bed2e15aad7ca4c4fd1ea
NEXTru86 <70795090+NEXTru86@users.noreply.github.com>
2020-11-15 15:57:13 +0200

    Хедер сделал полностью под все устройства

--- src/menu.js
+++ src/menu.js
@@ -1,27 +1,3 @@
-// (() => {
-//     const menuBtnRef = document.querySelector("[data-menu-open]");
-//     const closeBtnRef = document.querySelector("[data-menu-close]");
-//     const mobileMenuRef = document.querySelector("[data-menu");
-
-//      menuBtnRef.addEventListener("click", () => {
-//         const expanded =
-//             menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-//         menuBtnRef.classList.toggle("is-open");
-//         menuBtnRef.setAttribute("aria-expanded", !expanded);
-
-//         mobileMenuRef.classList.toggle("is-open");
-//      });
-    
-//     closeBtnRef.addEventListener("click", () => {
-//         const expanded =
-//             menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-//         menuBtnRef.classList.toggle("is-open");
-//         menuBtnRef.setAttribute("aria-expanded", !expanded);
-
-//         mobileMenuRef.classList.toggle("is-open");
-//     });
-// })();
-
 (() => {
     const refs = {
       openModalBtn: document.querySelector('[data-menu-open]'),
