e165d398f89d09195bc1ea6b3788b8e8589549ba
Igor <Travis789@gmail.com>
2020-11-16 01:47:23 +0100

    header-almost-done(to do: menu-btn-mobile)

--- src/js/header-menu.js
+++ src/js/header-menu.js
@@ -1,14 +1,16 @@
 (() => {
-    const menuBtnRef = document.querySelector("[data-menu-button]");
-    const mobileMenuRef = document.querySelector("[data-menu]");
-  
-    menuBtnRef.addEventListener("click", () => {
-      const expanded =
-        menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-  
-      menuBtnRef.classList.toggle("is-open");
-      menuBtnRef.setAttribute("aria-expanded", !expanded);
-  
-      mobileMenuRef.classList.toggle("is-open");
-    });
-  })();
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+  const mobileLogoRef = document.querySelector("[data-menu-logo]");
+
+  menuBtnRef.addEventListener("click", () => {
+    const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+
+    menuBtnRef.classList.toggle("is-open");
+    mobileLogoRef.classList.toggle("white");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+    
+    mobileMenuRef.classList.toggle("is-open");
+  });
+})();
