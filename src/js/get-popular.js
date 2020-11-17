12e2c5758f5d43d26942f1659646afc580be0837
Anastasiia Yermakova <yermak1037@gmail.com>
2020-11-18 00:17:51 +0200

    Update get-popular.js

--- src/js/get-popular.js
+++ src/js/get-popular.js
@@ -1,40 +1,18 @@
-import popularTpl from '../templates/popular.hbs';
+import popularTpl from '../templates/movies.hbs';
+import FilmsApiService from './api-service';
+import refs from './refs';
 
-const API_KEY = '6914e86918040074e2fe382ba8e8cb5e';
-const BASE_URL = 'https://api.themoviedb.org/3/';
-
-const refs = {
-  popularContainer: document.querySelector('.js-film-container'),
-};
-
-function getPopular(page) {
-  return fetch(`${BASE_URL}trending/movie/day?api_key=${API_KEY}&page=${page}`)
-    .then(response => response.json())
-    .then(({ results }) => results)
-    .catch(error => {
-      console.log(error);
-    });
-}
+const filmsApiService = new FilmsApiService();
 
 function markupPopularMovies(movies) {
   refs.popularContainer.insertAdjacentHTML('beforeend', popularTpl(movies));
 }
 
-function getGenres() {
-  return fetch(`${BASE_URL}genre/movie/list?api_key=${API_KEY}`)
-    .then(response => response.json())
+function showPopular() {
+  return filmsApiService
+    .fetchFilms()
     .then(list => {
-      return list.genres;
-    })
-    .catch(error => {
-      console.log(error);
-    });
-}
-
-function showPopular(page) {
-  return getPopular(page)
-    .then(list => {
-      return getGenres().then(arr =>
+      return filmsApiService.fetchGenres().then(arr =>
         list.map(el => ({
           ...el,
           genre_ids: el.genre_ids.flatMap(num =>
@@ -43,8 +21,7 @@ function showPopular(page) {
         })),
       );
     })
-
     .then(markupPopularMovies);
 }
 
-showPopular(1);
+showPopular();
