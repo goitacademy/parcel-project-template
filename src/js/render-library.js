486b3ef10cf190d3bef2ae0d9b41022ee991630c
vertraut <onlinemovement@gmail.com>
2020-11-18 23:20:55 +0200

    switch page
    Сделал переключение между вкладками watched и queue. Также сделал запоминание последней активной вкладки. Она будет отображена, когда пользователь зайдет в библиотеку.

--- src/js/render-library.js
+++ src/js/render-library.js
@@ -11,6 +11,8 @@ const refs = {
   queueBtn: document.querySelector('.queue-btn'),
 };
 
+addEvents(); // устанавливаем слушатели
+
 //проверяем, есть ли запись в localstorage о последней используемой вкладке, если нет,
 //то считаем, что пользователь впервые перешел в библиотеку и устанавливаем последнюю вкладку как watched
 if (localStorage.getItem('last-tab') === null)
@@ -21,11 +23,33 @@ function setTab(page) {
   if (page === WATCHEDKEY) {
     refs.watchedBtn.classList.add('activBtn');
     refs.queueBtn.classList.remove('activBtn');
-  } else {
+  }
+  if (page === QUEUEKEY) {
     refs.queueBtn.classList.add('activBtn');
     refs.watchedBtn.classList.remove('activBtn');
   }
   getCollection(page).then(films => {
-    refs.movieList.insertAdjacentHTML('beforeend', popularTpl(films));
+    console.log(films.length);
+    if (films.length < 1) {
+      infoMsg();
+      return;
+    }
+    refs.movieList.innerHTML = popularTpl(films);
   });
+}
+
+function addEvents() {
+  refs.watchedBtn.addEventListener('click', () => {
+    setTab(WATCHEDKEY);
+    localStorage.setItem('last-tab', WATCHEDKEY);
+  });
+  refs.queueBtn.addEventListener('click', () => {
+    setTab(QUEUEKEY);
+    localStorage.setItem('last-tab', QUEUEKEY);
+  });
+}
+
+function infoMsg() {
+  refs.movieList.innerHTML =
+    '<li class="info-msg">Вы еще не добавили фильмы в свою библиотеку.</li>';
 }
