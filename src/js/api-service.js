0c6eae5301d2270fdd1ced56963e0bd46a9444a9
Roman Voznyy <roman.voznyy.52@gmail.com>
2020-11-18 11:47:48 +0200

    create popup v.1

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,8 +1,71 @@
-export function singleRequest(id) {
-	const API_KEY = '6914e86918040074e2fe382ba8e8cb5e';
-	const BASE_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = '6914e86918040074e2fe382ba8e8cb5e';
+const BASE_URL = 'https://api.themoviedb.org/3/';
 
-	return fetch(`${BASE_URL}movie/${id}?api_key=${API_KEY}`)
-		.then(response => response.json())
-		.catch(error => console.log(error));
+export default class FilmsApiService {
+	constructor() {
+		this.searchQuery = '';
+		this.page = 1;
+	}
+
+	fetchFilms(url) {
+		return fetch(
+			`${BASE_URL}${url}?api_key=${API_KEY}&page=${this.page}`,
+		)
+			.then(response => response.json())
+			.then(({ results }) => {
+				this.incrementPage();
+				return results;
+			});
+	}
+
+	fetchGenres() {
+		return fetch(`${BASE_URL}genre/movie/list?api_key=${API_KEY}`)
+			.then(response => response.json())
+			.then(list => {
+				return list.genres;
+			});
+	}
+
+	getGenres(url) {
+		return this.fetchFilms(url).then(list => {
+			return this.fetchGenres().then(arr =>
+				list.map(el => ({
+					...el,
+					genre_ids: el.genre_ids.flatMap(num =>
+						arr.filter(el => el.id === num),
+					),
+				})),
+			);
+		});
+	}
+
+	showFilmsResult(url) {
+		return this.getGenres(url).then(list => {
+			return list.map(el => ({
+				...el,
+				release_date: el.release_date.split('-')[0],
+			}));
+		});
+	}
+
+	incrementPage() {
+		this.page += 1;
+	}
+
+	resetPage() {
+		this.page = 1;
+	}
+
+	get query() {
+		return this.searchQuery;
+	}
+
+	set query(newQuery) {
+		this.searchQuery = newQuery;
+	}
+
+	singleRequest(id) {
+		return fetch(`${BASE_URL}movie/${id}?api_key=${API_KEY}`)
+			.then(response => response.json());
+	}
 }
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,27 +1,40 @@
-// import popupMovieTpl from '../templates/popup-movie.hbs';
-import { singleRequest } from "./api-service.js";
+import popupMovieTpl from '../templates/popup-movie.hbs';
+import FilmsApiService from "./api-service.js";
 
 const refs = {
+	body: document.querySelector("body"),
 	popup: document.querySelector(".js-popup"),
 	popupOverlay: document.querySelector(".popup-overlay"),
-	btnClose: document.querySelector(".js-close")
+	movieField: document.querySelector(".popup-movie"),
+	btnClose: document.querySelector(".js-close"),
+	filmsApiService: new FilmsApiService()
 }
 
 export function startPopup(id) {
-	singleRequest(id).then(data => console.log(data));
+	refs.filmsApiService.singleRequest(id)
+		.then(film => renderPage(film))
+		.catch(error => console.log(error));
 
+	refs.body.classList.add("popup-open");
 	refs.popup.classList.add("is-open");
 	refs.btnClose.addEventListener("click", closePopup);
 	refs.popupOverlay.addEventListener("click", closePopup);
 	window.addEventListener("keyup", closePopup);
 }
 
+function renderPage(film) {
+	const markup = popupMovieTpl(film);
+	refs.movieField.innerHTML = markup;
+}
+
 function closePopup({ type, key }) {
 	const clearPopup = () => {
+		refs.body.classList.remove("popup-open");
 		refs.popup.classList.remove("is-open");
 		refs.btnClose.removeEventListener("click", closePopup);
 		refs.popupOverlay.removeEventListener("click", closePopup);
 		window.removeEventListener("keyup", closePopup);
+		refs.movieField.innerHTML = "";
 	}
 
 	if (type === "keyup") {
