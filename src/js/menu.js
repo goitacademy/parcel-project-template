55ff8f68548eb6b8d0112bd0f66faa7cdae72ed6
Mariya Alekseenko <elg.mariya@gmail.com>
2020-11-18 13:26:44 +0100

    Update menu.js

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,16 +3,14 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
-    header: document.querySelector('header'),
+    header: document.querySelector('header')
   };
-  refs.header.addEventListener('click', toggleModal);
+  refs.header.addEventListener('click', toggleModal)
   function toggleModal(e) {
-    if (
-      e.target.closest('button') === refs.openModalBtn ||
-      e.target.closest('button') === refs.closeModalBtn ||
-      e.target.href
-    ) {
+    if(e.target.closest('button') === refs.openModalBtn || e.target.closest('button') === refs.closeModalBtn){
       refs.modal.classList.toggle('is-hidden');
+    } else if (e.target.href){
+      refs.modal.classList.add('is-hidden')
     }
   }
 })();
