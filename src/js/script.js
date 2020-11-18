1ab70d620b6dd18e4320beb6c9ae82301e58f149
YanaGuzienko <69dusha69@ukr.net>
2020-11-18 22:11:21 +0200

    добавляет джиес

---
+++ src/js/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+  $('.slider-container').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    asNavFor: '.slider-list',
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
+  });
+  $('.slider-list').slick({
+    infinite: false,
+
+    speed: 300,
+    focusOnSelect: true,
+    slidesToShow: 7,
+    slidesToScroll: 7,
+    asNavFor: '.slider-container',
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+});
