783d7cd44b5bec3bccc3dc0f482ea5fea1a973e8
annasabbagh <annamay2491@yahoo.com>
2020-11-18 11:48:06 +0100

    готовая версия

--- src/slider-js/slider.js
+++ src/slider-js/slider.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.slider').slick({
         dots: true,
-        // autoplay: true,
+        autoplay: true,
         pauseOnFocus: true,
         pauseOnHover: true,
         slidesToShow: 1,
@@ -9,51 +9,10 @@ $(document).ready(function(){
         waitForAnimate: false,
         focusOnSelect: true,
         fade: true,
-        
-       
+        appendDots: $('.thumbnail'),
+        responsive: [{
+            breakpoint: 320,
+            settings: {}
+        }]  
     });
 });
-
-// $(document).ready(function(){
-//     $('.slider-thumbnail').slick({
-//         dots: false,
-//         arrows: false,
-//         speed: 800,
-//         autoplay: false,
-//         pauseOnFocus: true,
-//         pauseOnHover: true,
-//         slidesToShow: 7,
-//         centerMode: true,
-//         waitForAnimate: false,
-//         focusOnSelect: true,
-//         asNavFor:".slider-active",
-//         responsive:[{
-//             breakpoint: 767,
-//         	settings: {
-//                 slidesToShow:3,
-//                 arrows: true,
-//             }
-//         }			
-//         ]
-//     });
-//     $('.slider-active').slick({
-//         dots: false,
-//         arrows: true,
-//         speed: 800,
-//         autoplay: true,
-//         autoplaySpeed: 3000,
-//         pauseOnFocus: true,
-//         pauseOnHover: true,
-//         waitForAnimate: false,
-//         fade: true,
-//         asNavFor:".slider-thumbnail",
-//         responsive:[{
-//             breakpoint: 767,
-//         	settings: {
-//                 slidesToShow: 1,
-//                 arrows: false,
-//             }
-//         }			
-//         ]
-//     });
-// });
