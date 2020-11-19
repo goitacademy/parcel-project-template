2a2e59b99da39d4c7212a2d833ed2571871e7d71
yankyrychenko <68908555+yankyrychenko@users.noreply.github.com>
2020-11-19 16:34:25 +0200

    btn-animaion

---
+++ src/js/btn-animation.js
@@ -0,0 +1,17 @@
+var animateButton = function(e) {
+
+  e.preventDefault;
+  //reset animation
+  e.target.classList.remove('animate');
+  
+  e.target.classList.add('animate');
+  setTimeout(function(){
+    e.target.classList.remove('animate');
+  },700);
+};
+
+var bubblyButtons = document.getElementsByClassName("bubbly-button");
+
+for (var i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener('click', animateButton, false);
+}
