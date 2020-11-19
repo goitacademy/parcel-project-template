005d68b7b45ca5b1595726beca3f7ce74daec4de
olena-kharchenko <harchenko.es1990@gmail.com>
2020-11-19 10:56:46 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import './sass/main.scss';
 import './js/get-popular';
 import './js/team';
 import './js/pagination';
+import './js/button-up';
---
+++ src/js/button-up.js
@@ -0,0 +1,8 @@
+import { addBackToTop } from 'vanilla-back-to-top';
+
+addBackToTop({
+  backgroundColor: '#ff6b08',
+  innerHTML:
+    '<svg viewBox="0 0 24 24"><path d="M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z"/></svg>',
+  textColor: '#ffffff',
+});
