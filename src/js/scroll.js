5c49e6849b7d45c7a2ab114c1c03900e05141125
Andrey Shemet <an-shem@ukr.net>
2020-11-19 19:45:29 +0200

    Удалил закоментированный код

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,18 +1,3 @@
-// $(document).ready(function () {
-//   $(window).scroll(function () {
-//     if ($(this).scrollTop() > 100) {
-//       $('.scrollup').fadeIn();
-//     } else {
-//       $('.scrollup').fadeOut();
-//     }
-//   });
-
-//   $('.scrollup').click(function () {
-//     $('html, body').animate({ scrollTop: 0 }, 600);
-//     return false;
-//   });
-// });
-
 $(window).scroll(function () {
   var height = $(window).scrollTop();
   if (height > 100) {
