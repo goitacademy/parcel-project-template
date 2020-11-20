5c0da21147b8aeea0ab2269664d3e3357f1c333f
Max M <maxminullin@gmail.com>
2020-11-20 19:12:15 +0200

    end

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
 import "./sass/main.scss";
+
+$(document).ready(function () {
+	$('.slid-list').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		asNavFor: '.slid-list-small',
+		mobileFirst: true,
+		responsive: [
+			{
+				breakpoint: 320,
+				settings: {
+					arrows: false,
+				}
+			},]
+
+
+
+	});
+
+	$('.slid-list-small').slick({
+
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		asNavFor: '.slid-list',
+		mobileFirst: true,
+
+		responsive: [
+			{
+				breakpoint: 767,
+				settings: {
+					arrows: false,
+					slidesToShow: 7,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 1314,
+				settings: {
+					arrows: false,
+					slidesToShow: 7,
+					slidesToScroll: 1
+				},
+
+			},
+		]
+	});
+});
