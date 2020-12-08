c6b275d49a36a1b2adc4ff78fe13a87860062994
Dmitriy Bazhenov <dmitriy.bazhenov@gmail.com>
2020-12-08 12:01:43 +0200

    Добавление id для секции products и добавление плавного скролла при нажатии на ссылки в навигации

--- src/js/custom.js
+++ src/js/custom.js
@@ -20,4 +20,18 @@ $(document).ready(function () {
         }
     });
 
+    const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
 });
