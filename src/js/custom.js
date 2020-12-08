48bdf380ed2fcd05801d24e9eac3eab640325dec
Dmitriy Bazhenov <dmitriy.bazhenov@gmail.com>
2020-12-08 19:34:36 +0200

    Задан новый delay для анимации кнопок products

--- src/js/custom.js
+++ src/js/custom.js
@@ -10,11 +10,11 @@ $(document).ready(function () {
         var w_height = $(window).height(); // Высота окна браузера
         var d_height = $(document).height(); // Высота всего документа
         var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
-        if (w_top + 750 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+        if (w_top + 830 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
             $('.how-it-made__number').css('opacity', '1');
             $('.how-it-made__number').spincrement({
                 thousandSeparator: "",
-                duration: 2300
+                duration: 2500
             });
 
             show = false;
