4e22f10f1b03e35d414457afeafc76f85c5d9550
Vlad Safin <djutsu17@gmail.com>
2020-12-17 22:39:16 +0200

    добавил модальное окно

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,15 +1,15 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector('[data-menu-open]'),
+    closeModalBtn: document.querySelector('[data-menu-close]'),
+    modal: document.querySelector('[data-menu]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    document.body.classList.toggle("modal-open");
-    refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle("menu-open");
+    refs.modal.classList.toggle('menu-hidden');
   }
 })();
