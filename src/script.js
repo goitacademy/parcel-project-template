3e5ef23a7ddb7ddc030242ff9ecf6a0180361203
Pavel Ivchenko <71591602+Chivapchichi2@users.noreply.github.com>
2020-12-18 22:44:42 +0200

    Поднастроил слайдер
    Добавил автоплей и задержки

--- src/script.js
+++ src/script.js
@@ -2,6 +2,12 @@ $(document).ready(function(){
   $('.slider').slick({
     arrows: false,
     dots:true,
-    
-  });
+    autoplay: true,
+    autoplaySpeed: 5000,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
+    speed: 1000,
+    easing: 'ease',
+      });
 });
