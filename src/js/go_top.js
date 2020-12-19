d7c426db9935b8408018b74fb626c4af3cefc011
Artem Smirnov <aratatem2013@gmail>
2020-12-19 15:53:03 +0200

    додає кнопку "на верх"

---
+++ src/js/go_top.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $('body').append('<a href="#" id="go-top" title="Вверх"></a>');
+});
+
+$(function() {
+ $.fn.scrollToTop = function() {
+  $(this).hide().removeAttr("href");
+  if ($(window).scrollTop() >= "250") $(this).fadeIn("slow")
+  var scrollDiv = $(this);
+  $(window).scroll(function() {
+   if ($(window).scrollTop() <= "250") $(scrollDiv).fadeOut("slow")
+   else $(scrollDiv).fadeIn("slow")
+  });
+  $(this).click(function() {
+   $("html, body").animate({scrollTop: 0}, "slow")
+  })
+ }
+});
+
+$(function() {
+ $("#go-top").scrollToTop();
+});
