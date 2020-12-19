0398967d0c2fd9ecef1176aa2acec8961502c778
Lesya Stulkivska <67697045+Legrito@users.noreply.github.com>
2020-12-19 10:57:53 +0200

    Отключает анимацию при скролле на мобильном

--- src/js/aos.js
+++ src/js/aos.js
@@ -18,5 +18,4 @@ AOS.init({
   once: false, // whether animation should happen only once - while scrolling down
   mirror: false, // whether elements should animate out while scrolling past them
   anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-
 });
