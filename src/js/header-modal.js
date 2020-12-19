649ad19fd3a18b794706b52e9cda530134eea424
Irina Selina <iras198308@gmail.com>
2020-12-19 15:56:54 +0200

    Делает модальное окно в хедере

---
+++ src/js/header-modal.js
@@ -0,0 +1,14 @@
+(() => {
+        const refs = {
+          openModalBtn: document.querySelector('[data-header-modal-open]'),
+          closeModalBtn: document.querySelector('[data-header-modal-close]'),
+          modal: document.querySelector('[data-header-modal]'),
+        };
+
+        refs.openModalBtn.addEventListener('click', toggleModal);
+        refs.closeModalBtn.addEventListener('click', toggleModal);
+
+        function toggleModal() {
+          refs.modal.classList.toggle('is-hidden');
+        }
+      })();
