c3dcd2155dc2d449dd8ec7d864ae53c4fd1396df
Dmytro Filius <mindertan@gmil.com>
2020-12-20 17:26:57 +0200

    add modal

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import "./sass/main.scss";
 import "./js/slider";
+import "./js/menu";
+import "./js/moblie-modal";
---
+++ src/js/menu.js
@@ -0,0 +1,29 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-menu-open]'),
+      closeModalBtn: document.querySelector('[data-menu-close]'),
+      modal: document.querySelector('[data-menu]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('is-open');
+    }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
