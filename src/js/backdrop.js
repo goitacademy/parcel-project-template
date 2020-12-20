98d7ef157156a4635d2a24c43f5e36969e0a0693
Pavel Ivchenko <71591602+Chivapchichi2@users.noreply.github.com>
2020-12-20 21:35:04 +0200

    Поправил модалки на кнопках и немного анимации

--- src/js/backdrop.js
+++ src/js/backdrop.js
@@ -4,8 +4,14 @@
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
+  const refs2 = {
+    openModalBtn: document.querySelector('[data-modal-open2]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
+  refs2.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
