8ffedcb7c1a9ac640e623408e0a37be5a438de9d
Lesya Stulkivska <67697045+Legrito@users.noreply.github.com>
2020-12-20 11:22:42 +0200

    Фиксит подписание кнопки обратной прокрутки

--- src/js/aos.js
+++ src/js/aos.js
@@ -1,6 +1,6 @@
 AOS.init({
   // Global settings:
-  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  disable: 'phone', // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
   startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
   initClassName: 'aos-init', // class applied after initialization
   animatedClassName: 'aos-animate', // class applied on animation
