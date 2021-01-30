2757fa1b47fba964e1987dc33d1a0e4508ad32f1
Mariia-Kovalchuk <72386697+Mariia-Kovalchuk@users.noreply.github.com>
2021-01-30 12:44:16 +0200

    header position+scrollUp button

--- src/js/menu.js
+++ src/js/menu.js
@@ -12,3 +12,20 @@
       mobileMenuRef.classList.toggle("is-open");
     });
   })();
+
+  // Скрипт кнопки Вверх
+
+var btn = $('#top__button');
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 500) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function (e) {
+  e.preventDefault();
+  $('html, body').animate({ scrollTop: 0 }, '300');
+});
