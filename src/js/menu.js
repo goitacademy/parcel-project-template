1d13a0b631bdbc2a9db44cf904412cf3dad7870c
Mariia-Kovalchuk <72386697+Mariia-Kovalchuk@users.noreply.github.com>
2021-01-30 19:58:28 +0200

    js мобильное меню

--- src/js/menu.js
+++ src/js/menu.js
@@ -13,6 +13,34 @@
     });
   })();
 
+// Бургер меню
+
+//   function menuСontainer(selector) {
+//     let menu = $(selector);
+//     let button = menu.find('.menu-button');
+//     let links = menu.find('.menu-item-link');
+//     let overlay = menu.find('.menu-container-overlay');
+    
+//     button.on('click', (e) => {
+//         e.preventDefault();
+//         toggleMenu();
+//     });
+    
+//     links.on('click', () => toggleMenu());
+//     overlay.on('click', () => toggleMenu());
+    
+//     function toggleMenu() {
+//         menu.toggleClass('is-open');
+//         if (menu.hasClass('is-open')) {
+//             $('body').css('overflow', 'hidden');
+//         } else {
+//             $('body').css('overflow', 'visible');
+//         }
+//     }
+// }
+
+// menuСontainer ('.menu-container');
+
   // Скрипт кнопки Вверх
 
 var btn = $('#top__button');
