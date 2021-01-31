5864641ea9cdc5348a7689ce37ceddfdfa103fda
Sergey Nosan <sergey.nosan@gmail.com>
2021-01-31 20:31:13 +0200

    Внес правки вхедер, хиро, модалку

--- src/js/menu-modal.js
+++ src/js/menu-modal.js
@@ -1,14 +1,18 @@
 $(document).ready(function () {
   $('.menu-button').click(function (event) {
-    $('.menu-button-icon,.menu-container').toggleClass('menu-open');
+    $('.menu-button-icon,.burger-menu').toggleClass('menu-open');
     $('body').toggleClass('lock');
   });
 
-  $('.menu-button').click(function (event) {
-    $('.modal-window').toggleClass('animate__fadeInRight');
+  $('.header-btn').click(function (event) {
+    $('.backdrop').removeClass('is-hidden');
+    $('.modal-window').removeClass('animate__bounceOutRight');
+    $('.modal-window').toggleClass('animate__bounceInRight');
   });
 
   $('.close-btn').click(function (event) {
+    $('.backdrop').toggleClass('is-hidden');
+    $('.modal-window').removeClass('animate__bounceInRight');
     $('.modal-window').toggleClass('animate__bounceOutRight');
   });
 });
