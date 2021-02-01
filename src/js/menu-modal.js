d680b210d11a2332e3969812283c603e95c1252a
Sergey Nosan <sergey.nosan@gmail.com>
2021-02-01 18:15:33 +0200

    Настроил JS для работы модалки

--- src/js/menu-modal.js
+++ src/js/menu-modal.js
@@ -5,14 +5,23 @@ $(document).ready(function () {
     $('.logo').toggleClass('inverse');
   });
 
-  $('.header-btn').click(function (event) {
-    $('body').toggleClass('lock');
+  $('.header-mobile-btn').click(function (event) {
+    $('.backdrop').removeClass('is-hidden');
+    $('.modal-window').removeClass('animate__bounceOutRight');
+    $('.modal-window').toggleClass('animate__bounceInRight');
   });
 
   $('.header-btn').click(function (event) {
     $('.backdrop').removeClass('is-hidden');
     $('.modal-window').removeClass('animate__bounceOutRight');
     $('.modal-window').toggleClass('animate__bounceInRight');
+    $('body').toggleClass('lock');
+  });
+
+  $('.close-btn-mobile').click(function (event) {
+    $('.backdrop').toggleClass('is-hidden');
+    $('.modal-window').removeClass('animate__bounceInRight');
+    $('.modal-window').toggleClass('animate__bounceOutRight');
   });
 
   $('.close-btn').click(function (event) {
