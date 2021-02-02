9a2c5f4423224fab64498bf401d2645ede0a8b07
Mariia-Kovalchuk <72386697+Mariia-Kovalchuk@users.noreply.github.com>
2021-02-02 18:43:34 +0200

    del extra js

--- src/js/modal.js
+++ src/js/modal.js
@@ -15,38 +15,3 @@
       refs.modal.classList.toggle('is-hidden');
     }
   })();
-
-
-  function setCursorPosition(pos, e) {
-    e.focus();
-    if (e.setSelectionRange) e.setSelectionRange(pos, pos);
-    else if (e.createTextRange) {
-      var range = e.createTextRange();
-      range.collapse(true);
-      range.moveEnd("character", pos);
-      range.moveStart("character", pos);
-      range.select()
-    }
-  }
-
-  function mask(e) {
-    //console.log('mask',e);
-    var matrix = this.placeholder,// .defaultValue
-        i = 0,
-        def = matrix.replace(/\D/g, ""),
-        val = this.value.replace(/\D/g, "");
-    def.length >= val.length && (val = def);
-    matrix = matrix.replace(/[_\d]/g, function(a) {
-      return val.charAt(i++) || "_"
-    });
-    this.value = matrix;
-    i = matrix.lastIndexOf(val.substr(-1));
-    i < matrix.length && matrix != this.placeholder ? i++ : i = matrix.indexOf("_");
-    setCursorPosition(i, this)
-  }
-  window.addEventListener("DOMContentLoaded", function() {
-    var input = document.querySelector("#online_phone");
-    input.addEventListener("input", mask, false);
-    input.focus();
-    setCursorPosition(3, input);
-  });
