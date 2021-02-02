11c465f7090b8d830ba282ce87fe7d9c6599460f
Anna <72921518+AnnaIzhovska@users.noreply.github.com>
2021-02-02 09:56:21 +0200

    Правки слайдера

--- src/slider.js
+++ src/slider.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
     $('.slider').slick();
 });
+
+$(document).ready(function () {
+    $('.slider2').slick();
+});
+
+
+$('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // arrows: false,
+    // fade: true,
+    asNavFor: '.slider2'
+  });
+  $('.slider2').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider',
+    // centerMode: true,
+    // focusOnSelect: true
+  });
