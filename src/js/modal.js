c092a79d5269cc7a8a5e3d585d477b1c3cb92d10
Mariia-Kovalchuk <72386697+Mariia-Kovalchuk@users.noreply.github.com>
2021-02-02 18:40:58 +0200

    открітие модального двумя кнопками

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,11 +1,13 @@
 (() => {
     const refs = {
       openModalBtn: document.querySelector('[data-modal-open]'),
+      openModalBtnTab: document.querySelector('[data-modal-open-tab]'),
       closeModalBtn: document.querySelector('[data-modal-close]'),
       modal: document.querySelector('[data-modal]'),
     };
   
     refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.openModalBtnTab.addEventListener('click', toggleModal);
     refs.closeModalBtn.addEventListener('click', toggleModal);
   
     function toggleModal() {
@@ -15,18 +17,36 @@
   })();
 
 
-  // (() => {
-  //   const refs = {
-  //     openModalBtn: document.querySelector('[data-modal-open2]'),
-  //     closeModalBtn: document.querySelector('[data-modal-close2]'),
-  //     modal: document.querySelector('[data-modal]'),
-  //   };
-  
-  //   refs.openModalBtn.addEventListener('click', toggleModal);
-  //   refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-  //   function toggleModal() {
-  //     document.body.classList.toggle("modal-open");
-  //     refs.modal.classList.toggle('is-hidden');
-  //   }
-  // })();
+  function setCursorPosition(pos, e) {
+    e.focus();
+    if (e.setSelectionRange) e.setSelectionRange(pos, pos);
+    else if (e.createTextRange) {
+      var range = e.createTextRange();
+      range.collapse(true);
+      range.moveEnd("character", pos);
+      range.moveStart("character", pos);
+      range.select()
+    }
+  }
+
+  function mask(e) {
+    //console.log('mask',e);
+    var matrix = this.placeholder,// .defaultValue
+        i = 0,
+        def = matrix.replace(/\D/g, ""),
+        val = this.value.replace(/\D/g, "");
+    def.length >= val.length && (val = def);
+    matrix = matrix.replace(/[_\d]/g, function(a) {
+      return val.charAt(i++) || "_"
+    });
+    this.value = matrix;
+    i = matrix.lastIndexOf(val.substr(-1));
+    i < matrix.length && matrix != this.placeholder ? i++ : i = matrix.indexOf("_");
+    setCursorPosition(i, this)
+  }
+  window.addEventListener("DOMContentLoaded", function() {
+    var input = document.querySelector("#online_phone");
+    input.addEventListener("input", mask, false);
+    input.focus();
+    setCursorPosition(3, input);
+  });
