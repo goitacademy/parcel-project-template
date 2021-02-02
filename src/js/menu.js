b6a44b2a79582ef8d39dc397fcde1193faa577c4
YuriiDolzhenko <74153673+YuriiDolzhenko@users.noreply.github.com>
2021-02-02 18:39:21 +0200

    63

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,20 +1,20 @@
 function burgerMenu(selector) {
-  let menu = $(selector);
-  let button = menu.find('.menu-button,.buy-now');
-  let links = menu.find('.menu-link');
-  let overlay = menu.find('.menu-overlay');
-  button.on('click', (e) => {
-      e.preventDefault();
-      toggleMenu();
-  });
-  links.on('click', () => toggleMenu());
-  overlay.on('click', () => toggleMenu());
-  function toggleMenu() {
-      menu.toggleClass('menu-active');
-      if (menu.hasClass('menu-active')) {
-          $('body').css('overflow', 'hidden');
-      } else {
-          $('body').css('overflow', 'visible');
-      }
-  }
+    let menu = $(selector);
+    let button = menu.find('.menu-button,.buy-now');
+    let links = menu.find('.menu-link');
+    let overlay = menu.find('.menu-overlay');
+    button.on('click', (e) => {
+        e.preventDefault();
+        toggleMenu();
+    });
+    links.on('click', () => toggleMenu());
+    overlay.on('click', () => toggleMenu());
+    function toggleMenu() {
+        menu.toggleClass('menu-active');
+        if (menu.hasClass('menu-active')) {
+            $('body').css('overflow', 'hidden');
+        } else {
+            $('body').css('overflow', 'visible');
+        }
+    }
 }
