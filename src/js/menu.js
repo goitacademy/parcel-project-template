0aac2828a881f2247b003d09231857c660d67e71
Dmitriy Snitko <dmitriy.snitko@gmail.com>
2021-02-03 21:39:05 +0200

    Исправления по меню
    Исправил позиционирование кнопки меню и добавил скрытие бекдропа при нажатии на ссылку в меню.

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,14 +2,21 @@
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
   const body = document.querySelector("[data-body]");
+  const menuLnkRef = document.querySelector("[data-menu-link]");
 
   menuBtnRef.addEventListener("click", () => {
     const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
     menuBtnRef.classList.toggle("is-open");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
 
+    mobileMenuRef.classList.toggle("is-open");
+    body.classList.toggle("scroll-lock");
+  });
+
+  menuLnkRef.addEventListener("click", () => {
+    menuBtnRef.classList.toggle("is-open");
     mobileMenuRef.classList.toggle("is-open");
     body.classList.toggle("scroll-lock");
   });
