8686aed910f1ac72695c1bd559a4255de56b9cb7
Mariia Hunko <masha.gunko96@gmail.com>
2021-02-16 11:43:04 +0200

    Добавляет скрипт для модалки и моб. меню + hero + спрайт картинки

---
+++ src/js/menu.js
@@ -0,0 +1,14 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleModal);
+  refs.closeMenuBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.menu.classList.toggle("is-open");
+  }
+})();
