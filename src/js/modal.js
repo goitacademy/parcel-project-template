cb3f11e511c8e1955047cf5d1de5b18f65e71f8f
Mariia Hunko <masha.gunko96@gmail.com>
2021-02-16 15:49:12 +0200

    Фиксит кнопку-бургер + JS

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,12 +1,15 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.forEach(function (btn) {
+    btn.addEventListener('click', toggleModal);
+  });
   refs.closeModalBtn.addEventListener('click', toggleModal);
+
 
   function toggleModal() {
     document.body.classList.toggle("modal-open");
