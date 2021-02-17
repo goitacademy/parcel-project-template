e53732a5bc1c4b866925ca004108eb501ba3434a
Kulyk Danil <danilka4412@gmail.com>
2021-02-17 10:36:26 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './sass/main.scss';
+(() => {
+    const refs = {
+      openMenuBtn: document.querySelector('[data-menu-open]'),
+      closeMenuBtn: document.querySelector('[data-menu-close]'),
+      menu: document.querySelector('[data-menu]'),
+    };
+
+    refs.openMenuBtn.addEventListener('click', toggleModal);
+    refs.closeMenuBtn.addEventListener('click', toggleModal);
+
+    function toggleModal() {
+      refs.menu.classList.toggle('is-menu-hidden');
+    }
+  })(); 
