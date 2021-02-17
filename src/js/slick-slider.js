2bf04cd0520d31aba2416a6911d9e215dbd8beb4
Vladislaw1 <vladzajcenko077@gmail.com>
2021-02-17 11:56:49 +0200

    comit

---
+++ src/js/slick-slider.js
@@ -0,0 +1,14 @@
+
+      $(document).ready(function () {
+        $('.slick-slider').slick({
+          dots: true,
+          infinite: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: false,
+          autoplaySpeed: 5000,
+
+          // prevArrow:<button type="button" class="slick-prev">Назад</button>,
+          // nextArrow:<button type="button" class="slick-next">Вперёд</button>,
+        })
+      })
