2cea5d86863ef209f44e1383cccc6901da36f63e
Vladislaw1 <vladzajcenko077@gmail.com>
2021-02-17 15:07:43 +0200

    готовый слайдер

--- src/js/slick-slider.js
+++ src/js/slick-slider.js
@@ -2,13 +2,13 @@
       $(document).ready(function () {
         $('.slick-slider').slick({
           dots: true,
-          infinite: false,
+          infinite: true,
           slidesToShow: 1,
           slidesToScroll: 1,
-          autoplay: false,
-          autoplaySpeed: 5000,
+          autoplay: true,
+          autoplaySpeed: 4000,
 
-          // prevArrow:<button type="button" class="slick-prev">Назад</button>,
-          // nextArrow:<button type="button" class="slick-next">Вперёд</button>,
+          prevArrow:'<button type="button" class="slick-prev">Назад</button>',
+          nextArrow:'<button type="button" class="slick-next">Вперёд</button>',
         })
       })
