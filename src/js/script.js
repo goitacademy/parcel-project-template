4f39831c5b3278637a23a6e09207618155a23a9f
Kulyk Danil <danilka4412@gmail.com>
2021-02-18 10:33:13 +0200

    1

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    arrows: true,
+    dots: true,
+    adaptiveHeight: true,
+  //    autoplay: true,
+  // autoplaySpeed: 2000,
+    infinite: true,
+    slidesToShow: 1,
+  });
+  
+});
