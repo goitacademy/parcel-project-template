c8d4c2aa2f358290048303f960a1966987617ff1
Vladymyr Rzhanov <v.rzhanov@gmail.com>
2021-02-20 01:09:49 +0200

    Добавил фавикон, пофиксил хедер, добавил слайдер

---
+++ src/js/scroll.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+	$("#menu").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+});
