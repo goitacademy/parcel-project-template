6bc5bf3e690c4b49b1de945cc107de3e7f29f1c0
ffructis <ffructis94@gmail.com>
2021-02-19 21:19:00 +0200

    добавляет часть разметки и стилей

---
+++ src/js/hero-modal.js
@@ -0,0 +1,16 @@
+  
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-inside-open]'),
+    openModalBtn: document.querySelector('[data-modal-outside-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
