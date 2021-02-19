846dcb66453e9e8d15be9766b49d87465becaf56
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-02-19 17:15:17 +0200

    Подключает скрипт slick слайдера.

---
+++ src/js/slider.js
@@ -0,0 +1,10 @@
+$('.reviews__list').slick({
+    dots: true,
+  infinite: false,
+  speed: 500,
+  slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    arrows: false
+});
