506b2fa51c6cc9d06073e792eda371a34bd0f1f2
Roman Bilousov <pomafess@gmail.com>
2021-02-19 11:24:18 +0200

    Create menu.js

---
+++ src/menu.js
@@ -0,0 +1,9 @@
+var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
+    var hamburgers = document.querySelectorAll(".hamburger");
+    if (hamburgers.length > 0) {
+      forEach(hamburgers, function(hamburger) {
+        hamburger.addEventListener("click", function() {
+          this.classList.toggle("is-active");
+        }, false);
+      });
+    }
