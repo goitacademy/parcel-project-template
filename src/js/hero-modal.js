fb4b9e25efe83e2de2a03e3704d94778666c7733
ffructis <ffructis94@gmail.com>
2021-02-20 12:39:52 +0200

    add html, scss, js for header modal

--- src/js/hero-modal.js
+++ src/js/hero-modal.js
@@ -1,10 +1,9 @@
   
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-inside-open]'),
-    openModalBtn: document.querySelector('[data-modal-outside-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector('[data-header-modal-open]'),
+    closeModalBtn: document.querySelector('[data-header-modal-close]'),
+    modal: document.querySelector('[data-header-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -14,3 +13,19 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-header-in-modal-open]'),
+    // closeModalBtn: document.querySelector('[data-header-modal-close]'),
+    modal: document.querySelector('[data-header-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+
