04da004e2b9198eb328f15308c87caff038d3f52
Vitaliy Myts <74954880+Zombik007@users.noreply.github.com>
2021-02-21 22:37:08 +0200

    Добавляет модальное окно для кнопки "Franchise"

--- src/js/contacts-map.js
+++ src/js/contacts-map.js
@@ -1,14 +1,22 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openMapBtn: document.querySelector('[data-modal-map-open]'),
+    closeMapBtn: document.querySelector('[data-modal-map-close]'),
+    modalMap: document.querySelector('[data-modal-map]'),
+    openFranchiseBtn: document.querySelector('[data-modal-franchise-open]'),
+    closeFranchiseBtn: document.querySelector('[data-modal-franchise-close]'),
+    modalFranchise: document.querySelector('[data-modal-franchise]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openMapBtn.addEventListener('click', toggleModalMap);
+  refs.closeMapBtn.addEventListener('click', toggleModalMap);
+  refs.openFranchiseBtn.addEventListener('click', toggleModalFranchise);
+  refs.closeFranchiseBtn.addEventListener('click', toggleModalFranchise);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('contacts-backdrop-hidden');
+  function toggleModalMap() {
+    refs.modalMap.classList.toggle('contacts-backdrop-hidden');
+  }
+  function toggleModalFranchise() {
+    refs.modalFranchise.classList.toggle('franchise-backdrop-hidden');
   }
 })();
