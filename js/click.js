477b7d2bab60dd1f14692de48a282611599efa39
Yuliya <jknoroz@gmail.com>
2021-02-22 00:17:41 +0200

    btn hover

---
+++ js/click.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('a[href^="#"], *[data-href^="#"]').on('click', function(e){
+        e.preventDefault();
+        var t = 1000;
+        var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
+        $('html,body').stop().animate({ scrollTop: $(d).offset().top }, t);
+    });
+});
