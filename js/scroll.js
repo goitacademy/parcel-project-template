5fc35206ce608a3d711b42e0ae03f2c4db4ec730
Yuliya <jknoroz@gmail.com>
2021-02-22 12:42:35 +0200

    top_button

---
+++ js/scroll.js
@@ -0,0 +1,6 @@
+$(document).on("click", "nav a", function(e) {
+    e.preventDefault();
+    var id  = $(this).attr('href');
+    var top = $(id).offset().top; // получаем координаты блока
+    $('body, html').animate({scrollTop: top}, 800); // плавно переходим к блоку
+});
---
+++ js/top.js
@@ -0,0 +1,21 @@
+$(function() {
+  // при нажатии на кнопку scrollup
+  $('.scrollup').click(function() {
+    // переместиться в верхнюю часть страницы
+    $("html, body").animate({
+      scrollTop:0
+    },700);
+  })
+})
+// при прокрутке окна (window)
+$(window).scroll(function() {
+  // если пользователь прокрутил страницу более чем на 200px
+  if ($(this).scrollTop()>200) {
+    // то сделать кнопку scrollup видимой
+    $('.scrollup').fadeIn();
+  }
+  // иначе скрыть кнопку scrollup
+  else {
+    $('.scrollup').fadeOut();
+  }
+});
