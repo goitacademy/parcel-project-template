4139d9ae8396baccc8da02c89bd3e02deec33df7
Yuliya <jknoroz@gmail.com>
2021-02-22 14:08:26 +0200

    стрелочка

--- js/scroll.js
+++ js/scroll.js
@@ -2,5 +2,5 @@ $(document).on("click", "nav a", function(e) {
     e.preventDefault();
     var id  = $(this).attr('href');
     var top = $(id).offset().top; // получаем координаты блока
-    $('body, html').animate({scrollTop: top}, 800); // плавно переходим к блоку
+    $('body, html').animate({scrollTop: top}, 700); // плавно переходим к блоку
 });
