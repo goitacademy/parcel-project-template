bbe28ca5db37db2be6b5a7968b2f15712ca82633
Bogdan Dyachenko <bogdandicenko315@gmail.com>
2021-02-22 23:45:00 +0200

    ready-slider

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './sass/main.scss';
+
+$(document).ready(function () {
+  $('.reviews-list').slick({
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow:1,
+  });
+});
