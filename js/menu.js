920a80e55c1bbdd814f87b3de15f61f51ed2fa5f
Maryna-Skrypnyk <marina.skrypnyk@gmail.com>
2021-02-22 15:03:26 +0200

    Додає в menu.js заборону прокрутки сайту при відкритому мобільному меню

--- js/menu.js
+++ js/menu.js
@@ -9,6 +9,7 @@
     menuBtnRef.classList.toggle("is-open");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
 
+    document.body.classList.toggle("menu-open");
     mobileMenuRef.classList.toggle("is-open");
   });
 })();
