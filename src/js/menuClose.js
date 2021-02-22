644e87575aba524df1ac0a8db38a1a529168da54
Sergii Bogush <sergbogush@gmail.com>
2021-02-22 17:43:33 +0200

    Добавляет outline color

---
+++ src/js/menuClose.js
@@ -0,0 +1,21 @@
+const btnMenu = document.querySelector('.menu__button');
+const menu = document.querySelector('.menu__container');
+const toggleMenu = function() {
+    menu.classList.toggle('is-open');
+}
+
+btnMenu.addEventListener('click', function(e) {
+    e.stopPropagation();
+    toggleMenu();
+});
+
+document.addEventListener('click', function(e) {
+    const target = e.target;
+    const its_menu = target == menu || menu.contains(target);
+    const its_btnMenu = target == btnMenu;
+    const menu_is_active = menu.classList.contains('is-open');
+    
+    if (!its_menu && !its_btnMenu && menu_is_active) {
+        toggleMenu();
+    }
+});
