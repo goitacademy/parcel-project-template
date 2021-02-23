b3516159c323d7fba6feda944e9f7b5d983b9ccf
Valeriia980 <75097633+Valeriia980@users.noreply.github.com>
2021-02-24 00:13:17 +0100

    1
    1

---
+++ src/scroll.js
@@ -0,0 +1,31 @@
+
+$('.navigation a').on('click', function (e) {
+  if (this.hash !== '') {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800,
+    );
+  }
+});
+
+
+$('.button-to-up').on('click', function (e) {
+  if (this.hash !== '') {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      800,
+    );
+  }
+});
