f9129fc4508e254e593b0e2750d9fa400aba954e
Sergii Bogush <sergbogush@gmail.com>
2021-02-24 11:39:51 +0200

    Fixed

--- src/js/backdrop.js
+++ src/js/backdrop.js
@@ -5,10 +5,11 @@
       modal: document.querySelector('[data-backdrop]'),
     };
   
-    /*refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);*/
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
   
     function toggleModal() {
-      refs.modal.classList.toggle('is-hidden-backdrop');
+      document.body.classList.toggle('modal-open');
+        refs.modal.classList.toggle('is-hidden');
     }
   })();
--- src/js/modalMap.js
+++ src/js/modalMap.js
@@ -1,14 +1,16 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector('[data-map-open]'),
-      closeModalBtn: document.querySelector('[data-map-close]'),
-      modal: document.querySelector('[data-map]'),
-    };
-  
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-    function toggleModal() {
+  const refs = {
+    openModalBtn: document.querySelector('[data-map-open]'),
+    closeModalBtn: document.querySelector('[data-map-close]'),
+    modal: document.querySelector('[data-map]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+   
+      document.body.classList.toggle('modal-open');
       refs.modal.classList.toggle('is-hidden');
-    }
-  })();
+  }
+})();
