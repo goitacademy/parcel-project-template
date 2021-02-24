a03da25807b6d57136a64d6f6b5cd9abca35be73
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-02-24 17:34:45 +0200

    Update menu.js

--- src/js/menu.js
+++ src/js/menu.js
@@ -11,34 +11,17 @@
       menuBtnRef.setAttribute("aria-expanded", !expanded);
   
       mobileMenuRef.classList.toggle("is-open");
-    });
-  
-  // $('html, body').animate(
-  //   {
-  //     scrollTop: $($(this).attr('#')).offset().top,
-  //   },
-  //   1000,
-  //   'linear',
-  // );
-
-  $('a[href*="#menu"]').on('click', function (e) {
-  e.preventDefault();
-
-  const mobileMenuRef = document.querySelector('[data2-menu]');
-  const menuBtnRef = document.querySelector('[data2-menu-button]');
-  mobileMenuRef.classList.toggle('is-open');
-  menuBtnRef.classList.toggle('is-open');
-
-  $('html, body').animate(
-    {
-      scrollTop: $($(this).attr('href')).offset().top,
-    },
-    10000,
-    'linear',
-  );
+        
+        modal.classList.remove('is-hidden');
+     document.body.classList.toggle('modal-open');
+        
+        $('.menu__backdrop, .menu__container').click(function () {
+    mobileMenuRef.classList.remove('is-open'),
+    menuBtnRef.classList.remove('is-open'),
+    document.body.classList.remove('modal-open'),
+   modal.classList.toggle('is-hidden'); 
 });
-    
-  })();
+    });
 
 // (() => {
 //   const menuBtnRef = document.querySelector("[data-menu-button]");
@@ -54,12 +37,7 @@
 //     modal.classList.remove('is-hidden');//показывает бекдроп
 //     document.body.classList.toggle('modal-open');
 
-//     // $('.menu__backdrop, .menu__container').click(function () {
-//     //     mobileMenuRef.classList.remove('is-open'),
-//     //     menuBtnRef.classList.remove('is-open'),
-//     //     document.body.classList.remove('modal-open'),
-//     //     modal.classList.toggle('is-hidden'); 
-//     // });
+//     // 
 
 //   })()
 // };
