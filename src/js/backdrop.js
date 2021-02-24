013b27e970afa034caddac160d4c83f20f175e0b
Sergii Bogush <sergbogush@gmail.com>
2021-02-24 16:30:44 +0200

    Added .noselect, changed id="comment"

--- src/js/backdrop.js
+++ src/js/backdrop.js
@@ -1,15 +1,15 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector('[data-open-backdrop]'),
-      closeModalBtn: document.querySelector('[data-close-backdrop]'),
-      modal: document.querySelector('[data-backdrop]'),
-    };
-  
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-    function toggleModal() {
-      document.body.classList.toggle('modal-open');
-        refs.modal.classList.toggle('is-hidden');
-    }
-  })();
+  const refs = {
+    openModalBtn: document.querySelector('[data-open-backdrop]'),
+    closeModalBtn: document.querySelector('[data-close-backdrop]'),
+    modal: document.querySelector('[data-backdrop]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle('modal-open');
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
