585cab7e2f56d5fceba2dae1ef537a2045710550
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-02-26 09:09:39 +0200

    Update menu.js

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,3 +1,23 @@
+//  (() => {
+//   const menuBtnRef = document.querySelector("[data-menu-button]");
+//   const mobileMenuRef = document.querySelector("[data-menu]");
+//   const modal = document.querySelector("[data-backdrop]");
+//   menuBtnRef.addEventListener("click", () => {
+//     const expanded =
+//       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+//     menuBtnRef.classList.toggle("is-open");
+//     menuBtnRef.setAttribute("aria-expanded", !expanded);
+//     mobileMenuRef.classList.toggle('is-open');//открывает меню
+//     modal.classList.remove('is-hidden');//показывает бекдроп
+//     document.body.classList.toggle('modal-open');
+
+//      $('.menu__backdrop, .menu__container').click(function () {
+//       mobileMenuRef.classList.remove('is-open'),
+//         menuBtnRef.classList.remove('is-open');
+//     });
+   
+//   });
+// })();
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
