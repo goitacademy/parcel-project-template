cfac6319c474a6bcfef811b2c9dfa3330afffe00
Kovalenko Makcim <max.kow@yandex.ru>
2021-02-27 22:07:54 +0300

    header
    Выполнено:
    1) добавлены стили для mobile
    2) добавлены стили tablet
    3) добавлены стили для decktop
    4) оформлено меню в мобиле и таблетке

---
+++ src/js/menuMobile.js
@@ -0,0 +1,15 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-openMenu]'),
+    closeModalBtn: document.querySelector('[data-modal-closeMenu]'),
+    modal: document.querySelector('[data-modal-menu]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle("modal-open");
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
