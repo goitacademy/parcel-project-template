bbea7d0c2c4efa63c4f17d525b7c1aeff0d12e3d
Ivan Ozerin <mrOzerin@gmail.com>
2021-03-05 11:57:06 +0300

    Внес все свои изменения
    Фавикон хэдер эбаут и гаранти

--- modal.js
+++ modal.js
@@ -1,8 +1,11 @@
 const mobileMenuBtn = document.querySelector('#mobile-menu-btn');
 const CertificateBtn = document.querySelector('#certificate-btn');
+const ConsultationBtn = document.querySelector('.consultation-button');
 
-const CertificateModal = document.querySelector('#certificate-modal');
 const mobileMenu = document.querySelector('#mobile-menu');
+const CertificateModal = document.querySelector('#certificate-modal');
+const ConsultationModal = document.querySelector('#consultation-modal');
+
 
 const modalCloseBtns = document.querySelectorAll('.modal-close-btn');
 
@@ -15,58 +18,58 @@ const BODY_SCROLL_DISABLE_CLASS = 'body-scroll-disable';
 enableCloseModalOnBgdClick();
 hideModalOnMobileMenuElementsClick();
 
-const modals = [mobileMenu, CertificateModal];
-const buttons = [mobileMenuBtn, CertificateBtn];
+const modals = [mobileMenu, CertificateModal, ConsultationModal];
+const buttons = [mobileMenuBtn, CertificateBtn, ConsultationBtn];
 
 buttons.forEach((btn, index) => {
-	const projectModal = modals[index];
+    const projectModal = modals[index];
 
-	if (btn) {
-		btn.addEventListener('click', (event) => {
-			event.preventDefault();
-			projectModal.classList.add(MODAL_ACTIVE_CLASS);
+    if (btn) {
+        btn.addEventListener('click', (event) => {
+            event.preventDefault();
+            projectModal.classList.add(MODAL_ACTIVE_CLASS);
 
-			document.body.classList.add(BODY_SCROLL_DISABLE_CLASS);
-		})
-	}
+            document.body.classList.add(BODY_SCROLL_DISABLE_CLASS);
+        })
+    }
 });
 
 
 modalCloseBtns.forEach(btn => {
-	btn.addEventListener('click', hideModal);
+    btn.addEventListener('click', hideModal);
 })
 
 function enableCloseModalOnBgdClick() {
-	if (modalContainers.length) {
-		modalContainers.forEach(container => {
-			container.addEventListener('click', event => event.stopPropagation());
-		});
-	}
+    if (modalContainers.length) {
+        modalContainers.forEach( container => {
+            container.addEventListener('click', event => event.stopPropagation());
+        });
+    }
 
-	if (modalsWrappers.length) {
-		modalsWrappers.forEach(container => {
-			container.addEventListener('click', hideModal);
-		});
-	}
+    if (modalsWrappers.length) {
+        modalsWrappers.forEach( container => {
+            container.addEventListener('click', hideModal);
+        });
+    }
 }
 
 
 function hideModal() {
-	const modalToClose = document.querySelector(`.${MODAL_ACTIVE_CLASS}`);
+    const modalToClose = document.querySelector(`.${MODAL_ACTIVE_CLASS}`);
 
-	if (modalToClose) {
-		modalToClose.classList.remove(MODAL_ACTIVE_CLASS);
-		document.body.classList.remove(BODY_SCROLL_DISABLE_CLASS);
-	}
+    if (modalToClose) {
+        modalToClose.classList.remove(MODAL_ACTIVE_CLASS);
+        document.body.classList.remove(BODY_SCROLL_DISABLE_CLASS);    
+    }
 }
 
 function hideModalOnMobileMenuElementsClick() {
-	const MOBILE_MENU_ITEM_CLOSE_DELAY = 150;
-	const menuElements = document.querySelectorAll('.mobile-menu-item');
+    const MOBILE_MENU_ITEM_CLOSE_DELAY = 150;
+    const menuElements = document.querySelectorAll('.mobile-menu-item');
 
-	if (menuElements.length) {
-		menuElements.forEach(container => {
-			container.addEventListener('click', () => setTimeout(hideModal, MOBILE_MENU_ITEM_CLOSE_DELAY));
-		});
-	}
+    if (menuElements.length) {
+        menuElements.forEach( container => {
+            container.addEventListener('click', () => setTimeout(hideModal, MOBILE_MENU_ITEM_CLOSE_DELAY));
+        });
+    }
 }
