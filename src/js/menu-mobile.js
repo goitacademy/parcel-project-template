f945d307cd7b0b90b4361c10fcf7a2683214a435
PaulPugovkin <coastalrhodes@gmail.com>
2021-03-05 12:39:47 +0300

    update

--- src/js/menu-mobile.js
+++ src/js/menu-mobile.js
@@ -6,7 +6,7 @@
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  // refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     document.body.classList.toggle("modal-open");
