3c1a74bbfe0d8930dac9774c2c19b0ca16949e7c
Winnbor <59263951+Winnbor@users.noreply.github.com>
2021-03-07 00:02:08 +0300

    Add animation fixes and svg animation

---
+++ src/js/svg-animation.js
@@ -0,0 +1,16 @@
+new Vivus('adv-svg', {
+  type: 'sync',
+  duration: 125,
+  animTimingFunction: Vivus.EASE,
+});
+
+new Vivus('adv-svg2', {
+  type: 'sync',
+  duration: 125,
+  animTimingFunction: Vivus.EASE,
+});
+new Vivus('adv-svg3', {
+  type: 'sync',
+  duration: 125,
+  animTimingFunction: Vivus.EASE,
+});
