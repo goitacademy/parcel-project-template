2d190ef6fa92d50996a4ef7c858abb3f3ade2d57
miLLlog <milllog@mail.ru>
2021-03-09 21:47:27 +0300

    header-mobile

---
+++ src/js/menu.js
@@ -0,0 +1,10 @@
+(() => {
+  const 
+    menuBtnRef = document.querySelector("[data-menu-button]");
+    mobileMenuRef = document.querySelector("[data-menu]");
+  menuBtnRef.addEventListener('click', () => {
+    menuBtnRef.classList.toggle('is-open');
+
+    mobileMenuRef.classList.toggle('is-open');
+  });
+  })();
