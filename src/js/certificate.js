064b5940a400a4101f399516506ea117653713b0
Winnbor <59263951+Winnbor@users.noreply.github.com>
2021-03-09 21:14:27 +0300

    Add script for bg clicks to close modals

--- src/js/certificate.js
+++ src/js/certificate.js
@@ -1,15 +1,32 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-certificate-open]'),
-    closeModalBtn: document.querySelector('[data-certificate-close]'),
-    modal: document.querySelector('[data-certificate]'),
-  };
+const modalsWrappers = document.querySelectorAll('.modal-cert-bgd');
+const modalContainers = document.querySelectorAll('.modal-cert-content');
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+const refs = {
+  openModalBtn: document.querySelector('[data-certificate-open]'),
+  closeModalBtn: document.querySelector('[data-certificate-close]'),
+  modal: document.querySelector('[data-certificate]'),
+};
 
-    function toggleModal() {
-      document.body.classList.toggle("certificate-open");
-    refs.modal.classList.toggle('is-hidden');
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
+
+function toggleModal() {
+  document.body.classList.toggle('certificate-open');
+  refs.modal.classList.toggle('is-hidden');
+}
+
+enableCloseModalOnBgdClick();
+
+function enableCloseModalOnBgdClick() {
+  if (modalContainers.length) {
+    modalContainers.forEach(container => {
+      container.addEventListener('click', event => event.stopPropagation());
+    });
   }
-})();
+
+  if (modalsWrappers.length) {
+    modalsWrappers.forEach(container => {
+      container.addEventListener('click', toggleModal);
+    });
+  }
+}
--- src/js/links-close.js
+++ src/js/links-close.js
@@ -1,12 +1,18 @@
 const links = document.querySelectorAll('[data-modal-link]');
 
+const modalsWrappers = document.querySelectorAll('.modal-area-bgd');
+const modalContainers = document.querySelectorAll('.modal-area-content');
+
 const MODAL_HIDDEN_CLASS = 'is-hidden';
 const BODY_SCROLL_DISABLE_CLASS = 'modal-open';
 const BURGER_VISIBLE = 'is-active';
 
+enableCloseModalOnBgdClick();
+
 links.forEach(link => {
   link.addEventListener('click', hideModal);
 });
+
 function hideModal() {
   const modalToClose = document.querySelector('.backdrop-mobile');
   const burger = document.querySelector('.hamburger');
@@ -14,4 +20,18 @@ function hideModal() {
   modalToClose.classList.add(MODAL_HIDDEN_CLASS);
   burger.classList.remove(BURGER_VISIBLE);
   document.body.classList.remove(BODY_SCROLL_DISABLE_CLASS);
+}
+
+function enableCloseModalOnBgdClick() {
+  if (modalContainers.length) {
+    modalContainers.forEach(container => {
+      container.addEventListener('click', event => event.stopPropagation());
+    });
+  }
+
+  if (modalsWrappers.length) {
+    modalsWrappers.forEach(container => {
+      container.addEventListener('click', hideModal);
+    });
+  }
 }
