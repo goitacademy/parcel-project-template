4f7a0c95ef4b6f10afb736f5448ca5bb305cbfab
Наталья <nataliiayefymova@gmail.com>
2021-03-11 16:22:36 +0200

    add js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('.slider').slick(
+        {
+            arrows: false,
+            dots: true,
+            speed: 500,
+            infinite: false,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            pauseOnDotsHover: true,
+            // appendDots:$('.sviper-dots'),
+        }
+    );
+});
