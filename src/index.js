637dcffbf199c6a260a3b799f675e4474661283e
Kateryna-dzhy <k.dzhyviriaha@cleanpoint.ua>
2021-03-12 06:07:46 +0200

    исправляет путь к свг

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import './sass/main.scss';
+//<!-- Скрипты для слайдера -->
+    //<!--<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    //<script src="./js/slick.min.js"></script>
+    //<script src="./js/script.js"></script>-->
