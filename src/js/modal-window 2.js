1b6de1def20cda69dc112b095dc31b58bf0bf730
AndreyKeleberda <74091304+AndreyKeleberda@users.noreply.github.com>
2021-03-13 23:42:55 +0200

    добавил основную разметку и стили галереи

---
+++ src/js/modal-window 2.js
@@ -0,0 +1,15 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle("modal-open");
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
