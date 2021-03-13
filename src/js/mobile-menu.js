d621c38a3d162c32599c71a043e2ff1549a9417b
Kirill-Filonchuk <76174969+Kirill-Filonchuk@users.noreply.github.com>
2021-03-13 21:25:21 +0200

    add js (#10)

---
+++ src/js/mobile-menu.js
@@ -0,0 +1,13 @@
+(() => {
+  const menuBtnRef = document.querySelector("[data-menu-button]");
+  const mobileMenuRef = document.querySelector("[data-menu]");
+  const mobileBtnClose = document.querySelector("[data-menu-close]");
+
+  menuBtnRef.addEventListener("click", () => {
+    mobileMenuRef.classList.toggle("is-open");
+  })
+
+  mobileBtnClose.addEventListener('click', () => {
+    mobileMenuRef.classList.toggle("is-open");
+  });
+})()
