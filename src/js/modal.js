0d72d7b2e4ebdf9585d272311d4d70637d83404b
Sholom Konstantin <sholom1998@icloud.com>
2021-03-14 23:54:18 +0100

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,5 +1,17 @@
 (() => {
   const refs2 = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+  refs2.openModalBtn.addEventListener("click", toggleModal);
+  refs2.closeModalBtn.addEventListener("click", toggleModal);
+  function toggleModal () {
+    refs2.modal.classList.toggle("is-hidden");
+  }
+})();
+(() => {
+  const refs2 = {
     openModalBtn: document.querySelector("[data-modal-open2]"),
     closeModalBtn: document.querySelector("[data-modal-close2]"),
     modal: document.querySelector("[data-modal2]"),
