e58795582144e8d4abfffae3600f3f1c305af49b
Sholom Konstantin <sholom1998@icloud.com>
2021-03-14 23:47:04 +0100

    рабочие кнопки (карта без стилей)

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,32 +1,26 @@
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-    openModalBtn2: document.querySelector("[data-modal-open2]"),
-    closeModalBtn2: document.querySelector("[data-modal-close2]"),
-    modal2: document.querySelector("[data-modal2]"),
-    openModalBtn3: document.querySelector("[data-modal-open3]"),
-    closeModalBtn3: document.querySelector("[data-modal-close3]"),
-    modal3: document.querySelector("[data-modal3]"),
+  const refs2 = {
+    openModalBtn: document.querySelector("[data-modal-open2]"),
+    closeModalBtn: document.querySelector("[data-modal-close2]"),
+    modal: document.querySelector("[data-modal2]"),
   };
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-  refs.openModalBtn2.addEventListener("click", toggleModal2);
-  refs.closeModalBtn2.addEventListener("click", toggleModal2);
-   refs.openModalBtn3.addEventListener("click", toggleModal3);
-  refs.closeModalBtn3.addEventListener("click", toggleModal3);
-  
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+  refs2.openModalBtn.addEventListener("click", toggleModal2);
+  refs2.closeModalBtn.addEventListener("click", toggleModal2);
+  function toggleModal2 () {
+    refs2.modal.classList.toggle("is-hidden");
   }
-    function toggleModal2() {
-      refs.modal.classList.toggle("is-hidden");
+})();
+(() => {
+  const refs3 = {
+    openModalBtn: document.querySelector("[data-modal-open3]"),
+    closeModalBtn: document.querySelector("[data-modal-close3]"),
+    modal: document.querySelector("[data-modal3]"),
+  };
+  refs3.openModalBtn.addEventListener("click", toggleModal3);
+  refs3.closeModalBtn.addEventListener("click", toggleModal3);
+  function toggleModal3 () {
+    refs3.modal.classList.toggle("is-hidden");
   }
-  function toggleModal3() {
-      refs.modal.classList.toggle("is-hidden");
-  }
-    
 })();
 
 
