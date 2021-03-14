e50ad8bd998ec5d5083269d5e8bbafaa591887e3
Kateryna-dzhy <k.dzhyviriaha@cleanpoint.ua>
2021-03-14 22:52:53 +0200

    подключены анкетные модалки

--- src/js/modal.js
+++ src/js/modal.js
@@ -6,11 +6,16 @@
     openModalBtn2: document.querySelector("[data-modal-open2]"),
     closeModalBtn2: document.querySelector("[data-modal-close2]"),
     modal2: document.querySelector("[data-modal2]"),
+    openModalBtn3: document.querySelector("[data-modal-open3]"),
+    closeModalBtn3: document.querySelector("[data-modal-close3]"),
+    modal3: document.querySelector("[data-modal3]"),
   };
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
   refs.openModalBtn2.addEventListener("click", toggleModal2);
   refs.closeModalBtn2.addEventListener("click", toggleModal2);
+   refs.openModalBtn3.addEventListener("click", toggleModal3);
+  refs.closeModalBtn3.addEventListener("click", toggleModal3);
   
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
@@ -18,12 +23,11 @@
     function toggleModal2() {
       refs.modal.classList.toggle("is-hidden");
   }
+  function toggleModal3() {
+      refs.modal.classList.toggle("is-hidden");
+  }
     
 })();
 
 
-  $(document).ready(function() {
-    $(".btn").click(function () {
-      $("#myModal").modal('show');
-    });
-  });
+ 
