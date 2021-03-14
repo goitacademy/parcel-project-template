b5efde3e79e927a0225c94830217a742ee8587fc
Kateryna Burlaka <71180234+CapMcCat@users.noreply.github.com>
2021-03-14 23:08:26 +0200

    добавляет стилизацию слайдера для десктопа

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,11 +1,11 @@
 
-// $(document).ready(function(){
-//   $('.slider').slick({
-//     dots: true,
-//   infinite: true,
-//   autoplay: true,
-//   autoplaySpeed: 5000,
-//   cssEase: 'linear'
-//   });
-// });
+$(document).ready(function(){
+  $('.slider').slick({
+     dots: true,
+infinite: true,
+   autoplay: true,
+   autoplaySpeed: 5000,
+  cssEase: 'linear'
+   });
+ });
 	
