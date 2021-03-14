cf0cbb34b88944d25d4a7ede5af2ef1da5633aa6
Mark <52928930+Mark4Titan@users.noreply.github.com>
2021-03-14 13:20:59 +0200

    завершив верстати модалку

--- src/index.js
+++ src/index.js
@@ -4,36 +4,37 @@ import './sass/main.scss';
     const menuBtnRef = document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
     const mobileBtnClose = document.querySelector("[data-menu-close]");
+
+
   
-    const Btn1 = document.querySelector("[bat-0]");
-    const Btn2 = document.querySelector("[bat-0]");
-    const Btn3 = document.querySelector("[bat-0]");
-    const Btn4 = document.querySelector("[bat-0]");
-    const Btn5 = document.querySelector("[bat-0]");
-    const Btn6 = document.querySelector("[bat-0]");
-   
+    const BtnClose = document.querySelector("[bat-close]");
+    const MenuRef = document.querySelector("[bat-ref]");
   
     menuBtnRef.addEventListener("click", () => {
       mobileMenuRef.classList.toggle("is-open");
 
-      Btn1.classList.toggle("bat-0");
-      Btn2.classList.toggle("bat-0");
-      Btn3.classList.toggle("bat-0");
-      Btn4.classList.toggle("bat-0");
-      Btn5.classList.toggle("bat-0");
-      Btn6.classList.toggle("bat-0");
-          })
-
-    mobileBtnClose.addEventListener('click', () => {
+      BtnClose.classList.toggle("bat-close");
+      MenuRef.classList.toggle("bat-ref");
+    })  
+   
+  
+  
+    MenuRef.addEventListener('click', () => {
       mobileMenuRef.classList.toggle("is-open");
-      
-      Btn1.classList.toggle("bat-0");
-      Btn2.classList.toggle("bat-0");
-      Btn3.classList.toggle("bat-0");
-      Btn4.classList.toggle("bat-0");
-      Btn5.classList.toggle("bat-0");
-      Btn6.classList.toggle("bat-0");
-     
+
+      BtnClose.classList.toggle("bat-close");
+      MenuRef.classList.toggle("bat-ref");  
       
     });
-  })()
+  
+  
+    mobileBtnClose.addEventListener('click', () => {
+      mobileMenuRef.classList.toggle("is-open");
+
+      BtnClose.classList.toggle("bat-close");
+      MenuRef.classList.toggle("bat-ref");  
+      
+    });
+  
+  
+})()
