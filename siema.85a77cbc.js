8e6bfcaf2e3eaac7885f9bc89f9ba00890345865
abakarovmaks <abakarovmaks@gmail.com>
2021-03-15 21:22:01 +0200

    Updates

--- siema.85a77cbc.js
+++ siema.85a77cbc.js
@@ -179,7 +179,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64304" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51656" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
