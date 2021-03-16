8611dc8d6e09257ffa9733a1b0468f7fea4a06e0
Kirill Filonchuk <kir.realtor@gmail.com>
2021-03-16 23:09:35 +0200

    morebetter-plus

---
+++ src/js/scrollup.js
@@ -0,0 +1,28 @@
+window.onload = function() { // после загрузки страницы
+
+	var scrollUp = document.getElementById('scrollup'); // найти элемент
+
+	scrollUp.onmouseover = function() { // добавить прозрачность
+		scrollUp.style.opacity=0.3;
+		scrollUp.style.filter  = 'alpha(opacity=30)';
+	};
+
+	scrollUp.onmouseout = function() { //убрать прозрачность
+		scrollUp.style.opacity = 0.5;
+		scrollUp.style.filter  = 'alpha(opacity=40)';
+	};
+
+	scrollUp.onclick = function() { //обработка клика
+		window.scrollTo(0,0);
+	};
+
+// show button
+
+	window.onscroll = function () { // при скролле показывать и прятать блок
+		if ( window.pageYOffset > 0 ) {
+			scrollUp.style.display = 'block';
+		} else {
+			scrollUp.style.display = 'none';
+		}
+	};
+};
