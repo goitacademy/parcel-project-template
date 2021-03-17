ba1ee918a7913505aafd2c3a9c90f355e1501e59
abakarovmaks <abakarovmaks@gmail.com>
2021-03-17 11:01:16 +0200

    Updates

--- menu.0c91648c.js
+++ menu.0c91648c.js
@@ -169,7 +169,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60230" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52528" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
