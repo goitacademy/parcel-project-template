07709f9f522a939d3b696d202659d5dfdd385fb1
EKhoroshko <xapoxa14@gmail.com>
2021-03-17 18:21:21 +0200

    scroll

--- src/counter.js
+++ src/counter.js
@@ -13,3 +13,14 @@ $(document).ready(function () {
         delay: 10000
     });
 });
+
+$(document).ready(function () {
+    $("a[href*=#]").on("click", function (e) {
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top
+        }, 777);
+        e.preventDefault();
+        return false;
+    });
+});
