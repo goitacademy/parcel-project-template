62d01751a2c8de6d53447ba6f3ac579b048351e2
EKhoroshko <xapoxa14@gmail.com>
2021-03-17 18:50:14 +0200

    js

--- src/counter.js
+++ src/counter.js
@@ -13,3 +13,21 @@ $(document).ready(function () {
         delay: 10000
     });
 });
+
+$(document).ready(function () {
+    $("a[href*=#]").on("click", function (e) {
+        var anchor = $(this);
+        trigger1 = $('.hamburger'),
+            overlay1 = $('.overlay'),
+            wrapper = $('#wrapper');
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top
+        }, 777);
+        e.preventDefault();
+        trigger1.removeClass('is-open');
+        trigger1.addClass('is-closed');
+        wrapper.removeClass('toggled');
+        overlay1.hide();
+        return false;
+    });
+});
