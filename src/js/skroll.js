3ffc0027d87d253c696358236f1493722ae52d6a
Yurii Yurents <yura19950117@gmail.com>
2021-03-21 16:39:06 +0200

    Плавная прокрутка

---
+++ src/js/skroll.js
@@ -0,0 +1,7 @@
+var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 400);
+    return false;
+});
