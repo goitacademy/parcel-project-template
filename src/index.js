136af4a49453d510aa404471beb48a25b97a28cb
OlhaKostenko <78738413+OlhaKostenko@users.noreply.github.com>
2021-04-25 20:21:51 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import './sass/main.scss';
 
+var elem = document.querySelector('.grid');
+var msnry = new Masonry(elem, {
+  // options
+  itemSelector: '.grid-item',
+  columnWidth: 200,
+});
+// element argument can be a selector string
+//   for an individual element
+var msnry = new Masonry('.grid', {
+  // options
+  gutter: 3,
+});
