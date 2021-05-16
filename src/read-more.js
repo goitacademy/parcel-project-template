0ddcbd3e6a5ddba8ed8673379a38dfba5a02cafc
Svyatoslav29 <svyatoslav29@gmail.com>
2021-05-16 19:01:47 +0300

    без ридмор и стилей

---
+++ src/read-more.js
@@ -0,0 +1,17 @@
+// function readmore() {
+//     var dot = document.getElementById ("dot");
+//     var more = document.getElementById ("more");
+//     var btn = document.getElementById ("btn");
+
+//     if (dot.style.display === "none") {
+//         dot.style.display = "inline";
+//         more.style.display = "none";
+//         btn.innerHTML="Less";
+//     }
+//      else {
+//         dot.style.display = "none";
+//         more.style.display = "inline";
+//         btn.innerHTML="Less"; 
+//      }
+// };
+
