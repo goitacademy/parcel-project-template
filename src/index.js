8be385543ffbdc5df86c8d2e1cbe835f1aad1fb4
yuliiasonko <yuliiasonko@gmail.com>
2021-05-23 12:12:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './sass/main.scss';
+import menuElement from './menu.json';
+import menuTemplate from './templates/templates.hbs';
+import './changeTheme.js';
+import './styles.css';
+
+
+
+let menu = document.querySelector('.js-menu');
+
+function buildMenu(array) {
+    const markup = array.map(post => menuTemplate(post)).join('');
+    menu.insertAdjacentHTML('beforeend', markup);
+}
+
+buildMenu(menuElement);
