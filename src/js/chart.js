03e8dd656fa6278f549dbe08f3052ed83666c14c
kokhanskyi <kokhanskyi1975@gmail.com>
2021-05-25 10:18:38 +0300

    chatt-bace

--- src/js/chart.js
+++ src/js/chart.js
@@ -1,2 +1,47 @@
 import chartTpl from '../templates/chart.hbs';
 import ApiService from '../js/apiService.js';
+
+document.querySelector('.content').insertAdjacentHTML('beforeend', chartTpl());
+
+// document.querySelector('.content').innerHTML = chartTpl();
+console.log(chartTpl());
+
+const ctx = document.querySelector('.js-chart').getContext('2d');
+console.log(ctx);
+
+// new Chart(ctx, {
+//   type: 'bar',
+//   data: {
+//     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//     datasets: [
+//       {
+//         label: '# of Votes',
+//         data: [12, 19, 3, 5, 2, 3],
+//         backgroundColor: [
+//           'rgba(255, 99, 132, 0.2)',
+//           'rgba(54, 162, 235, 0.2)',
+//           'rgba(255, 206, 86, 0.2)',
+//           'rgba(75, 192, 192, 0.2)',
+//           'rgba(153, 102, 255, 0.2)',
+//           'rgba(255, 159, 64, 0.2)',
+//         ],
+//         borderColor: [
+//           'rgba(255, 99, 132, 1)',
+//           'rgba(54, 162, 235, 1)',
+//           'rgba(255, 206, 86, 1)',
+//           'rgba(75, 192, 192, 1)',
+//           'rgba(153, 102, 255, 1)',
+//           'rgba(255, 159, 64, 1)',
+//         ],
+//         borderWidth: 1,
+//       },
+//     ],
+//   },
+//   options: {
+//     scales: {
+//       y: {
+//         beginAtZero: true,
+//       },
+//     },
+//   },
+// });
