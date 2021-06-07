ebdb0a7e9eaac501f0e6b1ad8e7d0b40971fe1f9
nelli-belovol <nelli199020@gmail.com>
2021-06-07 21:45:56 +0300

    Update branch
    add ls for products__overlay,
    add some styles for items

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './sass/main.scss';
+
+(() => {
+    const refs = {
+      openProductBtn: document.querySelector('[data-product-open]'),
+      closeProductBtn: document.querySelector('[data-product-close]'),
+      product: document.querySelector('[data-product]'),
+    };
+  
+    refs.openProductBtn.addEventListener('click', toggleProduct);
+    refs.closeProductBtn.addEventListener('click', toggleProduct);
+  
+    function toggleProduct() {
+      document.body.classList.toggle("product-open");
+      refs.product.classList.toggle('is-hidden');
+    }
+  })();
