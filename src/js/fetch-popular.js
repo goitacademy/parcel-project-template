6371ac481af4bbcde4b8cd71c2886a585564a1b6
Maria Kononchuk <77247240+mariyka-kononchuk@users.noreply.github.com>
2021-08-09 15:19:22 +0300

    в запросе исправила all на movie

--- src/js/fetch-popular.js
+++ src/js/fetch-popular.js
@@ -7,7 +7,7 @@ const API_KEY = "27c4b211807350ab60580c41abf1bb8c";
 
 //значение переменной queryParams надо указывать в функции, которая будет отвечать
 //в слушателе событий за нужный поиск
-let queryParams = `trending/all/week?api_key=${API_KEY}`;
+let queryParams = `trending/movie/week?api_key=${API_KEY}`;
 
 //функция запроса - асинхронный код
 async function getFilms(queryParams) {
