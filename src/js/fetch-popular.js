af8cc29c20f416dfcb8af57af313b1139a35a347
Maria Kononchuk <77247240+mariyka-kononchuk@users.noreply.github.com>
2021-08-09 11:22:21 +0300

    изменила код функции запроса

--- src/js/fetch-popular.js
+++ src/js/fetch-popular.js
@@ -5,10 +5,13 @@ const BASE_URL = "https://api.themoviedb.org/3/";
 axios.defaults.baseURL = BASE_URL;
 const API_KEY = "27c4b211807350ab60580c41abf1bb8c";
 
+//значение переменной queryParams надо указывать в функции, которая будет отвечать
+//в слушателе событий за нужный поиск
+let queryParams = `trending/all/week?api_key=${API_KEY}`;
+
 //функция запроса - асинхронный код
-async function getPopularFilms() {
-    const QUERY_PARAMS = `trending/all/week?api_key=${API_KEY}`;
-    let url = BASE_URL + QUERY_PARAMS;
+async function getFilms(queryParams) {
+    let url = BASE_URL + queryParams;
         
     try {
         const response = await axios.get(url);
@@ -24,8 +27,34 @@ async function getPopularFilms() {
                 throw(error)
             }
 }
-getPopularFilms();
-export default { getPopularFilms };
+getFilms(queryParams);
+export default { getFilms };
+
+
+
+
+
+
+// async function getPopularFilms() {
+//     const QUERY_PARAMS = `trending/all/week?api_key=${API_KEY}`;
+//     let url = BASE_URL + QUERY_PARAMS;
+        
+//     try {
+//         const response = await axios.get(url);
+//         const data = response.data;
+//         console.log("результат запроса:",data);
+//         //массив объектов - популярные фильмы
+//         const film = data.results;
+//         console.log("массив объектов: популярные фильмы", film);
+//         const totalResults = data.total_results;
+//         console.log("всего найдено фильмов:", totalResults);
+//         return data, film, totalResults;
+//         } catch(error) {
+//                 throw(error)
+//             }
+// }
+// getPopularFilms();
+// export default { getPopularFilms };
 
 //поиск по ключевому слову
 //https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&page=1&include_adult=false
@@ -36,6 +65,14 @@ export default { getPopularFilms };
 //полное описание фильма
 //https://api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=en-US
 
+// function onGetPopularFilms() {
+//     let QUERY_PARAMS = `trending/all/week?api_key=${API_KEY}`;
+//     getFilms(QUERY_PARAMS);
+// }
 
+// function onGetSearchFilms() {
+//     let QUERY_PARAMS = `search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false`;
+//     getFilms(QUERY_PARAMS);
+// }
 
 
