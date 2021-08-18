a1a6833f5b67073ba5760392a02a373fa2e0f811
Yurii Lukianovych <mr.lukyanovich@gmail.com>
2021-08-18 06:21:56 +0300

    finish library

--- src/js/header-logic.js
+++ src/js/header-logic.js
@@ -1,4 +1,5 @@
 import watchedMovies from './watchedMovies';
+import queueMovies from './queueMovies';
 import modalWindow from './modal-window';
 import { watchedArray, queueArray } from './library';
 
@@ -22,6 +23,8 @@ refs.libraryNavigationBtn.addEventListener('click', onLibraryNavigationBtnClick)
 refs.filmoteca.addEventListener('click', onMainHeaderNavigationClick);
 refs.filmIcon.addEventListener('click', onMainHeaderNavigationClick);
 refs.homeNavigatiomBtn.addEventListener('click', onMainHeaderNavigationClick);
+refs.watchedButton.addEventListener('click', onWatchedBtnClick);
+refs.queueButton.addEventListener('click', onQueueBtnClick);
 
 function onLibraryNavigationBtnClick() {
   refs.searchForm.classList.add('is-hiden');
@@ -47,4 +50,24 @@ function onMainHeaderNavigationClick() {
   refs.homeGalleryList.classList.remove('is-hiden');
   refs.watchedGalleryList.classList.add('is-hiden');
   refs.queueGalleryList.classList.add('is-hiden');
+}
+
+function onWatchedBtnClick() {
+  refs.queueButton.classList.remove('btn--active');
+  refs.watchedButton.classList.add('btn--active');
+  refs.watchedGalleryList.classList.remove('is-hiden');
+  refs.queueGalleryList.classList.add('is-hiden');
+
+  watchedMovies(watchedArray);
+  modalWindow();
+}
+
+function onQueueBtnClick() {
+  refs.watchedButton.classList.remove('btn--active');
+  refs.queueButton.classList.add('btn--active');
+  refs.queueGalleryList.classList.remove('is-hiden');
+  refs.watchedGalleryList.classList.add('is-hiden');
+
+  queueMovies(queueArray);
+  modalWindow();
 }
