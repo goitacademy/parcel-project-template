311cebe6ea763ab8cd3194ce78079107a2873cdb
Serhii Tkachenko <x.ray868@gmail.com>
2021-09-04 11:18:30 +0300

    update

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,15 +1,13 @@
+const refs = {
+  openModalBtn: document.querySelector('[data-modal-open]'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+  modal: document.querySelector('[data-modal]'),
+};
 
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-export default  function toggleModal() {
+export default function toggleModal() {
   document.body.classList.toggle('modal-open');
   refs.modal.classList.toggle('is-hidden');
-  }
- 
+}
