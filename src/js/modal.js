7a53f623214394cfb5af86406a101945fbfde0f8
Andrian-Aff <affear.123@gmail.com>
2021-09-05 14:58:22 +0300

    удалил конфликт

--- src/js/modal.js
+++ src/js/modal.js
@@ -38,17 +38,3 @@ function closeModal() {
   window.removeEventListener('keydown', closeModal);
 }
 
-// const refs = {
-//   openModalBtn: document.querySelector('[data-modal-open]'),
-//   closeModalBtn: document.querySelector('[data-modal-close]'),
-//   modal: document.querySelector('[data-modal]'),
-
-// };
-
-// refs.openModalBtn.addEventListener('click', toggleModal);
-// refs.closeModalBtn.addEventListener('click', toggleModal);
-
-// export default function toggleModal() {
-//   document.body.classList.toggle('modal-open');
-//   refs.modal.classList.toggle('is-hidden');
-// }
