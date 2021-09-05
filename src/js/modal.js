514d354dce71d889029504b879c2478845baacdf
Serhii Tkachenko <x.ray868@gmail.com>
2021-09-05 15:15:07 +0300

    Update

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,39 +1,39 @@
-import modalTpl from '../templates/modalTpl.hbs';
-import homeMovies from '../templates/home-movies.hbs'
+// import modalTpl from '../templates/modalTpl.hbs';
+// import homeMovies from '../templates/home-movies.hbs'
 
-const refs = {
-  moviesContainer: document.querySelector('.movies__collection'),
-  backdrop: document.querySelector('[data-modal]'),
-  closeModalBtn: document.querySelector('[data-modal-close]')
-};
+// const refs = {
+//   moviesContainer: document.querySelector('.movies__collection'),
+//   backdrop: document.querySelector('[data-modal]'),
+//   closeModalBtn: document.querySelector('[data-modal-close]')
+// };
 
-refs.moviesContainer.addEventListener('click', openModal);
-refs.closeModalBtn.addEventListener('click', closeModal);
-refs.backdrop.addEventListener('click', closeOnBackdrop);
+// refs.moviesContainer.addEventListener('click', openModal);
+// refs.closeModalBtn.addEventListener('click', closeModal);
+// refs.backdrop.addEventListener('click', closeOnBackdrop);
 
-function openModal(evt) {
-  if (!evt.target.classList.contains('movie-card')) {
-    return;
-  }
-  document.body.classList.toggle('modal-open');
-  refs.backdrop.classList.toggle('is-hidden');
-  window.addEventListener('keydown', closeEscModal);
-}
+// function openModal(evt) {
+//   if (!evt.target.classList.contains('movie-card')) {
+//     return;
+//   }
+//   document.body.classList.toggle('modal-open');
+//   refs.backdrop.classList.toggle('is-hidden');
+//   window.addEventListener('keydown', closeEscModal);
+// }
 
-function closeOnBackdrop(event) {
-  if (event.target === event.currentTarget) {
-    closeModal(event);
-  }
-}
+// function closeOnBackdrop(event) {
+//   if (event.target === event.currentTarget) {
+//     closeModal(event);
+//   }
+// }
 
-function closeEscModal(event) {
-  const ESC_KEY_CODE = 'Escape';
-  if (event.code === ESC_KEY_CODE) {
-    closeModal();
-  };
-}
+// function closeEscModal(event) {
+//   const ESC_KEY_CODE = 'Escape';
+//   if (event.code === ESC_KEY_CODE) {
+//     closeModal();
+//   };
+// }
 
-function closeModal() {
-  refs.backdrop.classList.toggle('is-hidden');
-  window.removeEventListener('keydown', closeModal);
-}
+// function closeModal() {
+//   refs.backdrop.classList.toggle('is-hidden');
+//   window.removeEventListener('keydown', closeModal);
+// }
