e41f6b321ec9604860a4efa8726490d8af69917f
Vitaliy <vtk0679@gmail.com>
2021-09-05 21:06:17 +0300

    adds team-modal styles

--- src/js/team-modal.js
+++ src/js/team-modal.js
@@ -2,6 +2,17 @@ import * as basicLightbox from './basicLightbox.min.js';
 import renderTeamModalMarkup from '../templates/team-modal.hbs';
 import team from '../our-team.json';
 
-let markUp = renderTeamModalMarkup(team);
+const teamLink = document.querySelector('.team__link');
 
-basicLightbox.create(markUp).show();
+teamLink.onclick = onClickTeam;
+
+function onClickTeam(event) {
+  event.preventDefault();
+  const modal = basicLightbox.create(renderTeamModalMarkup(team));
+  modal.show();
+  const teamModal = document.querySelector('.team-modal');
+  document.onkeydown = evt => {
+    if (evt.code === 'Escape') modal.close();
+  };
+  teamModal.onclick = () => modal.close();
+}
