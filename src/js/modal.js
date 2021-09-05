b478dce8c1f4c542050f56cd3196b128cf2ecf6e
Andrian-Aff <affear.123@gmail.com>
2021-09-05 14:18:55 +0300

    add modal.js and repare code

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,13 +1,55 @@
+import modalTpl from '../templates/modalTpl.hbs';
+import homeMovies from '../templates/home-movies.hbs'
+
 const refs = {
-  openModalBtn: document.querySelector('[data-modal-open]'),
-  closeModalBtn: document.querySelector('[data-modal-close]'),
-  modal: document.querySelector('[data-modal]'),
+  moviesContainer: document.querySelector('.movies__collection'),
+  backdrop: document.querySelector('[data-modal]'),
+  closeModalBtn: document.querySelector('[data-modal-close]')
 };
 
-refs.openModalBtn.addEventListener('click', toggleModal);
-refs.closeModalBtn.addEventListener('click', toggleModal);
+refs.moviesContainer.addEventListener('click', openModal);
+refs.closeModalBtn.addEventListener('click', closeModal);
+refs.backdrop.addEventListener('click', closeOnBackdrop);
 
-export default function toggleModal() {
+function openModal(evt) {
+  if (!evt.target.classList.contains('movie-card')) {
+    return;
+  }
   document.body.classList.toggle('modal-open');
-  refs.modal.classList.toggle('is-hidden');
+  refs.backdrop.classList.toggle('is-hidden');
+  window.addEventListener('keydown', closeEscModal);
 }
+
+function closeOnBackdrop(event) {
+  if (event.target === event.currentTarget) {
+    closeModal(event);
+  }
+}
+
+function closeEscModal(event) {
+  const ESC_KEY_CODE = 'Escape';
+  if (event.code === ESC_KEY_CODE) {
+    closeModal();
+  };
+}
+
+function closeModal() {
+  refs.backdrop.classList.toggle('is-hidden');
+  window.removeEventListener('keydown', closeModal);
+}
+
+
+// const refs = {
+//   openModalBtn: document.querySelector('[data-modal-open]'),
+//   closeModalBtn: document.querySelector('[data-modal-close]'),
+//   modal: document.querySelector('[data-modal]'),
+
+// };
+
+// refs.openModalBtn.addEventListener('click', toggleModal);
+// refs.closeModalBtn.addEventListener('click', toggleModal);
+
+// export default function toggleModal() {
+//   document.body.classList.toggle('modal-open');
+//   refs.modal.classList.toggle('is-hidden');
+// }
