7e0d847e0a547c4f45d5374013d46803f6e663a5
Olgashulz <41305602+Olgashulz@users.noreply.github.com>
2021-09-05 19:25:35 +0300

    закрытие по Esc

--- src/js/inputSearch.js
+++ src/js/inputSearch.js
@@ -6,21 +6,29 @@ import showAllert from './show-allert';
 
 const refs = {
     searchForm: document.querySelector('.search-form'),
+    body: document.querySelector('body'),
 }
+let inputQuery = '';
+
+console.log(refs.body)
 
 refs.searchForm.addEventListener('click', getinputQuery);
-
+window.addEventListener('keydown', clearInputEsc);
+//window.addEventListener('keydown', openEnter)
+//refs.body.addEventListener('click', clearInput);
 
 
 function getinputQuery (event){
     event.preventDefault();
 
-    if (event.target !== document.querySelector('.search-button')){
+    if (event.target !== document.querySelector('.search-icon')){
     return;
     }
     
-    let inputQuery = '';
+    
     inputQuery = event.currentTarget.elements.input.value;
+    console.log(event.currentTarget);
+    console.log(event.target);
     //console.log(inputQuery)
     findMovies(inputQuery);
 }
@@ -28,4 +36,19 @@ function getinputQuery (event){
 function findMovies(query) { 
     apiService.findMovies(query).then(createGalleryMarkup).catch(showAllert);
 }
+
+function clearInputEsc(event) {
+    event.preventDefault;
+    const ESC_KEY_CODE = 'Escape';
+    if (event.code === ESC_KEY_CODE) {
+        console.log(inputQuery);
+        event.preventDefault;
+        inputQuery = '';
+        document.querySelector('.search-input').value = "";
+    }
+  }
+
+
+
+
 
