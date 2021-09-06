528211fa14ce5c10be877f3fe21fa9daba11ecb8
Max <maxminullin@gmail.com>
2021-09-06 00:16:16 -0700

    fix button header

--- src/menu.js
+++ src/menu.js
@@ -2,6 +2,7 @@
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
   const headerRef = document.querySelector('header');
+  const telLinkRef = document.querySelector('.tel-link-wrapper');
 
   menuBtnRef.addEventListener('click', () => {
     const expanded =
