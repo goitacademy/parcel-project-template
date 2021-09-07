2c3e7d973b88414a2617e9a7f035265f3c9e4930
ElenaPetrus <leliksmp@ukr.net>
2021-09-07 20:12:05 +0300

    class is made

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,17 +1,19 @@
 import getRefs from './get-refs';
 const refs = getRefs();
 
+class Pagination {
+  constructor  (){
+this.totalPages = 20;
+this.page = 10;
+  }
 
-let totalPages = 20;
-let page = 10;
-//calling function with passing parameters and adding inside element which is ul tag
-refs.ulTag.innerHTML = createPagination(totalPages, page);
 
-function createPagination(totalPages, page){
+createPagination(totalPages, page){
 let liTag = '';
 let activeLi;
 let beforePage = page-1;// 5-1=4
 let afterPage = page+1;//5+1=6
+
 
 
 if(page >1){//if page value is geater than 1 then add new li which is  the previous btn
@@ -53,15 +55,23 @@ for (let plength = beforePage; plength <= afterPage; plength++) {
     liTag+= `<li class="pagination_item numb ${activeLi}" onclick="createPagination(totalPages, ${plength})"><span>${plength}</span></li>`;   
 }
 
- if(page < totalPages - 1){ //if page value is less than totalPage value by -1 then show the last li or page
-if(page < totalPages - 2){ //if page value is less than totalPage value by -2 then add this (...) before the last li or page
-    liTag += `<li class="dots"><span>...</span></li>`;
+  if(page < totalPages - 1){ //if page value is less than totalPage value by -1 then show the last li or page
+  if(page < totalPages - 2){ //if page value is less than totalPage value by -2 then add this (...) before the last li or page
+      liTag += `<li class="dots"><span>...</span></li>`;
+    }
+    liTag += `<li class="pagination_item last numb" onclick="createPagination(totalPages, ${totalPages})"><span>${totalPages}</span></li>`;
   }
-  liTag += `<li class="pagination_item last numb" onclick="createPagination(totalPages, ${totalPages})"><span>${totalPages}</span></li>`;
+  if (page < totalPages) { //show the next button if the page value is less than totalPage(20)
+      liTag+= `<li class="pagination_item btn_pgn" onclick="createPagination(totalPages, ${page + 1})"><span><i class="fas fa-arrow-right"></i></i></span></li>`;
+  }
+  refs.ulTag.innerHTML = liTag; //add li tag inside ul tag
+    return liTag; //reurn the li tag
+  }
 }
-if (page < totalPages) { //show the next button if the page value is less than totalPage(20)
-    liTag+= `<li class="pagination_item btn_pgn" onclick="createPagination(totalPages, ${page + 1})"><span><i class="fas fa-arrow-right"></i></i></span></li>`;
-}
-refs.ulTag.innerHTML = liTag; //add li tag inside ul tag
-  return liTag; //reurn the li tag
-}
+
+  //новый экземпляр
+  // const newsPagination = new Pagination();
+
+
+    // //calling function with passing parameters and adding inside element which is ul tag
+    // refs.ulTag.innerHTML = createPagination(totalPages, page);
