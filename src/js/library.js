117e0aead8cc7d602d982fb22723ac8bf9512630
Serhii Tkachenko <79119130+Tkachenko-Serhii@users.noreply.github.com>
2021-09-07 18:44:56 +0300

    update header

--- src/js/library.js
+++ src/js/library.js
@@ -1,10 +1,9 @@
 import getRefs from './get-refs';
-import libraryLink from './get-refs';
 import createMarkupHeaderLib from '../templates/header-lib.hbs';
 
-getRefs().libraryLink.addEventListener('click', onClickLibRender);
+getRefs().library.addEventListener('click', onClickLibRender);
 
 function onClickLibRender(e) {
   e.preventDefault();
-  getRefs().container.innerHTML = createMarkupHeaderLib();
+  getRefs().headerJs.innerHTML = createMarkupHeaderLib();
 }
