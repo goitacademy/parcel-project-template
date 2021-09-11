5af05d02c385345ba5e1a2ee9609b0cfd5ebfae2
Serhii Tkachenko <79119130+Tkachenko-Serhii@users.noreply.github.com>
2021-09-11 13:03:36 +0300

    Update

--- src/js/modal.js
+++ src/js/modal.js
@@ -70,14 +70,14 @@ function chechTheme(modalWindow) {
 function bodyClassToggle() {
   const modalOpen = document.querySelector('.basicLightbox');
   modalOpen.addEventListener('click', onBackdropClick);
-  refs.body.classList.toggle('hidden');
+  refs.bodyRef.classList.toggle('hidden');
 }
 
 function onBackdropClick(e) {
   if (e.target !== e.currentTarget) {
     return;
   }
-  refs.body.classList.toggle('hidden');
+  refs.bodyRef.classList.toggle('hidden');
 }
 
 // function addGenreMarginTop() {
