2c7f72e3a55af21bf244b8e2f7226259379888e4
Vitaliy <vtk0679@gmail.com>
2021-09-14 16:37:03 +0300

    adds youTubeApi

--- src/js/showTrailer.js
+++ src/js/showTrailer.js
@@ -1,10 +1,11 @@
-import getRefs from './get-refs';
 import { apiService } from '../index';
 import getPlayerMarkup from '../templates/player.hbs';
 import * as basicLightbox from './basicLightbox.min.js';
 import showAlert from './show-allert.js';
 
-const refs = getRefs();
+prepareYouTubeAPI();
+
+let modal;
 
 export default function showTrailer(e) {
   const id = e.target.dataset.id;
@@ -13,10 +14,40 @@ export default function showTrailer(e) {
       showAlert('Unfortunately, no video found');
       return;
     }
-    const modal = basicLightbox.create(getPlayerMarkup(key));
+    modal = basicLightbox.create(getPlayerMarkup(key));
     modal.show();
     document.addEventListener('keydown', evt => {
       if (evt.code === 'Escape') modal.close();
     });
+
+    createYouTubePlayerAPI(key);
   });
+}
+
+function prepareYouTubeAPI() {
+  var tag = document.createElement('script');
+  tag.src = 'https://www.youtube.com/player_api';
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+}
+
+function createYouTubePlayerAPI(key) {
+  new YT.Player('ytplayer', {
+    height: '100%',
+    width: '100%',
+    videoId: key,
+    origin: 'https://tkachenko-serhii.github.io/filmoteka_project_group_1/',
+    events: {
+      onReady: onPlayerReady,
+    },
+  });
+}
+
+function onPlayerReady(e) {
+  const state = e.target.getPlayerState();
+  if (state === -1) {
+    modal.close();
+    showAlert('Unfortunately, video is currently unavailable');
+    return;
+  }
 }
