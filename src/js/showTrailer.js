f8f9d7ed34794879cc7ef8ca75d77a312f6059f3
Vitaliy <vtk0679@gmail.com>
2021-09-14 11:24:35 +0300

    fixes no-video problem

--- src/js/showTrailer.js
+++ src/js/showTrailer.js
@@ -2,12 +2,17 @@ import getRefs from './get-refs';
 import { apiService } from '../index';
 import getPlayerMarkup from '../templates/player.hbs';
 import * as basicLightbox from './basicLightbox.min.js';
+import showAlert from './show-allert.js';
 
 const refs = getRefs();
 
 export default function showTrailer(e) {
   const id = e.target.dataset.id;
   apiService.getTrailerKeyByMovieId(id).then(key => {
+    if (!key) {
+      showAlert('Unfortunately, no video found');
+      return;
+    }
     const modal = basicLightbox.create(getPlayerMarkup(key));
     modal.show();
     document.addEventListener('keydown', evt => {
