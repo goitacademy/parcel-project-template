1d3f9f552099d53354b0b288126a32d9688d7dc4
Artem Bilodid <bilodid.sumy@gmail.com>
2021-10-10 21:25:31 +0300

    added event listener to header, minor refactoring

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './sass/main.scss';
 
 import './js/site-nav.js';
+import refs from './js/refs';
+const { homeLink } = refs;
 
 import Paginator from './js/components/paginator';
 import Trending from './js/components/trending';
@@ -8,5 +10,5 @@ import Trending from './js/components/trending';
 window.paginator = new Paginator();
 const trending = new Trending();
 
-
 document.addEventListener('DOMContentLoaded', trending.onHomePageLoaded.bind(trending));
+homeLink.addEventListener('click', trending.onHomeButtonClicked.bind(trending));
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,3 +1,8 @@
 export default {
   paginationEl: document.getElementById('pagination'),
+  header: document.querySelector('header'),
+  btnBox: document.querySelector('.buttons-box'),
+  searchForm: document.querySelector('.header-form'),
+  homeLink: document.getElementById('home'),
+  libraryLink: document.getElementById('library'),
 };
