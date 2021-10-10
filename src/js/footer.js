019e2ec1098e40da0edef51fd45e1a9bdbcee5b6
osipm <54916138+osipm@users.noreply.github.com>
2021-10-10 12:35:03 +0300

    модальне вікно команди без фото

---
+++ src/js/footer.js
@@ -0,0 +1,21 @@
+const team = document.querySelector('#team');
+const developers = document.querySelector('.developers')
+const btnCloseModal = document.querySelector('[data-action="close-developers"]');
+
+team.addEventListener('click', onOpenModalFooter);
+btnCloseModal.addEventListener('click', onCloseModalClick);
+
+function onOpenModalFooter(){
+    developers.classList.add('is-open');
+    window.addEventListener('keydown', onEscKeyPress);
+}
+
+function onCloseModalClick(e) {
+    developers.classList.remove('is-open');
+  };
+
+  function onEscKeyPress(e) {
+    if (e.code === 'Escape') {
+      onCloseModalClick();
+    }
+};
