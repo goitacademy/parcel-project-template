91f12cb9158a57020662a7c42d46a3c2fae0a8b1
Artem Bilodid <ar.bilodid@draftkings.com>
2021-10-11 15:16:52 +0300

    trending moved to a separate file to fix events listener conflict

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 import './sass/main.scss';
-import refs from './js/refs';
-const { homeLink } = refs;
 
-import './js/site-nav.js';
 import './js/page-render.js';
-
-import Paginator from './js/components/paginator';
-import Trending from './js/components/trending';
-
-window.paginator = new Paginator();
-const trending = new Trending();
-
-document.addEventListener('DOMContentLoaded', trending.onHomePageLoaded.bind(trending));
-homeLink.addEventListener('click', trending.onHomeButtonClicked.bind(trending));
+import './js/trending';
+import './js/site-nav.js';
