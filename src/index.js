efd5ccf334a0a959602bea92da474b21ae162f7e
Artem Bilodid <ar.bilodid@draftkings.com>
2021-10-12 16:38:06 +0300

    fixed broken imports

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import './sass/main.scss';
 
-import './js/site-nav.js';
-
-import './js/search-renderer.js';
 import './js/displayUserLibrary.js';
-
-import Paginator from './js/components/paginator';
-import Trending from './js/components/trending';
-
-window.paginator = new Paginator();
-const trending = new Trending();
-
-document.addEventListener('DOMContentLoaded', trending.onHomePageLoaded.bind(trending));
-
 import './js/search-renderer.js';
 import './js/trending';
 import './js/site-nav.js';
