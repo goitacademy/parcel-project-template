f6a60b940ee57cd05416ea8bf8fca31d1be495b6
Myroniuk Ilona <knopkakyrs@gmail.com>
2021-10-16 22:02:54 +0300

    add logic to save btn-state REMOVE after closing modal  if id in LS

--- src/js/modal.js
+++ src/js/modal.js
@@ -10,8 +10,9 @@ list.addEventListener('click', openModal);
 function openModal(e) {
   if (e.target.nodeName === 'IMG') {
     const id = e.target.dataset.sourse;
+    // ---set id to LS for render WATCED/QUEUE list
     localStorage.setItem('idModal', id);
-
+    // ---
     apiService.fetchMovie(id).then(data => {
       if (data.genres.length === 0) {
         data.genre = 'Other';
@@ -21,8 +22,18 @@ function openModal(e) {
 
       const modal = new renderModalClass(data);
       modal.showModal();
-
       body.classList.add('modal-open');
+
+      //----default btn-state REMOVE if id in LS
+      const btnWatched = document.getElementById('toWatch');
+      const btnQueue = document.getElementById('toQueue');
+      if (localStorage.getItem('watched').includes(id)) {
+          btnWatched.textContent = 'REMOVE FROM WATCHED';
+      }
+      if (localStorage.getItem('queue').includes(id)) {
+          btnQueue.textContent = 'REMOVE FROM QUEUE';
+      }
+      // -----
 
       body.addEventListener('keydown', closeModalByKey);
       body.addEventListener('click', closeModalByClick);
@@ -53,6 +64,7 @@ function openModal(e) {
   }
 }
 
+// LOCAL STORAGE
 // ADD TO LS
 const addFilmsToLSbyButtonClick = {
   watched: [],
@@ -72,14 +84,14 @@ const addFilmsToLSbyButtonClick = {
 
   buttonListener(e) {
     addFilmsToLSbyButtonClick.checkLStoEmpty();
-
+    
     if (e.target.nodeName === 'BUTTON') {
       const btnEl = e.target;
       btnEl.dataset.data = addFilmsToLSbyButtonClick.getId();
       this.id = btnEl.dataset.data;
       const btnWatched = document.getElementById('toWatch');
       const btnQueue = document.getElementById('toQueue');
-
+      // IF CHECKED ADD TO WATCHED
       if (e.target.id === 'toWatch') {
         if (!localStorage.getItem('watched').includes(addFilmsToLSbyButtonClick.getId())) {
           let getItemWatched = localStorage.getItem('watched');
@@ -87,21 +99,21 @@ const addFilmsToLSbyButtonClick = {
           getItemWatched.push(this.id);
           localStorage.setItem('watched', JSON.stringify(getItemWatched));
           btnWatched.classList.add('modal_btn_active');
-          btnWatched.textContent = 'Remove';
+          btnWatched.textContent = 'REMOVE FROM WATCHED';
         } else {
-          alert('Are you realy want to delete this film from Watched-list?');
+          confirm('Are you realy want to delete this film from Watched-list?');
+
+          btnWatched.classList.remove('modal_btn_active'); 
+          btnWatched.textContent = `ADD TO WATCHED`;
 
           let getItemWatched = localStorage.getItem('watched');
           getItemWatched = JSON.parse(getItemWatched);
           let index = getItemWatched.indexOf(this.id)
           getItemWatched.splice(index, 1);
           localStorage.setItem('watched', JSON.stringify(getItemWatched));
-          
-          btnWatched.classList.remove('modal_btn_active'); 
-          btnWatched.textContent = `ADD TO WATCHED`;
         }
       }
-
+      // IF CHECKED ADD TO QUEUE
       if (e.target.id === 'toQueue') {
         if (!localStorage.getItem('queue').includes(addFilmsToLSbyButtonClick.getId())) {
           let getItemQueue = localStorage.getItem('queue');
@@ -109,18 +121,18 @@ const addFilmsToLSbyButtonClick = {
           getItemQueue.push(this.id);
           localStorage.setItem('queue', JSON.stringify(getItemQueue));
           btnQueue.classList.add('modal_btn_active');
-          btnQueue.textContent = 'Remove';
+          btnQueue.textContent = 'REMOVE FROM QUEUE';
         } else {
-          alert('Are you realy want to delete this film from Queue-list?');
+          confirm('Are you realy want to delete this film from Queue-list?');
+
+          btnQueue.classList.remove('modal_btn_active'); 
+          btnQueue.textContent = `ADD TO QUEUE`;
 
           let getItemQueue = localStorage.getItem('queue');
           getItemQueue = JSON.parse(getItemQueue);
           let index = getItemQueue.indexOf(this.id)
           getItemQueue.splice(index, 1);
           localStorage.setItem('queue', JSON.stringify(getItemQueue));
-
-          btnQueue.classList.remove('modal_btn_active'); 
-          btnQueue.textContent = `ADD TO QUEUE`;
         }
       }
     }
