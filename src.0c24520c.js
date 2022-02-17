dfb12c79c194530dc9a1546c3a0779366f71c471
SashaDyts <94003232+SashaDyts@users.noreply.github.com>
2022-02-17 07:16:00 +0000

    Deploying to gh-pages from @ SashaDyts/project-demo@87251099b0534bc19f42df5634e4527b550519b8 🚀

--- src.0c24520c.js
+++ src.0c24520c.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/parcel-project-template/src.9139aab2.js.map
+//# sourceMappingURL=/project-demo/src.0c24520c.js.map
