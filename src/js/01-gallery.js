6ad632e6fd34c885c29c33279d4e78f48a04be87
Juliya Khanenko <jullietnm@gmail.com>
2022-08-19 21:04:58 +0300

    1 ok

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,41 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+
+// Change code below this line
+
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const galleryList = createGalleryCardsItem(galleryItems);
+gallery.insertAdjacentHTML("beforeend", galleryList);
+
+
+ function createGalleryCardsItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__img"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+.join("");
+ }
+
+
+
+
