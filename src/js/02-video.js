50c09be2235c1df6d4fe236ee00a7bb45ae8eea8
Juliya Khanenko <jullietnm@gmail.com>
2022-08-20 19:26:09 +0300

    ок

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+// import Player from '@vimeo/player';
+// import { throttle } from 'lodash';
 
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+
+// player.on('timeupdate',  throttle( e => {
+//     localStorage.setItem('videoplayer-current-time', e.seconds);
+//     }, 1000)
+//     );
+
+// player
+// .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+// .catch(function (error) {
+//     console.error(error)
+// });
+
+
+
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
