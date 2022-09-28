c21e55037e1a6c85abbb5ad8667a5830f970a645
Viktoriia <viktoria.murzhak@gmail.com>
2022-09-28 10:31:36 +0300

    рандом

--- src/js/getRandomCoctails.js
+++ src/js/getRandomCoctails.js
@@ -1,31 +1,29 @@
 const cocktailsList = document.querySelector('.gallery__cards');
 
 async function fetchRundomCockteil() {
-  // let arr = [];
-  // for (let i = 0; i < 9; i += 1) {
-  //   const cocktail = await fetch(
-  //     `https://www.thecocktaildb.com/api/json/v1/1/random.php`
-  //   );
-  //   arr.push(cocktail);
-  const response = await fetch(
-    `https://www.thecocktaildb.com/api/json/v1/1/random.php`
-  );
+  const rundomArr = [];
+  if (document.documentElement.clientWidth >= 1280) {
+    for (let i = 0; i < 9; i++) {
+      const response = await fetch(
+        `https://www.thecocktaildb.com/api/json/v1/1/random.php`
+      );
+      rundomArr.push(response);
+    }
+  }
   if (!response.ok) {
     throw new Error('no data loaded!');
   }
-  return await response.json();
-}
-// }
 
-createCardsListMarkup();
+  console.log('rundomArr', rundomArr);
+  // return await response.json();
+}
+getRundomCoctailData();
+// createCardsListMarkup();
 
 function getRundomCoctailData() {
   fetchRundomCockteil()
     .then(data => {
       console.log('data.drinks', data.drinks);
-      let dataArr = [];
-      // let all = data;
-      // dataArr.push(PromiseAll);
       createCardMarkup(data.drinks);
     })
     .catch(err => {
@@ -33,27 +31,27 @@ function getRundomCoctailData() {
     });
 }
 
-function createCardsListMarkup() {
-  if (document.documentElement.clientWidth >= 1280) {
-    for (let i = 0; i < 9; i++) {
-      getRundomCoctailData();
-    }
-  } else if (
-    document.documentElement.clientWidth >= 768 &&
-    document.documentElement.clientWidth < 1280
-  ) {
-    for (let i = 0; i < 6; i++) {
-      getRundomCoctailData();
-    }
-  } else if (
-    document.documentElement.clientWidth > 0 &&
-    document.documentElement.clientWidth < 768
-  ) {
-    for (let i = 0; i < 3; i++) {
-      getRundomCoctailData();
-    }
-  }
-}
+// function createCardsListMarkup() {
+//   if (document.documentElement.clientWidth >= 1280) {
+//     for (let i = 0; i < 9; i++) {
+//       getRundomCoctailData();
+//     }
+//   } else if (
+//     document.documentElement.clientWidth >= 768 &&
+//     document.documentElement.clientWidth < 1280
+//   ) {
+//     for (let i = 0; i < 6; i++) {
+//       getRundomCoctailData();
+//     }
+//   } else if (
+//     document.documentElement.clientWidth > 0 &&
+//     document.documentElement.clientWidth < 768
+//   ) {
+//     for (let i = 0; i < 3; i++) {
+//       getRundomCoctailData();
+//     }
+//   }
+// }
 
 function createCardMarkup(drinksArr) {
   const markup = drinksArr
@@ -64,12 +62,16 @@ function createCardMarkup(drinksArr) {
      <h3 class='gallery__card-name'>${strDrink}</h3>
      <div>
      <button class='gallery__btn-load-more' data-open='open-modal-description'>Learn more</button>
-      <button class='gallery__btn-add-to-fav' data-add='add-to-fav'>Add to</button>
+      <button class='gallery__btn-add-to-fav' data-add='add-to-fav'>Add to
+       <svg class="icon-modal-cocktail" height="32" width="32">
+        <use href="../img/sprite.svg/#icon-heart"></use>
+       </svg></button>
       </div>
      </div>
      </li>`
     )
     .join('');
 
+  // return markup;
   cocktailsList.insertAdjacentHTML('beforeend', markup);
 }
