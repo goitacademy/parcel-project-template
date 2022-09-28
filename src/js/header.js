a495f4aec40b7a8b804c9f238236a259d83fa10a
makslusak <106471649+makslusak@users.noreply.github.com>
2022-09-28 13:20:29 +0300

    Markup and styles for Header

--- src/js/header.js
+++ src/js/header.js
@@ -1,14 +1,26 @@
 const burgerBtnRef = document.querySelector('.header__burger');
 const crossBtnRef = document.querySelector('.header__cross');
 const mobilMenuRef = document.querySelector('.header__mobil');
+const favoriteDeskRef = document.querySelector('.js-nav__link--desk');
+const favoriteMobilRef = document.querySelector('.js-nav__link--mobil');
+const navDeskRef = document.querySelector('.nav__links--desktop');
+const navMobileRef = document.querySelector('.nav__links--mobil');
 
-burgerBtnRef.addEventListener('click', onMobilMenuOpen);
-crossBtnRef.addEventListener('click', onMobilMenuClose);
+burgerBtnRef.addEventListener('click', removeIsHiden);
+crossBtnRef.addEventListener('click', addIsHiden);
+favoriteDeskRef.addEventListener('click', toggleIsHidenn);
+favoriteMobilRef.addEventListener('click', toggleIsHiden);
 
-function onMobilMenuOpen(evt) {
+function removeIsHiden(evt) {
   mobilMenuRef.classList.remove('is-hidden');
 }
 
-function onMobilMenuClose(evt) {
+function addIsHiden(evt) {
   mobilMenuRef.classList.add('is-hidden');
+}
+function toggleIsHiden(evt) {
+  navMobileRef.classList.toggle('is-hidden');
+}
+function toggleIsHidenn(evt) {
+  navDeskRef.classList.toggle('is-hidden');
 }
