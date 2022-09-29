533c8840af3072bc1e8d9ac09dde9054f25047e9
Ivan Chornyi <theivanchornyi@gmail.com>
2022-09-29 10:27:54 +0300

    temporary databese set/read

--- src/servise/firebase.js
+++ src/servise/firebase.js
@@ -6,6 +6,7 @@ import {
   GoogleAuthProvider,
   onAuthStateChanged,
 } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
 
 import { firebaseConfig } from '../config/firebase-config';
 import { userIn } from '../js/authorization-button';
@@ -13,6 +14,7 @@ import { userIn } from '../js/authorization-button';
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
+const database = getDatabase();
 
 export const signUp = () => {
   signInWithPopup(auth, provider)
@@ -46,3 +48,12 @@ onAuthStateChanged(auth, user => {
     userIn('disable');
   }
 });
+
+// function writeUserData(userId, name, email, imageUrl) {
+//   const db = getDatabase();
+//   set(ref(db, 'users/' + userId), {
+//     username: name,
+//     email: email,
+//     profile_picture: imageUrl,
+//   });
+// }
