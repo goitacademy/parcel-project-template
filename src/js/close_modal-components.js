606a34cca99ef94ee77c9b543e8ccf9419110a70
Abdrii Borinskij <aborinskij@gmail.com>
2022-09-29 03:09:34 +0300

    типо js

--- src/js/close_modal-components.js
+++ src/js/close_modal-components.js
@@ -1,23 +1,75 @@
+import axios from 'axios';
+import { forEach, forEachRight } from 'lodash';
 // window.onscroll = function () {
 //   return false;
 // };
 
-// const BASE_URL = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+const modalRef = document.querySelector('.modal__description');
+const closeRef = document.querySelector('.button__close');
 
-// const a = fetch(`${BASE_URL}`)
-//   .then(response => {
-//     console.log('response', response);
-//     // Response handling
-//   })
+closeRef.addEventListener('click', () => {
+  modalRef.classList.add('visually-hidden');
+});
 
-//   .catch(error => {
-//     console.log('error!!!!!!!!!!', error);
-//     // Error handling
-//   });
-// console.log(
-//   'a',
-//   asing a.then(response => {
-//     console.log('response2', response);
-//     // Data handling
-//   })
-// );
+// axios.defaults.baseURL = 'http://www.thecocktaildb.com/api/json/v1/1';
+// document.addEventListener('DOMContentLoaded', getIngridient);
+// import axios from 'axios';
+// async function getIngridient() {
+//   const ingridient = await axios(
+//     'https://www.thecocktaildb.com/api/json/v1/1/search.php?i=vodka'
+//   );
+//   console.log('ingridient :>> ', ingridient);
+// }
+
+// const axios = require('axios');
+
+async function getIngridient() {
+  const request = await axios(
+    'https://www.thecocktaildb.com/api/json/v1/1/search.php?i=bourbon'
+  )
+    .catch(function (error) {
+      // обработка ошибки
+      console.log(error);
+    })
+    .then(({ data: { ingredients } }) => {
+      check(ingredients);
+    });
+}
+getIngridient();
+
+let a = [];
+
+async function check(element) {
+  marcup(element, a);
+  await element.map(e => {
+    for (let key in e) {
+      if (e[key] === null) {
+        console.log('no', key);
+        return;
+      }
+      a.push(key);
+    }
+  });
+}
+console.log('a', a);
+
+async function marcup(data, a) {
+  for (const key of a) {
+    console.log(' key', key);
+    await data.map(([key]) => {
+      data[key];
+      console.log('key', key);
+    });
+  }
+  console.log('data', data);
+}
+
+// const { strABV, strDescription, strType, strIngredient, strAlcohol } = data;
+// console.log('strAlcohol :', strAlcohol);
+// console.log('strIngredient :', strIngredient);
+// console.log('strType :', strType);
+// console.log('strDescription :', strDescription);
+// console.log('strABV :', strABV);
+
+// const testRef = document.querySelector('.description__span');
+// testRef.insertAdjacentHTML('beforeend', strDescription.substring(0, 400));
