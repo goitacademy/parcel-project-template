697b80fd5d3be81520918667020d69667ab92121
Оксана Качмар <108275501+Profiscribe@users.noreply.github.com>
2022-09-30 18:38:21 +0300

    ++++

---
+++ src/coctails.js
@@ -0,0 +1,14 @@
+// const image = document.querySelector(".img")
+// const url = "https://www.thecocktaildb.com";
+
+// async function fetchHandler() {
+//     try {
+//         const response = await fetch(url);
+//         const data = await response.json();
+//         image.src = data.file;
+    
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
