adc92af65cf6807c63a62645bf875bff161e5320
Markiyan333 <markiyan333@gmail.com>
2023-01-10 18:31:18 +0200

    update task2

---
+++ src/js/01-gallery.js
@@ -0,0 +1,36 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const imageGallery = createImageGallery(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', imageGallery);
+
+
+function createImageGallery(galleryItems) {
+    return galleryItems
+    .map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+        <a class="gallery__link"href="${original}">
+ <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+
+    })
+        .join("")
+}
+
+new SimpleLightbox('.gallery a', {
+	captionDelay: 250
+})
---
+++ src/js/02-video.js
@@ -0,0 +1,16 @@
+import Player from '@vimeo/player'
+
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');   
+const player = new Player(iframe);
+const lodashTime = 1000;
+
+    player.on('timeupdate', throttle( e => {
+        localStorage.setItem("videoplayer-current-time", e.seconds, lodashTime)
+    }));
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time") || 0)
+    .catch(function (error) {
+        console.error(error)
+    });
