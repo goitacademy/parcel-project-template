fcd1864ff8546b15001d4b23cd98f782ffd6852f
Markiyan333 <markiyan333@gmail.com>
2023-01-10 18:57:02 +0200

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,10 +6,12 @@ const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const lodashTime = 1000;
 
+
     player.on('timeupdate', throttle( e => {
         localStorage.setItem("videoplayer-current-time", e.seconds, lodashTime)
     }));
 
+    
 player.setCurrentTime(localStorage.getItem("videoplayer-current-time") || 0)
     .catch(function (error) {
         console.error(error)
