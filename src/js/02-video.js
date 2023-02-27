9eaa12a675b76d5edbd187a35f6b2644d3c8e0c8
Pawel Bogusz1 <pbog2000@gmail.com>
2023-02-27 21:13:26 +0100

    cz2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
