205d46965c953e5a0699d5b8b5835d6746b49f47
Pawel Bogusz1 <pbog2000@gmail.com>
2023-02-27 19:55:24 +0100

    cz1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,4 @@
-// Opisany w dokumentacji
 import SimpleLightbox from "simplelightbox";
-// Dodatkowy import stylów
 import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
