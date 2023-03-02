d86b8537d4707f560650b139a1175f273418716c
Pawel Bogusz1 <pbog2000@gmail.com>
2023-03-02 22:04:13 +0100

    zad3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,19 +1,11 @@
 import throttle from 'lodash.throttle';
 
-const form = document.querySelector("#feedback-form");
-const output = document.querySelector("#output");
-const LOCALSTORAGE_KEY = "feedback-form-state";
-
-updateOutput();
-form.addEventListener("submit", saveMessage);
-
-function saveMessage(evt) {
-  evt.preventDefault();
-  localStorage.setItem(LOCALSTORAGE_KEY, form.elements.message.value);
-  updateOutput();
-  form.reset();
-}
-
-function updateOutput() {
-  output.textContent = localStorage.getItem(LOCALSTORAGE_KEY) || "";
-}
+const submitBtn = document.getElementsByName(submitBtn);
+submitBtn.addEventListener("click", () =>{
+const email = document.querySelector("input").value;
+localStorage.setItem("feedback-form-state", email)
+const message = document.querySelector("input").value;
+localStorage.setItem("feedback-form-state", message)
+});
+const savedEmail = localStorage.getItem("feedback-form-state")
+ localStorage.getItem("feedback-form-state")
