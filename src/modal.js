dbe9859f3b05a3f2930872f23f0136fcfaf3b111
Silvian Ranetescu <silvian.ranetescu@gmail.com>
2023-04-08 22:23:46 +0300

    header-done

--- src/modal.js
+++ src/modal.js
@@ -1,14 +1,25 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
+// (() => {
+//   const refs = {
+//     openModalBtn1: document.querySelector('[data-modal-open="modal1"]'),
+//     openModalBtn2: document.querySelector('[data-modal-open="modal2"]'),
+//     closeModalBtn: document.querySelector('[data-modal-close]'),
+//     modal: document.querySelector('[data-modal]'),
+//   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+//   refs.openModalBtn1.addEventListener('click', toggleModal);
+//   refs.openModalBtn2.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle('modal--hidden');
+//   function toggleModal() {
+//     refs.modal.classList.toggle('modal--hidden');
+//   }
+// })();
+
+function myFunction() {
+  var x = document.getElementById('myDIV');
+  if (x.style.display === 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
   }
-})();
+}
