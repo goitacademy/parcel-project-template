41984d14406655755ddeb0ec4a70ba391634d892
ovidiucosarca <ovidiucosarca@gmail.com>
2023-04-11 10:28:22 +0300

    fifth

---
+++ src/js/swipe.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
