b69685e4d1ed5bb4e4aa713ed965a906d43a853a
Silvian Ranetescu <silvian.ranetescu@gmail.com>
2023-04-12 21:43:51 +0300

    errors fixed

--- src/footer-modal-1.js
+++ src/footer-modal-1.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector('[footer-data-modal-open-1]'),
+    closeModalBtn: document.querySelector('[footer-data-modal-close-1]'),
+    modal: document.querySelector('[footer-data-modal-1]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle('footer-is-hidden');
   }
 })();
