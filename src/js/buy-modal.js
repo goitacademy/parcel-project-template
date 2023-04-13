2f293832fe780053c25bc207da3c365484d2683e
ToaderG85 <123585550+ToaderG85@users.noreply.github.com>
2023-04-13 12:16:50 +0300

    update 10

--- src/js/buy-modal.js
+++ src/js/buy-modal.js
@@ -1,11 +1,15 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector(".buy-modal-button[data-modal-open]"),
+    openModalBtnMenu: document.querySelector(".buy-modal-button-menu[data-modal-open]"),
     closeModalBtn: document.querySelector(".buy-modal-button[data-modal-close]"),
     modal: document.querySelector(".buy-modal[data-modal]"),
   };
+
   refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtnMenu.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+
   function toggleModal() {
     refs.modal.classList.toggle("buy-modal-is-hidden");
   }
