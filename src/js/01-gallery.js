f978f6d27525cd818a07c73865a37cd3462f0a55
sergey-g80 <119445810+sergey-g80@users.noreply.github.com>
2023-05-07 13:34:40 +0300

    st5

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,9 +1,10 @@
 import SimpleLightbox from 'simplelightbox';
 import "simplelightbox/dist/simple-lightbox.min.css";
-// Add imports above this line
+// // Add imports above this line
 import { galleryItems } from './gallery-items';
+console.log(galleryItems)
 
-// Change code below this line
+// // Change code below this line
 
 const galleryContainer = document.querySelector('.gallery');
 const itemsMarkup = createGalleryItemsMarkup(galleryItems);
