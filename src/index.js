a138e42d0b6d113c236b5cd47d21026ba5b071e0
Ciinaruandreea <ciinaruandreea@gmail.com>
2023-06-19 02:32:21 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import './js/modal.js';
 import './js/contact-popup.js';
 import './js/franchise-popup.js';
+import './js/about-modal.js';
+// import './js/mobile-menu.js';
+
 // import './js/slider.js';
---
+++ src/js/about-modal.js
@@ -0,0 +1,13 @@
+(() => {
+  const aboutBtnRef = document.querySelector('[data-about-modal-open]');
+  const aboutModalRef = document.querySelector('[data-about-modal]');
+  const aboutBtnClose = document.querySelector('[data-about-modal-close]');
+
+  aboutBtnRef.addEventListener('click', () => {
+    aboutModalRef.classList.toggle('is-hidden');
+  });
+
+  aboutBtnClose.addEventListener('click', () => {
+    aboutModalRef.classList.toggle('is-hidden');
+  });
+})();
