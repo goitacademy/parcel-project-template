210f85e069aab30f29f17439b26fb18d9efc1896
Luka821 <dinga_luca@hotmail.com>
2023-06-19 00:49:13 +0300

    s

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-(() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
+
+var modal1 = document.getElementById("cream-modal");
+var modal2 = document.getElementById("coffee-modal");
+var modal3 = document.getElementById("milk-modal");
+
+// Get the button that opens the modal
+var btn1 = document.getElementById("creambtn");
+var btn2 = document.getElementById("coffeebtn");
+var btn3 = document.getElementById("milkbtn");
+
+
+
+// When the user clicks on the button, open the modal
+
+
+btn1.onclick = function() {
+  modal1.style.display = "block";
+};
+
+btn2.onclick = function() {
+  modal2.style.display = "block";
+};
+
+btn3.onclick = function() {
+  modal3.style.display = "block";  
+};
+
+
+
+
+
 
 
 
