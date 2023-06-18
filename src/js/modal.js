111fbaa307f850fc5e4b9d8f2deb230c7d026067
Ciinaruandreea <ciinaruandreea@gmail.com>
2023-06-19 00:12:01 +0300

    update about modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,11 +1,31 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.forEach(function (e) {
+    e.addEventListener('click', toggleModal);
+  });
+
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+(() => {
+  const refs = {
+    openModalBtn: document.querySelectorAll('[data-about-modal-open]'),
+    closeModalBtn: document.querySelector('[data-about-modal-close]'),
+    modal: document.querySelector('[data-about-modal]'),
+  };
+
+  refs.openModalBtn.forEach(function (e) {
+    e.addEventListener('click', toggleModal);
+  });
+
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
