46eacdf05a750df14c612346b85a8df5cbe06242
Lazaros Adrian Rudopulos <rudopulos@yahoo.com>
2023-06-19 19:57:44 +0300

    Update commit v1

--- src/js/slider.js
+++ src/js/slider.js
@@ -2,17 +2,14 @@ var slideIndex = 1;
 var slideIndex2 = 1; // Adăugăm un nou index pentru slider-ul mySlides2
 showSlides(slideIndex);
 showSlides2(slideIndex2); // Apelăm funcția pentru slider-ul mySlides2
-
 function plusSlides(n) {
   showSlides((slideIndex += n));
   showSlides2((slideIndex2 += n)); // Actualizăm index-ul pentru ambele slide-uri
 }
-
 function currentSlide(n) {
   showSlides((slideIndex = n));
   showSlides2((slideIndex2 = n)); // Actualizăm index-ul pentru ambele slide-uri
 }
-
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName('mySlides');
@@ -32,11 +29,9 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = 'block';
   dots[slideIndex - 1].className += ' active';
 }
-
 function showSlides2(n) {
   var i;
   var slides = document.getElementsByClassName('mySlides2');
-  var dots = document.getElementsByClassName('dot2'); // Presupunem că există și clasele 'mySlides2' și 'dot2' pentru slider-ul mySlides2
   if (n > slides.length) {
     slideIndex2 = 1;
   }
@@ -46,9 +41,5 @@ function showSlides2(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = 'none';
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(' active', '');
-  }
   slides[slideIndex2 - 1].style.display = 'block';
-  dots[slideIndex2 - 1].className += ' active';
 }
