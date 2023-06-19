41f3606ded6e6e6242403448c7890f9adfc10e63
AdelaHermina <129310498+AdelaHermina@users.noreply.github.com>
2023-06-19 05:08:07 +0300

    up

--- src/modal.js
+++ src/modal.js
@@ -1,14 +1,16 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
+    openModalBtn1: document.querySelector('[data-modal-open="modal1"]'),
+    openModalBtn2: document.querySelector('[data-modal-open="modal2"]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn1.addEventListener('click', toggleModal);
+  refs.openModalBtn2.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle('modal--hidden');
   }
 })();
