7816f75dbef42304a378e25afbe88f87cbe7b47d
Demouser702 <128376983+Demouser702@users.noreply.github.com>
2023-06-19 15:31:57 +0300

    Add files via upload

---
+++ JS/modal1.js
@@ -0,0 +1,17 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelectorAll("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.forEach(function (e) {
+    e.addEventListener("click", toggleModal);
+  });
+
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
