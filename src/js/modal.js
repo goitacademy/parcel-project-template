be2229de0cc45fd0460d436872f595f3955b59e1
Catalin-George Glinta <catalinglinta5@gmail.com>
2023-06-19 20:33:42 +0300

    modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,17 +1,16 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelectorAll('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
+    closeModalBtn: document.querySelectorAll('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.forEach(function (e) {
-    e.addEventListener('click', toggleModal);
-  });
+  refs.openModalBtn.forEach(btn => btn.addEventListener('click', toggleModal));
+  refs.closeModalBtn.forEach(btn => btn.addEventListener('click', toggleModal));
 
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  function toggleModal(e) {
+    const modal = e.currentTarget.nextElementSibling
+    modal.classList.toggle('is-hidden');
 
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
   }
 })();
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,23 +1,21 @@
-// import Swiper JS
-// import Swiper from 'swiper';
-// import Swiper styles
-// import 'swiper/swiper-bundle.css';
+// https://swiperjs.com/get-started
 
-// core version + navigation, pagination modules:
+// import Swiper from 'swiper';
+// import 'swiper/swiper-bundle.css';
 // import SwiperCore, { Navigation, Pagination } from 'swiper/core';
 
-// configure Swiper to use modules
-SwiperCore.use([Navigation, Pagination]);
 
-var swiper = new Swiper('.swiper-container', {
-  spaceBetween: 30,
-  effect: 'fade',
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
+// SwiperCore.use([Navigation, Pagination]);
+
+// var swiper = new Swiper('.swiper-container', {
+//   spaceBetween: 30,
+//   effect: 'fade',
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+// });
