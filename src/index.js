709a5f5d5d15b31a8c376bd10c583a40d7609707
Catalin-George Glinta <catalinglinta5@gmail.com>
2023-06-19 19:08:01 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-// import './js/buynow-popup.js';
+import './js/about-modal.js';
+import './js/buynow-popup.js';
 import './js/contact-popup.js';
 import './js/franchise-popup.js';
-import './js/about-modal.js';
-// import './js/mobile-menu.js';
+import './js/mobile-menu.js';
 import './js/modal.js';
 // import './js/slider.js';
-
-
