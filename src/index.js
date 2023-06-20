5f63a21f9e4208047611b33ea3d513df8a2fb824
Catalin-George Glinta <catalinglinta5@gmail.com>
2023-06-20 11:48:43 +0300

    update

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ import './js/contact-popup.js';
 import './js/franchise-popup.js';
 import './js/mobile-menu.js';
 import './js/modal.js';
-// import './js/slider.js';
+import './js/slider.js';
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,21 +1,21 @@
 // https://swiperjs.com/get-started
 
-// import Swiper from 'swiper';
-// import 'swiper/swiper-bundle.css';
-// import SwiperCore, { Navigation, Pagination } from 'swiper/core';
+import Swiper from 'swiper';
+import 'swiper/swiper-bundle.css';
+import SwiperCore, { Navigation, Pagination } from 'swiper/core';
 
 
-// SwiperCore.use([Navigation, Pagination]);
+SwiperCore.use([Navigation, Pagination]);
 
-// var swiper = new Swiper('.swiper-container', {
-//   spaceBetween: 30,
-//   effect: 'fade',
-//   pagination: {
-//     el: '.swiper-pagination',
-//     clickable: true,
-//   },
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-// });
+var swiper = new Swiper('.swiper-container', {
+  spaceBetween: 30,
+  effect: 'fade',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
