be44117ed311a969d6b94c7f21a3721952813228
SorinaHMinea <sorina.hodorog@yahoo.com>
2023-06-21 01:00:08 +0300

    modificari finale

--- src/index.js
+++ src/index.js
@@ -1,35 +1,22 @@
-
-var modal1 = document.getElementById("cream-modal");
-var modal2 = document.getElementById("coffee-modal");
-var modal3 = document.getElementById("milk-modal");
+var modal1 = document.getElementById('cream-modal');
+var modal2 = document.getElementById('coffee-modal');
+var modal3 = document.getElementById('milk-modal');
 
 // Get the button that opens the modal
-var btn1 = document.getElementById("creambtn");
-var btn2 = document.getElementById("coffeebtn");
-var btn3 = document.getElementById("milkbtn");
-
-
+var btn1 = document.getElementById('creambtn');
+var btn2 = document.getElementById('coffeebtn');
+var btn3 = document.getElementById('milkbtn');
 
 // When the user clicks on the button, open the modal
 
-
-btn1.onclick = function() {
-  modal1.style.display = "block";
+btn1.onclick = function () {
+  modal1.style.display = 'block';
 };
 
-btn2.onclick = function() {
-  modal2.style.display = "block";
+btn2.onclick = function () {
+  modal2.style.display = 'block';
 };
 
-btn3.onclick = function() {
-  modal3.style.display = "block";  
+btn3.onclick = function () {
+  modal3.style.display = 'block';
 };
-
-
-
-
-
-
-
-
-  
