76e0b983d772aad02294e72d0039ae2c94bb9cc6
SorinaHMinea <sorina.hodorog@yahoo.com>
2023-06-21 18:27:11 +0300

    modificari finale

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,5 +1,4 @@
 (() => {
-
   function initModal(id) {
     const refs = {
       openModalBtn: document.querySelector("[data-modal-open='" + id + "']"),
@@ -18,22 +17,3 @@
   initModal(2);
   initModal(3);
 })();
-
-    function initModal(id) {
-      const refs = {
-        openModalBtn: document.querySelector("[data-modal-open='" + id + "']"),
-        closeModalBtn: document.querySelector("[data-modal-close='" + id + "']"),
-        modal: document.querySelector("[data-modal='" + id + "']"),
-      };
-  
-      refs.openModalBtn.addEventListener("click", toggleModal);
-      refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-      function toggleModal() {
-        refs.modal.classList.toggle("is-hidden");
-      }
-    }
-    initModal(1)
-    initModal(2)
-    initModal(3)
-  })();
