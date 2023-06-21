bebfd28917bc753ed18ee030e08395965efbfaaf
SorinaHMinea <sorina.hodorog@yahoo.com>
2023-06-21 18:34:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ function currentSlide(n) {
 }
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName(‘mySlides’);
-  var dots = document.getElementsByClassName(‘dot’);
+  var slides = document.getElementsByClassName('mySlides');
+  var dots = document.getElementsByClassName('dot');
   if (n > slides.length) {
     slideIndex = 1;
   }
@@ -17,25 +17,26 @@ function showSlides(n) {
     slideIndex = slides.length;
   }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = ‘none’;
+    slides[i].style.display = 'none';
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(' active’, ‘’);
+    dots[i].className = dots[i].className.replace(' active', '');
   }
-  slides[slideIndex - 1].style.display = ‘block’;
-  dots[slideIndex - 1].className += ' active’;
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className += 'active';
+
   function myFunction() {
-    var dots = document.getElementById(‘dots’);
-    var moreText = document.getElementById(‘more’);
-    var btnText = document.getElementById(‘myBtn’);
-    if (dots.style.display === ‘none’) {
-      dots.style.display = ‘inline’;
-      btnText.innerHTML = ‘Read more’;
-      moreText.style.display = ‘none’;
+    var dots = document.getElementById('dots');
+    var moreText = document.getElementById('more');
+    var btnText = document.getElementById('myBtn');
+    if (dots.style.display === 'none') {
+      dots.style.display = 'inline';
+      btnText.innerHTML = 'Read more';
+      moreText.style.display = 'none';
     } else {
-      dots.style.display = ‘none’;
-      btnText.innerHTML = ‘Read less’;
-      moreText.style.display = ‘inline’;
+      dots.style.display = 'none';
+      btnText.innerHTML = 'Read less';
+      moreText.style.display = 'inline';
     }
   }
 }
