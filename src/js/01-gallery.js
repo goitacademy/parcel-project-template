e7e7d65e8865be57038f349650ddaa5d749c488f
Mihail <Maria@MihailMichael>
2023-06-27 16:37:31 +0300

    goit-js-hw-08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
 
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const items = [];
+galleryItems.forEach(element =>{
+    const photoLink = document.createElement('a')
+    photoLink.className = 'photo__link'
+    photoLink.href = element.original
+    const photoItem = document.createElement('img')
+    photoItem.className = 'gallery__image'
+    photoItem.src = element.preview;
+    photoItem.setAttribute('title', element.description)
+    photoItem.alt = element.description;
+    photoLink.append(photoItem)
+    items.push(photoLink)
+})
+gallery.append(...items)
+new SimpleLightbox('.gallery a',{
+    captionDelay: 250
+})
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import  throttle  from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate',  throttle( e => {
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+    }, 1000)
+    );
+
+player
+.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+.catch(function (error) {
+    console.error(error)
+});
