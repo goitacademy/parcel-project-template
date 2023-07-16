42fcdea12c422d792415d17887330db482804d61
matvii-holovan <golovanmatvij@gmail.com>
2023-07-16 16:11:19 +0200

    task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,6 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
