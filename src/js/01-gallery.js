a0ac3076cae770274d4e199c5d3cbad7ee2e8985
matvii-holovan <golovanmatvij@gmail.com>
2023-07-18 13:24:05 +0200

    task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,47 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import simplelightbox from "simplelightbox";  
 import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+function createGalleryItem(items) {
+  return items
+    .map(
+      (item) => `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+       <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>
+ </li>`
+    )
+    .join("");
+}
+
+const galleryItemsEl = createGalleryItem(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", galleryItemsEl);
+
+galleryEl.addEventListener("click", openImage);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionType: "alt",
+});
+
+function openImage(event) {
+  event.preventDefault();
+  if (event.target.tagName === "IMG") {
+    galleryEl.addEventListener("keydown", (event) => {
+      if (event.code === "Escape") {
+        instance.close();
+      }
+    });
+  }
+}
