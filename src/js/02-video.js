adbd323ff7417102ecac80e8b2f05b25b5b9dba0
matvii-holovan <golovanmatvij@gmail.com>
2023-07-18 13:40:20 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,18 @@
 
+
+
+
 const iframe = document.querySelector('iframe');
-    const player = new Vimeo.Player(iframe);
+const player = new Vimeo.Player(iframe);
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+player.on('play', function () {
+  console.log('played the video!');
+});
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const onPlay = function (data) {};
+
+player.on('play', onPlay);
