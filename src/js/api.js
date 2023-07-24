cb4d3bb0c27132ca38bdb47be874752ca56093c2
TarcaAndreea <adrianatarca1@gmail.com>
2023-07-24 15:54:01 +0300

    update-ENDPOIT

--- src/js/api.js
+++ src/js/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const ENDPOINT = 'https://openweathermap.org/api';
+const ENDPOINT = 'https://api.openweathermap.org';
 const API_KEY = 'd52884a2e04f7a405073b1f70d08c546';
 export async function fetchForecast(query, temperatureUnit) {
   const queryString = `${ENDPOINT}/data/2.5/forecast?q=${query}&appid=${API_KEY}&units=${temperatureUnit}`;
