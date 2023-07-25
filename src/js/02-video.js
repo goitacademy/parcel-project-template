5a2ec29b16c0a36ac6a6e3b77b0128c9040b67c5
matvii-holovan <golovanmatvij@gmail.com>
2023-07-25 14:44:11 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,9 +1,9 @@
 
 
-
+import Player from "@vimeo/player";
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 player.on('play', function () {
   console.log('played the video!');
