9991fe0a528f7c22f93f97afa103d8e265d9c69c
TarcaAndreea <adrianatarca1@gmail.com>
2023-07-25 12:44:41 +0300

    5days-up

--- src/js/btn-show-more.js
+++ src/js/btn-show-more.js
@@ -7,10 +7,3 @@ function showMore() {
     item.classList.add('show-on-mobile');
   });
 }
-document.getElementById('show-more').addEventListener('click', function () {
-  const hiddenItems = document.querySelectorAll('.hide-on-mobile');
-  hiddenItems.forEach(item => {
-    item.classList.remove('hide-on-mobile');
-    item.classList.add('show-on-mobile');
-  });
-});
