a4a9dc4f59e527d6a12f3af315845ca89b492143
AndreeaSichitiu <sichitiuandreea@gmail.com>
2023-07-26 18:36:37 +0300

    adaugare animatie cube

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import { gsap } from 'gsap';
+
+function animateSquares() {
+  const cubeFirst = document.querySelector('.cube-first');
+  const cubeSecond = document.querySelector('.cube-second');
+
+  gsap.to(cubeFirst, {
+    duration: 30,
+    x: 1200,
+    y: 600,
+    rotate: 400,
+    repeat: -1,
+    yoyo: true,
+  });
+
+  gsap.to(cubeSecond, {
+    duration: 30,
+    x: -1000,
+    y: 1000,
+    rotate: 400,
+    repeat: -1,
+    yoyo: true,
+  });
+}
+
+animateSquares();
