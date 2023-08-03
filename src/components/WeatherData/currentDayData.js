a3cdb130e5c5b03fc0dc5d57547f139a675a6ae0
AndreeaSichitiu <sichitiuandreea@gmail.com>
2023-08-03 18:25:00 +0300

    update citybackground function

--- src/components/WeatherData/currentDayData.js
+++ src/components/WeatherData/currentDayData.js
@@ -230,35 +230,44 @@ function submitForm(event) {
     });
     return;
   }
-  // //    afisare oras ales + functie modificare background cu orasul ales - in caz ca nu exista poze sa se afiseze peisaje cu cerul??
-  //     // daca este accesat butonul today  - accesare functie pt today
-  //     // daca este accesat butonul fivedays - accesare functie five days
-  //  stergere eveniment la o noua cautare
 
   weatherData.city = searchInput.value;
-  
   getCityBackground(searchInput.value);
   getWeatherForSearchedCity();
-   
+  // event.currentTarget.reset();
 }
 
-function getCityBackground(cityName)  {
+function getCityBackground(cityName) {
+ 
   const URL = 'https://pixabay.com/api/';
   const KEY = '&key=38046505-5b9e748b87046ce765cd21b85';
   const requestParameters = `?image_type=photo&category=travel&orientation=horizontal&q=${cityName}&page=1&per_page=40`;
   const bg = document.querySelector('.backgroundImage');
+
   fetch(URL + requestParameters + KEY, {
     method: 'GET',
   })
     .then(res => res.json())
     .then(image => {
       console.log(image);
-      const randomImg = Math.floor(Math.random() * image.hits.length);
-      const img = image.hits[randomImg].largeImageURL;      
-      bg.style.backgroundImage = `url(${img})`;
+      if (image.hits && image.hits.length > 0) {
+        const randomImg = Math.floor(Math.random() * image.hits.length);
+        const img = image.hits[randomImg].largeImageURL;
+        bg.style.backgroundImage = `url(${img})`;
+      } else {
+        const requestParameters = `?image_type=photo&category=buildings&orientation=horizontal&q=&page=1&per_page=40`;
+        fetch(URL + requestParameters + KEY, {
+          method: 'GET',
+        })
+          .then(res => res.json())
+          .then(image => {
+            const randomImg = Math.floor(Math.random() * image.hits.length);
+            const img = image.hits[randomImg].largeImageURL;
+            bg.style.backgroundImage = `url(${img})`;
+          });
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching background:', error);
     });
 }
-
-   
-
- 
