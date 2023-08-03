a89dc8d1d81ad872d2150f0307cb42157fc77c9d
TarcaAndreea <adrianatarca1@gmail.com>
2023-08-03 17:02:50 +0300

    btn-more

--- src/js/btn-show-more.js
+++ src/js/btn-show-more.js
@@ -2,6 +2,7 @@ const btnShowMoreRight = document.querySelector('.show-more__right');
 const btnShowMoreLeft = document.querySelector('.show-more__left');
 const initialItems = document.querySelectorAll('.show-initial');
 const hiddenItems = document.querySelectorAll('.hide-initial');
+const moreInfo = document.querySelector('.more-info');
 
 btnShowMoreLeft.addEventListener('click', () => {
   initialItems.forEach(item => {
@@ -23,4 +24,7 @@ btnShowMoreRight.addEventListener('click', () => {
     item.classList.add('hide-on-click');
     item.classList.remove('show-initial');
   });
+  if (window.matchMedia('(min-width: 768px) and (max-width: 1027px)').matches) {
+    moreInfo.scrollLeft += 100;
+  }
 });
