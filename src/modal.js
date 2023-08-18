d41b501fd518569fd2815765ac26c9499f0849b5
Popoaca Andrei <pandrei58@yahoo.com>
2023-08-19 00:55:15 +0300

    update

---
+++ src/modal.js
@@ -0,0 +1,19 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+const checkIcon = document.getElementById('check-icon');
+
+checkIcon.addEventListener('click', () => {
+  checkIcon.classList.toggle('clicked');
+});
