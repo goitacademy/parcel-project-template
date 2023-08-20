8a895ae414a4a2bfe340298e9833d597c44f205c
roberSovar <sovar_robert_ionut@yahoo.com>
2023-08-20 17:48:39 +0300

    finished contacts area for the project

--- src/index.js
+++ src/index.js
@@ -26,3 +26,34 @@
   });
 })();
 // ------------------------------------------------------
+
+// Contacts btn
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open-our]"),
+    closeModalBtn: document.querySelector("[data-modal-close-our]"),
+    modal: document.querySelector("[data-modal-our]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
