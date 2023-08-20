2f81127db2f7699a580ce980cbb1b47f50f80978
Nitu Alexandru <nitzu_alexandru@yahoo.com>
2023-08-20 17:48:08 +0300

    Finished Modal window

--- src/index.js
+++ src/index.js
@@ -25,4 +25,23 @@
     bodyScrollLock.enableBodyScroll(document.body);
   });
 })();
-// ------------------------------------------------------
+// Buy now button
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
+// const checkIcon = document.getElementById('check-icon');
+
+// checkIcon.addEventListener('click', () => {
+//   checkIcon.classList.toggle('clicked');
+// });
