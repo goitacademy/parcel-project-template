514fe68f00f14a6cd0695ffc385bc229a7ac210b
Githubuser289 <moldovan.mms@gmail.com>
2023-08-20 19:07:03 +0300

    small adjustments

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import './js/play-movie';
+import './js/swiper';
--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,46 +1,12 @@
 var swiper1 = new Swiper('.swiper1', {
-  
   // Optional parameters
   direction: 'horizontal',
   loop: false,
 
-  
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper1 .swiper-pagination',
     clickable: true,
-
-  },
-  slidesPerView: 1,
-  spaceBetween: 18,
-  // Responsive breakpoints
-  breakpoints: {
-    768: {
-      slidesPerView: 2.5,
-      spaceBetween: 18
-    },
-    1200: {
-      slidesPerView: 4,
-      spaceBetween: 18
-    }
-  },
-  autoplay: {
-    delay: 5000,
-  }
-});
-
-var swiper2 = new Swiper('.swiper2', {
-  
-  // Optional parameters
-  direction: 'horizontal',
-  loop: false,
-
-  
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-
   },
   slidesPerView: 1,
   spaceBetween: 16,
@@ -48,11 +14,11 @@ var swiper2 = new Swiper('.swiper2', {
   breakpoints: {
     768: {
       slidesPerView: 2,
-      spaceBetween: 16
+      spaceBetween: 16,
     },
     1200: {
       slidesPerView: 3,
-      spaceBetween: 28
-    }
-  }
+      spaceBetween: 28,
+    },
+  },
 });
