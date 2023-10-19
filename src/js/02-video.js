ae6e5c846f18208d94e868dc97c40683e17e5fe2
Oleg9145 <oleg.zinenko94@gmail.com>
2023-10-19 21:05:00 +0200

    Завдання 2 Ініціалізація плеєра

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Vimeo from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
