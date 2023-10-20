505caaff278dbba35835c445c802695352dec5ff
Razvan49 <128290757+Razvan49@users.noreply.github.com>
2023-10-20 12:26:59 +0300

    update2

--- src/js/pageBuilder.js
+++ src/js/pageBuilder.js
@@ -1,26 +1,22 @@
 import { fetchGenreList } from './fetchGenreList';
 import { handleResponse } from './galleryBuilder';
-import { fetchPopularMovies, fetchMovies } from './fetchMovieApi';
+import { fetchMovies, fetchPopularMovies } from './fetchMovieApi';
 
 // Funcție pentru popularea paginii in main
-const showPage = async (page, searchQuery = '') => {
+const showPage = async (page, isSearch = false, searchQuery = '') => {
   try {
     const genreList = await fetchGenreList();
     let response;
 
-    if (searchQuery) {
-      // Dacă există un termen de căutare, se obțin filmele corespunzătoare căutării   !!!! sa vedem cum imprelementam in search
+    if (isSearch) {
       response = await fetchMovies(searchQuery, page);
     } else {
-      // Dacă nu există un termen de căutare, se obțin filmele populare
       response = await fetchPopularMovies(page);
     }
 
-    // Se afișează răspunsul în galerie
-    handleResponse(response, searchQuery !== '', genreList);
+    handleResponse(response, isSearch, genreList);
   } catch (error) {
     console.error('Error', error);
   }
 };
-
 export { showPage };
