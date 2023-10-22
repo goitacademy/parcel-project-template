072210837b818eae9013f579a7b19e05cdc38b69
Oleg9145 <oleg.zinenko94@gmail.com>
2023-10-22 23:10:37 +0200

    Завдання 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,20 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+let formData = {};
 
+form.addEventListener(
+  'input',
+  throttle(e => {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  }, 500)
+);
+const savedData = localStorage.getItem('feedback-form-state');
+if (savedData) {
+  const savedFormData = JSON.parse(savedData);
+  formData = { ...formData, ...savedFormData };
+  emailInput.value = formData.email || '';
+  messageInput.value = formData.message || '';
+}
