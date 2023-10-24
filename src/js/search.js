3e64e40a00e4f7d869b6f308da6e5462473693c4
Razvan49 <128290757+Razvan49@users.noreply.github.com>
2023-10-24 22:48:41 +0300

    repair 2 done

--- src/js/search.js
+++ src/js/search.js
@@ -1,11 +1,10 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 import TheMovieDbApi from './themoviedb-api';
 import { clearGallery } from './clearGallery';
 import { handleResponse } from './galleryBuilder';
 
-const ITEMS_PER_PAGE = 20;
-
 const form = document.querySelector('.search-form');
-const noResultsError = document.querySelector('.no-results-error');
 
 const api = new TheMovieDbApi();
 
@@ -13,34 +12,31 @@ form.addEventListener('submit', onSearchFormSubmit);
 
 function onSearchFormSubmit(event) {
   event.preventDefault();
-  HideTextError(noResultsError);
 
   const queryString = event.currentTarget.elements.searchText.value.trim();
+
+  if (!queryString) {
+    // Afișează notificare pentru a specifica că trebuie să introduci un nume
+    Notify.failure('You must enter a movie name.');
+    return;
+  }
 
   api
     .getMoviesByName(queryString)
     .then(res => {
       console.log(res);
       if (res.results.length < 1) {
-        ShowTextError(noResultsError);
+        Notify.failure(
+          'Search result not successful. Enter the correct movie name.'
+        );
       } else {
-        HideTextError(noResultsError);
+        Notify.success('Results found successfully.');
         clearGallery(); // Șterge galeria înainte de a afișa rezultatele căutării
         handleResponse(res, true); // Afișează rezultatele căutării în galerie
       }
     })
     .catch(err => {
-      console.log(err);
+      Notify.failure('A apărut o eroare în timpul căutării.');
     })
     .finally(() => form.reset());
-}
-
-function HideTextError(error) {
-  error.classList.remove('shown');
-  error.classList.add('hidden');
-}
-
-function ShowTextError(error) {
-  error.classList.remove('hidden');
-  error.classList.add('shown');
 }
