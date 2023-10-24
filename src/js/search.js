b1923799c2d9afcbde892a38e0f1fbe4d59b21f3
DanielPredescu <danutz_preda@yahoo.com>
2023-10-24 20:58:04 +0300

    Update search-form

--- src/js/search.js
+++ src/js/search.js
@@ -1,9 +1,6 @@
 import TheMovieDbApi from './themoviedb-api.js';
-const ITEMS_PER_PAGE = 20;
 
 const form = document.querySelector('.search-form');
-const input = document.querySelector('.search-form-input');
-const emptySearchError = document.querySelector('.empty-search-error');
 const noResultsError = document.querySelector('.no-results-error');
 
 const api = new TheMovieDbApi();
@@ -12,20 +9,12 @@ form.addEventListener('submit', onSearchFormSubmit);
 
 function onSearchFormSubmit(event) {
   event.preventDefault();
-  HideTextError(emptySearchError);
   HideTextError(noResultsError);
 
   const queryString = event.currentTarget.elements.searchText.value.trim();
 
-  if (!queryString) {
-    ShowTextError(emptySearchError);
-    form.reset();
-    return;
-  } else {
-    HideTextError(emptySearchError);
-  }
-
-  fetchSearchedMovies(queryString)
+  api
+    .getMoviesByName(queryString)
     .then(res => {
       console.log(res);
       if (res.results.length < 1) {
@@ -48,8 +37,4 @@ function HideTextError(error) {
 function ShowTextError(error) {
   error.classList.remove('hidden');
   error.classList.add('shown');
-}
-
-async function fetchSearchedMovies(name) {
-  return await api.getMoviesByName(name);
 }
