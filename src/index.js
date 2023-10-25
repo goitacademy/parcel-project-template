49d2be466e0a8501bb096f53e170ab6adfecd354
gabriellaballa <balla_gabriella@yahoo.com>
2023-10-25 20:15:08 +0300

    update

--- src/index.js
+++ src/index.js
@@ -19,12 +19,10 @@ async function fetchWeatherData() {
 function updateForecast(data) {
   const forecastItems = document.getElementById('weather-forecast');
 
-  // Clear any existing forecast items
   forecastItems.innerHTML = '';
 
-  const dayMap = {}; // Use this object to group data by day
+  const dayMap = {};
 
-  // Loop through the data and group it by day
   data.list.forEach(item => {
     const date = new Date(item.dt * 1000);
     const day = date.toDateString();
@@ -36,12 +34,10 @@ function updateForecast(data) {
     dayMap[day].push(item);
   });
 
-  // Loop through the grouped data and populate the forecast for each day
   for (const day in dayMap) {
     const forecastItem = document.createElement('div');
     forecastItem.classList.add('weather-forecast-item');
 
-    // Extract data for the first item of each day (assuming it's at the same time for each day)
     const firstItem = dayMap[day][0];
 
     const dayElement = document.createElement('div');
@@ -50,7 +46,6 @@ function updateForecast(data) {
     dayElement.textContent = `${getDayOfWeek(date)}, ${formatDate(date)}`;
     forecastItem.appendChild(dayElement);
 
-    // Extract and display other weather information such as temperature and icons
     const temperatureElement = document.createElement('div');
     temperatureElement.classList.add('temperature');
     const minTemp = firstItem.main.temp_min;
@@ -58,7 +53,6 @@ function updateForecast(data) {
     temperatureElement.innerHTML = `Min: ${minTemp}&deg;C | Max: ${maxTemp}&deg;C`;
     forecastItem.appendChild(temperatureElement);
 
-    // Display the weather icon
     const iconElement = document.createElement('img');
     const iconCode = firstItem.weather[0].icon;
     iconElement.src = `https://openweathermap.org/img/wn/${iconCode}.png`;
