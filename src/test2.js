4e4594c2b72bde61cca446f0b9b9b552549d41f1
Dpescar <128416673+Dpescar@users.noreply.github.com>
2023-10-27 15:49:40 +0300

    main

--- src/test2.js
+++ src/test2.js
@@ -1,51 +1,273 @@
-const apiKey = '07aed853a2b3116bf7e19dfeee63b968';
-const latitude = 46.770439;
-const longitude = 23.591423;
+async function getWeatherData() {
+  const API_KEY = '07aed853a2b3116bf7e19dfeee63b968';
+  const city = 'Bucharest';
 
-const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`
+    );
 
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    const temperatureCelsius = data.list[0].main.temp - 273.15;
-    const humidity = data.list[0].main.humidity;
-    const windSpeed = data.list[0].wind.speed;
-    const atmosphericPressure = data.list[0].main.pressure;
+    const weatherData = response.data.list
+      .slice(0, 5)
+      .map(item => item.main.temp);
 
-    const weatherData = [
-      { label: 'Temperature (°C)', value: temperatureCelsius },
-      { label: 'Humidity (%)', value: humidity },
-      { label: 'Wind Speed (m/s)', value: windSpeed },
-      {
-        label: 'Atmospheric Pressure (mbar)',
-        value: atmosphericPressure,
-      },
-    ];
+    const labels = response.data.list.slice(0, 5).map(item => {
+      const date = new Date(item.dt * 1000);
+      const day = date.getDate().toString().padStart(2, '0');
+      const month = (date.getMonth() + 1).toString().padStart(2, '0');
+      const year = date.getFullYear();
+      const hours = date.getHours();
+      const minutes = date.getMinutes();
+      return `${day}/${month}/${year} ${hours}:${
+        minutes < 10 ? '0' : ''
+      }${minutes}`;
+    });
 
-    const ctx = document.getElementById('weather-chart').getContext('2d');
-    const weatherChart = new Chart(ctx, {
-      type: 'bar',
+    return { weatherData, labels };
+  } catch (error) {
+    console.error('Eroare la obținerea datelor meteorologice:', error);
+    return null;
+  }
+}
+
+async function getHumidityData() {
+  const API_KEY = '07aed853a2b3116bf7e19dfeee63b968';
+  const city = 'Bucharest';
+
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`
+    );
+
+    const humidityData = response.data.list
+      .slice(0, 5)
+      .map(item => item.main.humidity);
+    const labels = response.data.list.slice(0, 5).map(item => {
+      const date = new Date(item.dt * 1000);
+      const day = date.getDate().toString().padStart(2, '0');
+      const month = (date.getMonth() + 1).toString().padStart(2, '0');
+      const year = date.getFullYear();
+      const hours = date.getHours();
+      const minutes = date.getMinutes();
+      return `${day}/${month}/${year} ${hours}:${
+        minutes < 10 ? '0' : ''
+      }${minutes}`;
+    });
+
+    return { humidityData, labels };
+  } catch (error) {
+    console.error('Eroare la obținerea datelor de umiditate:', error);
+    return null;
+  }
+}
+
+async function getWindData() {
+  const API_KEY = '07aed853a2b3116bf7e19dfeee63b968';
+  const city = 'Bucharest';
+
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`
+    );
+
+    const windData = response.data.list
+      .slice(0, 5)
+      .map(item => item.wind.speed);
+    const labels = response.data.list.slice(0, 5).map(item => {
+      const date = new Date(item.dt * 1000);
+      const day = date.getDate().toString().padStart(2, '0');
+      const month = (date.getMonth() + 1).toString().padStart(2, '0');
+      const year = date.getFullYear();
+      const hours = date.getHours();
+      const minutes = date.getMinutes();
+      return `${day}/${month}/${year} ${hours}:${
+        minutes < 10 ? '0' : ''
+      }${minutes}`;
+    });
+
+    return { windData, labels };
+  } catch (error) {
+    console.error('Eroare la obținerea datelor pentru viteza vantului:', error);
+    return null;
+  }
+}
+
+async function getAtmosphereData() {
+  const API_KEY = '07aed853a2b3116bf7e19dfeee63b968';
+  const city = 'Bucharest';
+
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`
+    );
+
+    const atmosphereData = response.data.list
+      .slice(0, 5)
+      .map(item => item.main.pressure);
+
+    const labels = response.data.list.slice(0, 5).map(item => {
+      const date = new Date(item.dt * 1000);
+      const day = date.getDate().toString().padStart(2, '0');
+      const month = (date.getMonth() + 1).toString().padStart(2, '0');
+      const year = date.getFullYear();
+      const hours = date.getHours();
+      const minutes = date.getMinutes();
+      return `${day}/${month}/${year} ${hours}:${
+        minutes < 10 ? '0' : ''
+      }${minutes}`;
+    });
+
+    return { atmosphereData, labels };
+  } catch (error) {
+    console.error(
+      'Eroare la obținerea datelor pentru presiunea atmosferica:',
+      error
+    );
+    return null;
+  }
+}
+
+async function generateWeatherChart() {
+  const weatherData = await getWeatherData();
+  const humidityData = await getHumidityData();
+  const windData = await getWindData();
+  const atmosphereData = await getAtmosphereData();
+  if (weatherData) {
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const chartFont = () => {
+      if (window.innerWidth < 480) {
+        return 12;
+      } else {
+        return 18;
+      }
+    };
+    new Chart(ctx, {
+      type: 'line',
       data: {
-        labels: weatherData.map(item => item.label),
+        labels: weatherData.labels,
         datasets: [
           {
-            label: 'Weather Data',
-            data: weatherData.map(item => item.value),
-            backgroundColor: 'rgba(75, 192, 192, 0.2)',
-            borderColor: 'rgba(75, 192, 192, 1)',
-            borderWidth: 1,
+            label: 'Temperature',
+            data: weatherData.weatherData,
+            borderColor: 'rgb(255, 107, 9)',
+            borderWidth: 2,
+            fill: false,
+          },
+          {
+            label: 'Humidity',
+            data: humidityData.humidityData,
+            borderColor: 'rgb(9, 6, 235)',
+            borderWidth: 2,
+            fill: false,
+          },
+          {
+            label: 'Wind Speed',
+            data: windData.windData,
+            borderColor: 'rgb(234, 154, 5)',
+            borderWidth: 2,
+            fill: false,
+          },
+          {
+            label: 'Atmosphere pressure',
+            data: atmosphereData.atmosphereData,
+            borderColor: 'rgb(6, 120, 6)',
+            borderWidth: 2,
+            fill: false,
           },
         ],
       },
       options: {
+        responsive: true,
         scales: {
+          x: {
+            grid: {
+              color: 'rgb(100, 100, 100)',
+            },
+            ticks: {
+              color: 'rgb(100, 100, 100)',
+            },
+            title: {
+              display: true,
+              text: 'Date[dd/mm/yyyy - hour/minutes]',
+              color: 'rgb(100, 100, 100)',
+              font: {
+                size: chartFont(),
+              },
+            },
+          },
           y: {
             beginAtZero: true,
+            grid: {
+              color: 'rgb(100, 100, 100)',
+            },
+            ticks: {
+              color: 'rgb(100, 100, 100)',
+            },
+            title: {
+              display: true,
+              text: 'Value of Indicators',
+              color: 'rgb(100, 100, 100)',
+              font: {
+                size: chartFont(),
+              },
+            },
           },
         },
       },
     });
-  })
-  .catch(error => {
-    console.error('Error fetching weather data:', error);
-  });
+    const chartContainer = document.querySelector('.chart-container');
+    chartContainer.classList.add('hidden');
+
+    localStorage.setItem('chartContainerActivated', 'false');
+  }
+}
+
+window.addEventListener('resize', () => {
+  myChart.options.scales.x.ticks.font.size = chartFont();
+  myChart.options.scales.y.ticks.font.size = chartFont();
+  myChart.update();
+});
+
+generateWeatherChart();
+
+const chartCanvas = document.getElementById('myChart');
+const toggleButton = document.getElementById('chartButton');
+
+window.addEventListener('load', function () {
+  const chartState = localStorage.getItem('chartContainerActivated');
+
+  if (chartState === 'true') {
+    chartCanvas.style.display = 'block';
+    toggleButton.innerText = 'Hide Chart';
+  } else {
+    chartCanvas.style.display = 'none';
+    toggleButton.innerText = 'Show Chart';
+  }
+});
+
+toggleButton.addEventListener('click', function (event) {
+  event.preventDefault();
+  if (chartCanvas.style.display === 'none') {
+    chartCanvas.style.display = 'block';
+    toggleButton.innerText = 'Hide Chart';
+    localStorage.setItem('chartContainerActivated', 'true');
+  } else {
+    chartCanvas.style.display = 'none';
+    toggleButton.innerText = 'Show Chart';
+    localStorage.setItem('chartContainerActivated', 'false');
+  }
+});
+
+chartCanvas.style.display = 'none';
+
+function activateChartContainer() {
+  var chartContainer = document.querySelector('.chart-container');
+
+  if (chartContainer) {
+    chartContainer.classList.remove('hidden');
+    localStorage.setItem('chartContainerActivated', 'true');
+
+    var toggleButton = document.getElementById('chartButton');
+    toggleButton.innerText = 'Show Chart';
+  }
+}
