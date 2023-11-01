e4a250b6ab0bece9ee43c89b363f791bc5f42664
gabriellaballa <balla_gabriella@yahoo.com>
2023-11-01 09:13:26 +0200

    temperature

--- src/five-days.js
+++ src/five-days.js
@@ -57,8 +57,8 @@ function updateForecast(data) {
     temperatureElement.classList.add('temperature');
     const minTempKelvin = firstItem.main.temp_min;
     const maxTempKelvin = firstItem.main.temp_max;
-    const minTemp = Math.round(minTempKelvin);
-    const maxTemp = Math.round(maxTempKelvin);
+    const minTemp = Math.round(minTempKelvin - 273.15);
+    const maxTemp = Math.round(maxTempKelvin - 273.15);
     temperatureElement.innerHTML = `<div class="temperature__deg"><div class="temperature__design">min</div>
       <div class="temperature__data"> ${minTemp}&deg;C</div></div><span class="temperature__line"></span><div class="temperature__deg"><div class="temperature__design" > max</div>
     <div class="temperature__data"> ${maxTemp}&deg;C</div></div>`;
