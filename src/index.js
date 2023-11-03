014a109e9765d7bfa4b6e66a47c5464d05ec801b
gavrila0909 <gavriladanna@gmail.com>
2023-11-03 22:36:02 +0000

    outlined modal2

--- src/index.js
+++ src/index.js
@@ -12,3 +12,18 @@
       refs.modal.classList.toggle("is-hidden");
     }
   })();
+
+  (() => {
+    const refs = {
+      openModalBtn: document.querySelector("[data-modal-open-thanks]"),
+      closeModalBtn: document.querySelector("[data-modal-close-thanks]"),
+      modal: document.querySelector("[data-modal-thanks]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden-thanks");
+    }
+  })();
