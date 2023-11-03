e91b911b06834617bdd940bc8219526359bbc22c
Jasmine Lazar <lazarjasmine658@gmail.com>
2023-11-03 10:48:27 +0200

    final

--- src/today.js
+++ src/today.js
@@ -1,4 +1,6 @@
 const API_KEY = '07aed853a2b3116bf7e19dfeee63b968';
+const API_URL =
+  'https://api.openweathermap.org/data/2.5/weather?units=metric&q=cluj';
 const API_URL_BASE =
   'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
 const weatherIcon = document.querySelector('.hero-weather__emoji');
@@ -11,28 +13,33 @@ export async function todayWeather(cityName) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     var data = await response.json();
-    console.log(data);
-    // Update the DOM with new data
+
     document.querySelector('.hero-weather__city').textContent = data.name;
-    document.querySelector('.hero-weather__degree').textContent =
+    document.querySelector('.hero-weather__degrees').textContent =
       Math.round(data.main.temp) + '°';
     document.querySelector('.values__min h5').textContent =
       Math.round(data.main.temp_min) + '°';
     document.querySelector('.values__max h5').textContent =
       Math.round(data.main.temp_max) + '°';
-    // Update the weather icon based on the weather data
+
+    const heroWeatherIcon = document.querySelector('.hero-weather');
+    const existingIcon = document.querySelector('.hero-weather__emoji');
+    if (existingIcon) {
+      // If it exists, remove it
+      heroWeatherIcon.removeChild(existingIcon);
+    }
+    let iconToday = document.createElement('img');
+    iconToday.classList.add('hero-weather__emoji');
+    const iconApi = data.weather[0].icon;
+    const iconLink = `https://openweathermap.org/img/wn/${iconApi}@2x.png`;
+    iconToday.src = iconLink;
+    iconToday.alt = data.weather[0].description;
+    heroWeatherIcon.prepend(iconToday);
   } catch (error) {
     console.error('There was an error fetching the weather data:', error);
   }
-  const heroWeatherIcon = document.querySelector('.hero-weather');
-  let iconToday = document.createElement('img');
-  iconToday.classList.add('hero-weather__emoji');
-  const iconApi = data.weather[0].icon;
-  const iconLink = `https://openweathermap.org/img/wn/${iconApi}@2x.png`;
-  iconToday.src = iconLink;
-  iconToday.alt = data.weather[0].description;
-  heroWeatherIcon.prepend(iconToday);
 }
+
 // Event listeners for buttons
 todayBtn.addEventListener('click', e => {
   e.preventDefault();
@@ -40,13 +47,14 @@ todayBtn.addEventListener('click', e => {
   fiveDaysBtn.style.background = 'rgba(255, 255, 255, 0.5)';
   document.querySelector('.today-weather').style.display = 'block';
   document.querySelector('.chart-container').style.display = 'none';
+
   todayWeather(cityName);
 });
 fiveDaysBtn.addEventListener('click', e => {
-  e.preventDefault();
   fiveDaysBtn.style.background = 'white';
   todayBtn.style.background = 'rgba(255, 255, 255, 0.5)';
   document.querySelector('.today-weather').style.display = 'none';
   document.querySelector('.chart-container').style.display = 'flex';
+
   // Here you would call a function to display five days forecast
 });
