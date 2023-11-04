f002b3f293489716c082ce742bf2fb9df7ef2804
ovidutzul <142990412+ovidutzul@users.noreply.github.com>
2023-11-04 17:22:09 +0200

    modal name modified

--- src/buy-now-modal.js
+++ src/buy-now-modal.js
@@ -2,7 +2,7 @@
   const refs = {
     openModalBtn: document.querySelector('[data-buy-now-modal-open]'),
     closeModalBtn: document.querySelector('[data-buy-now-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    modal: document.querySelector('[data-buy-now-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
