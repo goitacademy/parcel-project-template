fabec34463b215cf57df33b42b7c53f530067751
Tihi Florin <tihi.florin66@gmail.com>
2023-11-06 01:18:49 +0200

    final

--- src/index-subscribe.js
+++ src/index-subscribe.js
@@ -9,6 +9,6 @@
     refs.closeModalBtn.addEventListener("click", toggleModal);
   
     function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
+      refs.modal.classList.toggle("is-hidden1");
     }
   })();
