4cdc1862ebf475dda83c9d2e4dd15bc33cbc587b
raynn07 <nicolaecimpoeru@yahoo.com>
2023-11-05 20:52:20 +0200

    Added modal section6

---
+++ src/subscribe-modal.js
@@ -0,0 +1,14 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector("[subscribe-modal-open]"),
+      closeModalBtn: document.querySelector("[subscribe-modal-close]"),
+      modal: document.querySelector("[subscribe-modal]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
+    }
+  })();
