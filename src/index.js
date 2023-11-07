17449027c99bd25ac2a3427c37dd866c53a442a6
IStefoi-Tutorial <iosif_stefoi93@yahoo.com>
2023-11-07 17:02:57 +0100

    add html and scss

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 
+let currentSlide = 0;
+const slides = document.querySelectorAll(".image");
+const imageCountElement = document.getElementById("imageCount");
+
+function showSlide(index) {
+    slides[currentSlide].style.transform = "translateX(-100%)";
+    currentSlide = (index + slides.length) % slides.length;
+    slides[currentSlide].style.transform = "translateX(0)";
+    updateIndicator();
+}
+
+function updateIndicator() {
+    imageCountElement.textContent = `Imagine ${currentSlide + 1} din ${slides.length}`;
+}
+
+function plusSlides(n) {
+    showSlide(currentSlide + n);
+}
+
+updateIndicator();
