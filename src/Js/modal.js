8c0606d0fcee98fd7be02efcd4c8d0f248e57ae9
Andyoase <136448630+Andyoase@users.noreply.github.com>
2023-11-07 17:14:41 +0100

    update modal

--- src/Js/modal.js
+++ src/Js/modal.js
@@ -5,8 +5,8 @@
     modal: document.querySelector("[data-modal]"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn?.addEventListener("click", toggleModal);
+  refs.closeModalBtn?.addEventListener("click", toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
