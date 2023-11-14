76e7a042504cbf704ef8daba6369f17c55bd57e6
exigibil <adrian.topa04@gmail.com>
2023-11-14 02:21:48 +0200

    welcome section css + about
    welcome section css + about

--- src/carousel.js
+++ src/carousel.js
@@ -1,28 +1,61 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+$(function() {
+  new Slider({
+      images: '.slider-1 img',
+      btnPrev: '.slider-1 .buttons .prev',
+      btnNext: '.slider-1 .buttons .next',
+      auto: false
+  });
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+new Slider({
+      images: '.slider-2 img',
+      btnPrev: '.slider-2 .buttons .prev',
+      btnNext: '.slider-2 .buttons .next',
+      auto: true,
+      rate: 2000
+  });
+});
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+function Slider(obj) {
+	this.images = $(obj.images);
+	this.auto = obj.auto;
+	this.btnPrev = obj.btnPrev;
+	this.btnNext = obj.btnNext;
+     this.rate = obj.rate || 1000;
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+	var i = 0;
+     var slider = this;
+
+    // The "Previous" button: to remove the class .shown, to show the previous image and to add the .shown class
+	this.prev = function () {
+		slider.images.eq(i).removeClass('shown');
+		i--;
+
+		if (i < 0) {
+			i = slider.images.length - 1;
+		}
+
+		slider.images.eq(i).addClass('shown');
+	}
+
+    // The "Next" button: to remove the class .shown, to show the next image and to add the .shown class
+	this.next = function () {
+		slider.images.eq(i).removeClass('shown');
+		i++;
+
+		if (i >= slider.images.length) {
+			i = 0;
+		}
+
+		slider.images.eq(i).addClass('shown');
+	}
+
+    // To add next and prev functions when clicking on the corresponding buttons
+    $(slider.btnPrev).on('click', function(){ slider.prev();});
+    $(slider.btnNext).on('click', function(){ slider.next();});
+
+    // For the automatic slider: this method calls the next function at the set rate
+	if (slider.auto)	{
+        setInterval(slider.next, slider.rate);
+    }
+};
+    
