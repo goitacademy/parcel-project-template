3bda24eb9894bd74340d17458f7ba0df413908d8
Kinga Janiak <janiak.kinga96@gmail.com>
2023-11-20 15:16:09 +0100

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from "./gallery-items.js";
+
+
+
+const galleryBox = document.querySelector("ul.gallery");
+
+for (const item of galleryItems) {
+ const imgGallery = `<li class="gallery__item"><a class="gallery__link" href="${item.original}">
+     <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+   </a></li>`;
+ galleryBox.insertAdjacentHTML("beforeend", imgGallery);
+}
+const lightboxInstance = new SimpleLightbox(".gallery__item a", {
+  captions: true, 
+  captionDelay: 250,
+  captionSelector: "img",
+  captionsData: "alt",
+  captionPosition: "bottom",
+});
+
 // Change code below this line
 
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+// time player
+player.on('timeupdate', throttle((event) => {
+    localStorage.setItem('videoplayer-current-time', event.seconds);
+  }, 1000));
+  
+// moment pauze video
+window.addEventListener('DOMContentLoaded', () => {
+  const time = localStorage.getItem('videoplayer-current-time');
+  player.setCurrentTime(time);
+});
