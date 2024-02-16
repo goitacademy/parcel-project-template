3e38586d2cd1c67ed5ba81a308e98a65bcc113a8
Fear3dB3ard <craciunstefanmarian@yahoo.com>
2024-02-16 14:00:05 +0200

    mai trebuie lucrat la hidden menu

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 (() => {
-  
-
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
