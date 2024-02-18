310a2e853bca17036a254045fc4dc146467596f9
GuvidSlayer <andgeorge0015@gmail.com>
2024-02-18 18:14:12 +0200

    terminat

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-menu-open]"),
-      closeModalBtn: document.querySelector("[data-menu-close]"),
-      modal: document.querySelector("[data-menu]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", openModal);
-    refs.closeModalBtn.addEventListener("click", closeModal);
-  
-    function openModal() {
-      refs.modal.style.visibility = "visible";
-    }
-  
-    function closeModal() {
-      refs.modal.style.visibility = "hidden";
-    }
-  })();
+  const refs = {
+    openModalBtn: document.querySelector('[data-menu-open]'),
+    closeModalBtn: document.querySelector('[data-menu-close]'),
+    modal: document.querySelector('[data-menu]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', openModal);
+  refs.closeModalBtn.addEventListener('click', closeModal);
+
+  function openModal() {
+    refs.modal.style.visibility = 'visible';
+  }
+
+  function closeModal() {
+    refs.modal.style.visibility = 'hidden';
+  }
+})();
+
+//thank - you;//
+
+var modal = document.getElementById('modal');
+var openModalBtn = document.getElementById('openModalBtn');
+var closeModal = document.getElementById('closeModal');
+
+openModalBtn.addEventListener('click', function () {
+  modal.style.display = 'block';
+});
+
+closeModal.addEventListener('click', function () {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+});
