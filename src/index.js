15789777291a12f8c3be1ec3b340f07ca8da84e4
Radu Bogdan Naramzoiu <turbomatrixxxl@gmail.com>
2024-02-19 21:41:14 +0200

    cadded iteration.js in src and made path to it

--- src/index.js
+++ src/index.js
@@ -12,33 +12,6 @@ document.addEventListener('DOMContentLoaded', () => {
   closeMenuBtn.addEventListener('click', toggleMenu);
 });
 
-// Carousel iteration
-let translate = 0;
-
-const onNext = () => {
-  const items = document.querySelectorAll('[data-carousel-item]');
-  if (translate < items.length - 1) {
-    translate = translate + 1;
-
-    percentage = -100 * translate;
-    items.forEach(item => {
-      item.style.transform = `translateY(${percentage}%)`;
-    });
-  }
-};
-
-const onBack = () => {
-  const items = document.querySelectorAll('[data-carousel-item]');
-  if (translate > 0) {
-    translate = translate - 1;
-
-    percentage = -100 * translate;
-    items.forEach(item => {
-      item.style.transform = `translateY(${percentage}%)`;
-    });
-  }
-};
-
 // Modal-form
 (() => {
   const refs = {
---
+++ src/iteration.js
@@ -0,0 +1,26 @@
+// Carousel iteration
+let translate = 0;
+
+const onNext = () => {
+  const items = document.querySelectorAll('[data-carousel-item]');
+  if (translate < items.length - 1) {
+    translate = translate + 1;
+
+    percentage = -100 * translate;
+    items.forEach(item => {
+      item.style.transform = `translateY(${percentage}%)`;
+    });
+  }
+};
+
+const onBack = () => {
+  const items = document.querySelectorAll('[data-carousel-item]');
+  if (translate > 0) {
+    translate = translate - 1;
+
+    percentage = -100 * translate;
+    items.forEach(item => {
+      item.style.transform = `translateY(${percentage}%)`;
+    });
+  }
+};
