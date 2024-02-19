0f3c1e4e2f715229ce39fea886973bf8a375b149
DragosU25 <149568004+DragosU25@users.noreply.github.com>
2024-02-19 18:58:43 +0100

    rezolvare buguri

--- src/index.js
+++ src/index.js
@@ -1,44 +1,4 @@
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-menu-open]'),
-    closeModalBtn: document.querySelector('[data-menu-close]'),
-    modal: document.querySelector('[data-menu]'),
-  };
-
-  refs.openModalBtn.addEventListener('click', openModal);
-  refs.closeModalBtn.addEventListener('click', closeModal);
-
-  function openModal() {
-    refs.modal.style.visibility = 'visible';
-  }
-
-  function closeModal() {
-    refs.modal.style.visibility = 'hidden';
-  }
-})();
-
-//thank - you;//
-
-var modal = document.getElementById('modal');
-var openModalBtn = document.getElementById('openModalBtn');
-var closeModal = document.getElementById('closeModal');
-
-openModalBtn.addEventListener('click', function () {
-  modal.style.display = 'block';
-});
-
-closeModal.addEventListener('click', function () {
-  modal.style.display = 'none';
-});
-
-window.addEventListener('click', function (event) {
-  if (event.target == modal) {
-    modal.style.display = 'none';
-  }
-});
-
-// mobile-menu
-(() => {
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
@@ -65,25 +25,4 @@ window.addEventListener('click', function (event) {
     openMenuBtn.setAttribute('aria-expanded', false);
     bodyScrollLock.enableBodyScroll(document.body);
   });
-})();
-
-// hamburger-menu
-
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-menu-open]"),
-    closeModalBtn: document.querySelector("[data-menu-close]"),
-    modal: document.querySelector("[data-menu]"),
-  };
-
-  refs.openModalBtn.addEventListener("click", openModal);
-  refs.closeModalBtn.addEventListener("click", closeModal);
-
-  function openModal() {
-    refs.modal.style.visibility = "visible";
-  }
-
-  function closeModal() {
-    refs.modal.style.visibility = "hidden";
-  }
 })();
