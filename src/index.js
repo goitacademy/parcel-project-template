e27027d33176af0009bb537a1da09bfa575d6320
Radu Bogdan Naramzoiu <turbomatrixxxl@gmail.com>
2024-02-21 14:39:55 +0200

    resolved loop iteration on hero and added js code and deleted js file

--- src/index.js
+++ src/index.js
@@ -119,3 +119,46 @@ function cardCarousel3d(carousels) {
 }
 
 cardCarousel3d(document.querySelectorAll('.card-carousel'));
+
+// carousel infinite 2
+
+const wrapper = document.querySelector('.wrapper');
+const container = document.querySelector('.container');
+const children = wrapper.querySelectorAll('.child');
+const leftArr = document.querySelector('.left-arrow');
+const rightArr = document.querySelector('.right-arrow');
+
+let counter = 0;
+let infiniteCounter = 0;
+
+container.addEventListener('click', function (e) {
+  wrapper.style.transition = '.5ms ease all';
+  if (e.target.classList.contains('left-arrow')) {
+    infiniteCounter = -1;
+    counter += 20;
+    wrapper.style.transform = `translateX(${counter}%)`;
+  }
+
+  if (e.target.classList.contains('right-arrow')) {
+    infiniteCounter = 1;
+    counter -= 20;
+    wrapper.style.transform = `translateX(${counter}%)`;
+  }
+});
+
+let infiniteCarousel = function () {
+  wrapper.style.transition = 'none';
+  counter = 0;
+
+  if (infiniteCounter === 1) {
+    wrapper.appendChild(wrapper.firstElementChild);
+    wrapper.style.transform = `translateX(0%)`;
+  }
+
+  if (infiniteCounter === -1) {
+    wrapper.style.transform = `translateX(0%)`;
+    wrapper.prepend(wrapper.lastElementChild);
+  }
+};
+
+wrapper.addEventListener('transitionend', infiniteCarousel);
